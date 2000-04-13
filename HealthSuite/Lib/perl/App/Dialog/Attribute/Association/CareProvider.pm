ba92d46284c88c7775e5860bbc73c348fd696a85
radha_kotagiri <radha_kotagiri>
2000-04-13 13:43:12 +0000

    Changed the field "Relationship" to "Specialty"

--- HealthSuite/Lib/perl/App/Dialog/Attribute/Association/CareProvider.pm
+++ HealthSuite/Lib/perl/App/Dialog/Attribute/Association/CareProvider.pm
@@ -30,14 +30,22 @@ sub new
 	croak 'schema parameter required' unless $schema;
 
 	$self->addContent(
-		new CGI::Dialog::Subhead(heading => 'Attach to Existing Record', name => 'exists_heading'),
-		new App::Dialog::Field::Person::ID(caption =>'Person ID', name => 'rel_id', hints => 'Please provide an existing Person ID. A link will be created between the patient and contact.'),
-		new CGI::Dialog::Subhead(heading => 'Define New Record', name => 'notexists_heading'),
-		new CGI::Dialog::Field(caption =>'Full Name', name => 'rel_name', hints => 'Please provide the full name of the contact if a record does not exist for him/her. A link will not be created between the patient and contact.'),
-		new CGI::Dialog::Subhead(heading => 'Contact Information', name => 'contact_heading'),
-		new CGI::Dialog::Field(caption => 'Relationship', name => 'rel_type', options => FLDFLAG_REQUIRED),
-		new CGI::Dialog::Field(type => 'phone', caption => 'Phone Number', name => 'phone_number', options => FLDFLAG_REQUIRED),
-		new CGI::Dialog::Field(type => 'date', caption => 'Begin Date', name => 'begin_date', defaultValue => ''),
+		#new CGI::Dialog::Subhead(heading => 'Attach to Existing Record', name => 'exists_heading'),
+		new App::Dialog::Field::Person::ID(caption =>'Physician/Provider ID', name => 'rel_id', hints => 'Please provide an existing Person ID.', options => FLDFLAG_REQUIRED),
+		#new CGI::Dialog::Subhead(heading => 'Define New Record', name => 'notexists_heading'),
+		#new CGI::Dialog::Field(caption =>'Full Name', name => 'rel_name', hints => 'Please provide the full name of the contact if a record does not exist for him/her. A link will not be created between the patient and contact.'),
+		#new CGI::Dialog::Subhead(heading => 'Contact Information', name => 'contact_heading'),
+		new CGI::Dialog::Field(caption => 'Specialty',
+								#type => 'foreignKey',
+								name => 'rel_type',
+								fKeyStmtMgr => $STMTMGR_PERSON,
+								fKeyStmt => 'selMedicalSpeciality',
+								fKeyDisplayCol => 0,
+								fKeyValueCol => 1,
+						invisibleWhen => CGI::Dialog::DLGFLAG_UPDORREMOVE),
+						
+		#new CGI::Dialog::Field(type => 'phone', caption => 'Phone Number', name => 'phone_number', options => FLDFLAG_REQUIRED),
+		#new CGI::Dialog::Field(type => 'date', caption => 'Begin Date', name => 'begin_date', defaultValue => ''),
 	);
 	$self->{activityLog} =
 	{
@@ -56,22 +64,22 @@ sub customValidate
 	my ($self, $page) = @_;
 
 	my $pId = $self->getField('rel_id');
-	my $pName = $self->getField('rel_name');
+	#my $pName = $self->getField('rel_name');
 	my $itemId = $page->param('item_id');
 
-	if($page->field('rel_id') && $page->field('rel_name'))
-	{
-		$pId->invalidate($page, "Cannot provide both '$pId->{caption}' and '$pName->{caption}'");
-		$pName->invalidate($page, "Cannot provide both '$pId->{caption}' and '$pName->{caption}'");
-	}
-	else
-	{
-		unless($page->field('rel_id') || $page->field('rel_name'))
-		{
-			$pId->invalidate($page, "Please provide either '$pId->{caption}' or '$pName->{caption}'");
-			$pName->invalidate($page, "Please provide either '$pId->{caption}' or '$pName->{caption}'");
-		}
-	}
+	#if($page->field('rel_id') && $page->field('rel_name'))
+	#{
+	#	$pId->invalidate($page, "Cannot provide both '$pId->{caption}' and '$pName->{caption}'");
+	#	$pName->invalidate($page, "Cannot provide both '$pId->{caption}' and '$pName->{caption}'");
+	#}
+	#else
+	#{
+	#	unless($page->field('rel_id') || $page->field('rel_name'))
+	#	{
+	#		$pId->invalidate($page, "Please provide either '$pId->{caption}' or '$pName->{caption}'");
+	#		$pName->invalidate($page, "Please provide either '$pId->{caption}' or '$pName->{caption}'");
+	#	}
+	#}
 }
 
 sub populateData
@@ -88,14 +96,14 @@ sub populateData
 	$page->field('begin_date', $data->{value_date});
 
 	my $valueInt =  $data->{value_int};
-	if($valueInt == 0)
-	{
-		$page->field('rel_name', $data->{value_text});
-	}
-	else
-	{
+	#if($valueInt == 0)
+	#{
+	#	$page->field('rel_name', $data->{value_text});
+	#}
+	#else
+	#{
 		$page->field('rel_id', $data->{value_text});
-	}
+	#}
 }
 
 sub execute
@@ -103,23 +111,27 @@ sub execute
 	my ($self, $page, $command, $flags) = @_;
 
 	my $relId = $page->field('rel_id');
-	my $relName = $page->field('rel_name');
+	#my $relName = $page->field('rel_name');
 
-	my $valueText = $relId eq '' ? $relName : $relId;
+	#my $valueText = $relId eq '' ? $relName : $relId;
 	my $constrained = $relId eq '' ? 0 : 1;
-
+	my $medSpecCode = $page->field('rel_type');
+	my $medSpecCaption = $STMTMGR_PERSON->getSingleValue($page, STMTMGRFLAG_CACHE, 'selMedicalSpecialtyCaption', $medSpecCode);
 	$page->schemaAction(
 		'Person_Attribute',	$command,
 		parent_id => $page->param('person_id') || undef,
 		item_id => $page->param('item_id') || undef,
-		item_name => $page->field('rel_type') || undef,
+		item_name => $medSpecCaption || undef,
 		value_type => App::Universal::ATTRTYPE_PROVIDER || undef,
-		value_text => $valueText || undef,
-		value_textB => $page->field('phone_number') || undef,
-		value_date => $page->field('begin_date') || undef,
+		#value_text => $valueText || undef,
+		value_text => $relId || undef,
+		#value_textB => $page->field('phone_number') || undef,
+		#value_date => $page->field('begin_date') || undef,
 		value_int => defined $constrained ? $constrained : undef,
 		_debug => 0
 	);
+	
+	
 	$self->handlePostExecute($page, $command, $flags | CGI::Dialog::DLGFLAG_IGNOREREDIRECT);
 	return "\u$command completed.";
 }
