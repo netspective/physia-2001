36f0f2fdbf7ab097af8db91be856f31df57665a6
moorthy_medavarapu <moorthy_medavarapu>
2000-04-14 00:09:12 +0000

    added 2 new url entries for  surgery procs

--- HealthSuite/Lib/perl/App/Page/Person.pm
+++ HealthSuite/Lib/perl/App/Page/Person.pm
@@ -360,6 +360,7 @@ sub prepare_view_profile
 					</TABLE><BR>
 					#component.stp-person.hospitalizationSurgeriesTherapies#<BR>
 					#component.stp-person.activeProblems#<BR>
+					#component.stp-person.surgeryProcedures#<BR>										
 					#component.stp-person.authorization#<BR>
 						<TABLE CELLSPACING=0 BORDER=0 CELLPADDING=0 WIDTH=100%>
 							<TR VALIGN=TOP>
@@ -408,6 +409,7 @@ sub prepare_view_chart
 					</TABLE><BR>
 					#component.stp-person.hospitalizationSurgeriesTherapies#<BR>
 					#component.stp-person.activeProblems#<BR>
+					#component.stp-person.surgeryProcedures#<BR>					
 					#component.stp-person.testsAndMeasurements#
 					</font>
 				</TD>
