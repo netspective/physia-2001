96a69dc2b2dfc9bd4de54faec40ba6fe953dbd67
radha_kotagiri <radha_kotagiri>
2000-04-16 16:50:19 +0000

    [B2]   Changed the code in PopulateData_add  to automatically display the "Bill Sequence"

--- HealthSuite/Lib/perl/App/Dialog/InsurancePlan/PersonalCoverage.pm
+++ HealthSuite/Lib/perl/App/Dialog/InsurancePlan/PersonalCoverage.pm
@@ -20,20 +20,20 @@ use Date::Manip;
 sub new
 {
 	my $self = CGI::Dialog::new(@_, id => 'coverage', heading => '$Command Personal Insurance Coverage');
-	
+
 		#my $id = $self->{'id'}; 	# id = 'insur_pay' | 'personal_pay'
-	
+
 		my $schema = $self->{schema};
 		delete $self->{schema};  # make sure we don't store this!
-	
+
 		croak 'schema parameter required' unless $schema;
-	
-		$self->addContent(	
+
+		$self->addContent(
 				new CGI::Dialog::Field(type => 'hidden', name => 'phone_item_id'),
 				new CGI::Dialog::Field(type => 'hidden', name => 'fax_item_id'),
 				new CGI::Dialog::Field(type => 'hidden', name => 'item_id'),
 				new App::Dialog::Field::Person::ID(caption => 'Person/Patient ID',types => ['Patient'],	name => 'person_id'),
-				new App::Dialog::Field::Organization::ID(caption => 'Insurance Company ID', name => 'ins_org_id', options => FLDFLAG_REQUIRED),		
+				new App::Dialog::Field::Organization::ID(caption => 'Insurance Company ID', name => 'ins_org_id', options => FLDFLAG_REQUIRED),
 				new CGI::Dialog::Field(caption => 'Product Name', name => 'product_name', options => FLDFLAG_REQUIRED, findPopup => '/lookup/insurance/product_name'),
 				new CGI::Dialog::Field(caption => 'Plan Name', name => 'plan_name', options => FLDFLAG_REQUIRED, findPopup => '/lookup/insurance/plan_name'),
 				#new CGI::Dialog::Field::TableColumn(
@@ -41,9 +41,9 @@ sub new
 				#					schema => $schema,
 				#					column => 'Insurance.ins_type',
 				#					typeGroup => ['insurance', 'workers compensation']),
-				
+
 				#new CGI::Dialog::Field(lookup => 'Bill_Sequence', caption => 'Insurance Sequence', name => 'bill_sequence', options => FLDFLAG_REQUIRED),
-					
+
 				new CGI::Dialog::Field::TableColumn(
 									caption => 'Insurance Sequence',
 									schema => $schema,
@@ -51,9 +51,9 @@ sub new
 									onValidate => \&App::Dialog::InsurancePlan::validateExistingInsSeq,
 									onValidateData => $self,
 									defaultValue => 1,
-									options => FLDFLAG_REQUIRED),			
-			#new CGI::Dialog::Field(caption => 'Fee Schedules', name => 'fee_schedules'),					
-			new CGI::Dialog::Subhead(heading => 'General Plan Information', name => 'gen_plan_heading'),			
+									options => FLDFLAG_REQUIRED),
+			#new CGI::Dialog::Field(caption => 'Fee Schedules', name => 'fee_schedules'),
+			new CGI::Dialog::Subhead(heading => 'General Plan Information', name => 'gen_plan_heading'),
 			new CGI::Dialog::MultiField(caption =>'Group Name/Group Number', name => 'group',
 				fields => [
 						new CGI::Dialog::Field::TableColumn(
@@ -63,7 +63,7 @@ sub new
 							schema => $schema,
 							column => 'Insurance.group_number'),
 					]),
-					
+
 			new CGI::Dialog::Field::TableColumn(
 							caption => 'Member Number',
 							schema => $schema,
@@ -77,7 +77,7 @@ sub new
 
 
 			#new CGI::Dialog::MultiField(caption =>'Insured/Guarantor ID', name => 'insured_guarantor_ids',
-			#	fields => [						
+			#	fields => [
 			#			new App::Dialog::Field::Person::ID(caption => 'Insured ID',
 			#				types => ['Patient'],
 			#				name => 'insured_id',options => FLDFLAG_REQUIRED),
@@ -117,7 +117,7 @@ sub new
 								new CGI::Dialog::Field(caption => 'Begin Date', name => 'coverage_begin_date', type => 'date', options => FLDFLAG_REQUIRED, pastOnly => 1),
 								new CGI::Dialog::Field(caption => 'End Date', name => 'coverage_end_date', type => 'date', futureOnly => 1),
 							]),
-					
+
 			new CGI::Dialog::MultiField(caption =>'Deductible Amounts', hints => 'Individual/Family', name => 'deduct_amts',
 					fields => [
 								new CGI::Dialog::Field::TableColumn(caption => 'Individual Deductible Amount',
@@ -125,7 +125,7 @@ sub new
 								new CGI::Dialog::Field::TableColumn(caption => 'Family Deductible Amount',
 									schema => $schema, column => 'Insurance.family_deductible_amt'),
 					]),
-					
+
 			new CGI::Dialog::MultiField(caption =>'Percentage Pay/Threshold', name => 'percentage_threshold',
 				fields => [
 					new CGI::Dialog::Field::TableColumn(
@@ -141,7 +141,7 @@ sub new
 						schema => $schema,
 					column => 'Insurance.copay_amt'),
 
-			new CGI::Dialog::Subhead(heading => 'Remittance Information', name => 'remittance_heading'),		
+			new CGI::Dialog::Subhead(heading => 'Remittance Information', name => 'remittance_heading'),
 			new CGI::Dialog::Field::TableColumn(caption => 'Remittance Type',
 						name => 'remit_type',
 						schema => $schema,
@@ -153,7 +153,7 @@ sub new
 						findPopup => '/lookup/envoypayer/id'),
 
 			new CGI::Dialog::Field(caption => 'Remit Payer Name', name => 'remit_payer_name'),
-				
+
 			new CGI::Dialog::Subhead(heading => 'Add Another Personal Insurance Coverage', name => 'insur_heading', invisibleWhen => CGI::Dialog::DLGFLAG_UPDORREMOVE),
 			new CGI::Dialog::Field(
 								type => 'bool',
@@ -161,14 +161,14 @@ sub new
 								caption => 'Add Additional Personal Insurance Coverage?',
 								style => 'check',
 								invisibleWhen => CGI::Dialog::DLGFLAG_UPDORREMOVE),
-	
+
 			new CGI::Dialog::MultiField(caption =>'InsCompanyID/ProductName/PlanName', name => 'insplan',invisibleWhen => CGI::Dialog::DLGFLAG_UPDORREMOVE,
 				fields => [
-							new App::Dialog::Field::Organization::ID(caption => 'Ins Company ID', name => 'ins_comp',invisibleWhen => CGI::Dialog::DLGFLAG_UPDORREMOVE),		
+							new App::Dialog::Field::Organization::ID(caption => 'Ins Company ID', name => 'ins_comp',invisibleWhen => CGI::Dialog::DLGFLAG_UPDORREMOVE),
 							new CGI::Dialog::Field(caption => 'Product Name', name => 'product', findPopup => '/lookup/insurance/product_name', invisibleWhen => CGI::Dialog::DLGFLAG_UPDORREMOVE),
-							new CGI::Dialog::Field(caption => 'Plan Name', name => 'plan', findPopup => '/lookup/insurance/plan_name', invisibleWhen => CGI::Dialog::DLGFLAG_UPDORREMOVE)				
+							new CGI::Dialog::Field(caption => 'Plan Name', name => 'plan', findPopup => '/lookup/insurance/plan_name', invisibleWhen => CGI::Dialog::DLGFLAG_UPDORREMOVE)
 						])
-				
+
 			);
 
 			$self->{activityLog} =
@@ -182,11 +182,11 @@ sub new
 							nextActions_add => [
 								['Add Another Insurance Coverage', "/person/%param.person_id%/dlg-add-ins-coverage?_f_product_name=%field.product%&_f_ins_org_id=%field.ins_comp%&_f_plan_name=%field.plan%&_f_bill_sequence=%field.bill_sequence%", 1],
 								['Go to Person Profile', "/person/%param.person_id%/profile"],
-							],								
+							],
 								cancelUrl => $self->{cancelUrl} || undef
 						)
 					);
-				
+
 			return $self;
 }
 
@@ -194,8 +194,8 @@ sub makeStateChanges
 {
 	my ($self, $page, $command, $dlgFlags) = @_;
 
-	$self->SUPER::makeStateChanges($page, $command, $dlgFlags);		
-	
+	$self->SUPER::makeStateChanges($page, $command, $dlgFlags);
+
 	$self->updateFieldFlags('person_id', FLDFLAG_INVISIBLE, 1) if ($page->param('person_id') ne '');
 	#turn guarantor id off if patient is 21 or over
 	if(my $personId = $page->param('person_id'))
@@ -204,7 +204,7 @@ sub makeStateChanges
 		#$self->updateFieldFlags('insured_guarantor_ids', FLDFLAG_INVISIBLE, $patientAge < 21 ? 0 : 1);
 		#$self->updateFieldFlags('insured_id', FLDFLAG_INVISIBLE, $patientAge < 21 ? 1 : 0);
 		my $insuredId = $patientAge < 21 ? '' : $personId;
-		$page->field('insured_id', $insuredId);		
+		$page->field('insured_id', $insuredId);
 	}
 }
 
@@ -214,35 +214,35 @@ sub customValidate
 
 	my $command = $self->getActiveCommand($page);
 
-	return () if $command eq 'remove';		
-	
+	return () if $command eq 'remove';
+
 	my $relToInsured = $page->field('rel_to_insured');
 	my $relToInsuredField = $self->getField('rel_to_insured');
 	my $insuredId = $page->field('insured_id');
 	my $insuredIdField = $self->getField('insured_id');
 
-	my $personId = $page->param('person_id');	
-	
+	my $personId = $page->param('person_id');
+
 	if ($relToInsured != 0 && ($insuredId eq $personId || $insuredId eq ''))
 	{
 		$relToInsuredField->invalidate($page, "Must select 'Self' in '$relToInsuredField->{caption}' if '$insuredIdField->{caption}' is left blank.");
 		$insuredIdField->invalidate($page, "Valid insured ID is needed (other than '$personId') if '$relToInsuredField->{caption}' is other than 'Self.'");
 	}
-	
+
 	elsif($relToInsured == 0 && ($insuredId ne $personId && $insuredId ne ''))
 	{
 		$relToInsuredField->invalidate($page, "Must select '$relToInsuredField->{caption}' (other than 'Self') if '$insuredIdField->{caption}' is not '$personId'.");
 		$insuredIdField->invalidate($page, "'$insuredIdField->{caption}' must be '$personId' when selecting 'Self' in '$relToInsuredField->{caption}'");
 	}
-	
-	my $insOrg = $self->getField('insplan')->{fields}->[0];		
+
+	my $insOrg = $self->getField('insplan')->{fields}->[0];
 	my $productName = $self->getField('insplan')->{fields}->[1];
 	my $PlanName = $self->getField('insplan')->{fields}->[2];
 	my $addIns = $page->field('add_insurance');
-	if($addIns ==1 && 
+	if($addIns ==1 &&
 		($page->field('ins_comp') eq '' || $page->field('product') eq '' || $page->field('plan') eq ''))
-	{	
-		$insOrg->invalidate($page, " 'Ins Org ID', 'ProductName' and 'PlanName' cannot be blank if the Insurance Coverage is checked.");			
+	{
+		$insOrg->invalidate($page, " 'Ins Org ID', 'ProductName' and 'PlanName' cannot be blank if the Insurance Coverage is checked.");
 	}
 }
 
@@ -251,40 +251,58 @@ sub populateData_add
 	my ($self, $page, $command, $activeExecMode, $flags) = @_;
 
 		return unless ($flags & CGI::Dialog::DLGFLAG_ADD_DATAENTRY_INITIAL);
-		my $personId = $page->param('person_id');			
-		my $insSequence = $page->field('bill_sequence');
-		my $newInsSequence = $insSequence + 1;
-		$newInsSequence > 4 ? 99 : $newInsSequence;
-		$page->field('bill_sequence', $newInsSequence);
+
+		my $personId = $page->param('person_id');
+		#my $insSequence = $page->field('bill_sequence');
+		#my $newInsSequence = $insSequence + 1;
+		#$newInsSequence > 4 ? 99 : $newInsSequence;
+		#$page->field('bill_sequence', $newInsSequence);
+
+		my $ownerId = $page->param('person_id');
+		my $billSequence = $STMTMGR_INSURANCE->getRowsAsHashList($page, STMTMGRFLAG_NONE, 'selInsSequence', $ownerId);
+		foreach my $existSequence(@{$billSequence})
+		{
+			my $sequence = $existSequence->{'bill_sequence'};
+			if ($sequence < 4)
+			{
+				my $newSequence = $sequence + 1;
+				$page->field('bill_sequence', $newSequence);
+			}
+			else
+			{
+				$page->field('bill_sequence', App::Universal::INSURANCE_INACTIVE);
+			}
+		}
+
 		my $productName = $page->field('product_name');
 		my $planName = $page->field('plan_name');
-		my $planType = App::Universal::RECORDTYPE_INSURANCEPLAN;		
-		my $planData = $STMTMGR_INSURANCE->createFieldsFromSingleRow($page, STMTMGRFLAG_NONE, 'selInsPlan', $productName, $planName);		
+		my $planType = App::Universal::RECORDTYPE_INSURANCEPLAN;
+		my $planData = $STMTMGR_INSURANCE->createFieldsFromSingleRow($page, STMTMGRFLAG_NONE, 'selInsPlan', $productName, $planName);
 }
 
 sub populateData_update
 {
 	my ($self, $page, $command, $activeExecMode, $flags) = @_;
 
-	# Populating the fields while updating the dialog	
+	# Populating the fields while updating the dialog
 	return unless ($flags & CGI::Dialog::DLGFLAG_UPDORREMOVE_DATAENTRY_INITIAL);
-	
+
 	my $insIntId = $page->param('ins_internal_id');
 	if(! $STMTMGR_INSURANCE->createFieldsFromSingleRow($page, STMTMGRFLAG_NONE, 'selInsuranceData', $insIntId))
 	{
 		$page->addError("Ins Internal ID '$insIntId' not found.");
-	}	
+	}
 }
 
 sub populateData_remove
 {
 	populateData_update(@_);
 }
-   
+
 sub execute
 {
 	my ($self, $page, $command, $flags) = @_;
-	
+
 	my $productName = $page->field('product_name');
 	my $planName = $page->field('plan_name');
 	my $recordType = App::Universal::RECORDTYPE_INSURANCEPLAN;
@@ -293,7 +311,7 @@ sub execute
 	my $insType = $planData->{'ins_type'};
 	my $editInsIntId = $page->param('ins_internal_id');
 	my $personId = $page->param('person_id') ne '' ? $page->param('person_id') : $page->field('person_id');
-	
+
 	my $insIntId = $page->schemaAction(
 				'Insurance', $command,
 				ins_internal_id => $editInsIntId || undef,
@@ -314,7 +332,7 @@ sub execute
 				guarantor_id => $page->field('guarantor_id') || undef,
 				rel_to_insured => $page->field('rel_to_insured') || undef,
 				indiv_deduct_remain => $page->field('indiv_deduct_remain') || undef,
-				family_deduct_remain => $page->field('family_deduct_remain') || undef,	
+				family_deduct_remain => $page->field('family_deduct_remain') || undef,
 				copay_amt => $page->field('copay_amt') || undef,
 				coverage_begin_date => $page->field('coverage_begin_date') || undef,
 				coverage_end_date => $page->field('coverage_end_date') || undef,
@@ -326,10 +344,10 @@ sub execute
 				remit_payer_id => $page->field('remit_payer_id') || undef,
 				remit_payer_name => $page->field('remit_payer_name') || undef,
 				_debug => 0
-			);	
-			
+			);
+
 	#$insIntId = $command eq 'add' ? $insIntId : $editInsIntId;
-		
+
 	#$self->handleAttributes($page, $command, $flags, $insIntId);
 	$self->handlePostExecute($page, $command, $flags);
 	return '';
@@ -356,7 +374,7 @@ sub _handleAttributes
 
 	my $textAttrType = App::Universal::ATTRTYPE_TEXT;
 	my $phoneAttrType = App::Universal::ATTRTYPE_PHONE;
-	my $faxAttrType = App::Universal::ATTRTYPE_FAX;	
+	my $faxAttrType = App::Universal::ATTRTYPE_FAX;
 
 	$page->schemaAction(
 			'Insurance_Attribute', $command,
@@ -377,7 +395,7 @@ sub _handleAttributes
 			value_text => $page->field('fax') || undef,
 			_debug => 0
 		);
-		
+
 	$self->handlePostExecute($page, $command, $flags);
 	return '';
 }
