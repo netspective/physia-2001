ae8a1a73a13b8f8a4076b72bd785596e96b4998f
radha_kotagiri <radha_kotagiri>
2000-04-19 15:29:30 +0000

    [B53] Added a dialog Id for Specialty pane

---
+++ HealthSuite/Lib/perl/App/Dialog/Attribute/Certificate/Specialty.pm
@@ -0,0 +1,39 @@
+###############################################################################
+package App::Dialog::Attribute::Certificate::Specialty;
+###############################################################################
+
+use DBI::StatementManager;
+use App::Statements::Invoice;
+
+use strict;
+use Carp;
+use CGI::Dialog;
+use App::Dialog::Attribute::Certificate;
+use App::Dialog::Field::Attribute;
+use CGI::Validator::Field;
+use App::Universal;
+use Date::Manip;
+use Devel::ChangeLog;
+use App::Statements::Person;
+use vars qw(@ISA @CHANGELOG);
+@ISA = qw(App::Dialog::Attribute::Certificate);
+
+sub initialize
+{
+	my $self = shift;
+
+	$self->heading('$Command Specialty');
+
+	$self->addContent(
+		new CGI::Dialog::Field(caption => 'Specialty',
+					#type => 'foreignKey',
+					name => 'value_text',
+					fKeyStmtMgr => $STMTMGR_PERSON,
+					fKeyStmt => 'selMedicalSpeciality',
+					fKeyDisplayCol => 0,
+					fKeyValueCol => 1)
+
+	);
+
+	$self->SUPER::initialize();
+}
