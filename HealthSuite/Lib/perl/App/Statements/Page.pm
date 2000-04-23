cabcd50de8b8b25a112113ece86de5da0c80633b
thai_nguyen <thai_nguyen>
2000-04-23 23:27:07 +0000

    [B36] Added Statement Manger for general Page activities

--- HealthSuite/Lib/perl/App/Statements/Page.pm
+++ HealthSuite/Lib/perl/App/Statements/Page.pm
@@ -6,7 +6,7 @@ use strict;
 use Exporter;
 use DBI::StatementManager;
 
-use vars qw(@ISA @EXPORT);
+use vars qw(@ISA @EXPORT $STMTMGR_PAGE);
 @ISA    = qw(Exporter DBI::StatementManager);
 @EXPORT = qw($STMTMGR_PAGE);
 
@@ -15,28 +15,65 @@ $STMTMGR_PAGE = new App::Statements::Page(
 	'sel_SessionInfo' => qq{
 		select * 
 		from Persess_View_Count 
-		where session_id = ?
+		where person_id = ?
 			and view_scope = ?
 			and view_key   = ?
 	},
 	
 	'upd_count' => qq{
 		update Persess_View_Count
-		set Count = Count +1,
+		set view_count = view_count +1,
 			view_latest = sysdate
-		where session_id = ?
+		where person_id = ?
 			and view_scope = ?
 			and view_key   = ?
 	},
 	
 	'ins_newKey' => qq{
 		insert into Persess_View_Count
-		(session_id, view_scope, view_key, view_caption, count, view_init, view_latest)
+		(session_id, person_id, view_scope, view_key, view_count, view_caption, view_arl, view_init, view_latest)
 		values
-		(?         , ?         , ?       , ?           , 1    , sysdate  , sysdate)
+		(?         , ?         , ?        , ?       , 1         , ?           , ?       , sysdate  , sysdate)
 	},
-	
-	
+
+	'person.mySessionViewCount' => {
+		sqlStmt => qq{
+			select view_caption, view_count, %simpleStamp:view_init%,	%simpleStamp:view_latest%
+			from Persess_View_Count 
+			where person_id = ?
+				and view_init > trunc(sysdate)
+			order by view_count DESC
+		},
+		sqlStmtBindParamDescr => ['Person ID for the perSess_View_Count table '],
+		
+		publishDefn => {
+			columnDefn => [
+				{ head=> 'Target' },
+				{ head=> 'Count', dAlign => 'right' },
+				{ head=> 'Init View' },
+				{ head=> 'Last View' },
+			],
+			#bullets => 'stpe-#my.stmtId#/dlg-update-attr-#0#/#1#?home=/#param.arl#',
+		},
+		publishDefn_panel =>
+		{
+			# automatically inherits columnDefn and other items from publishDefn
+			style => 'panel.static',
+			flags => 0,
+			frame => { heading => 'My Session View Count' },
+		},
+		publishDefn_panelTransp =>
+		{
+			# automatically inherits columnDefn and other items from publishDefn
+			style => 'panel.transparent.static',
+			inherit => 'panel',
+		},
+
+		publishComp_st => sub { my ($page, $flags, $personId) = @_; $personId ||= $page->session('user_id'); $STMTMGR_PAGE->createHtml($page, $flags, 'person.mySessionViewCount', [$personId] ); },
+		publishComp_stp => sub { my ($page, $flags, $personId) = @_; $personId ||= $page->session('user_id'); $STMTMGR_PAGE->createHtml($page, $flags, 'person.mySessionViewCount', [$personId], 'panel'); },
+		publishComp_stpe => sub { my ($page, $flags, $personId) = @_; $personId ||= $page->session('user_id'); $STMTMGR_PAGE->createHtml($page, $flags, 'person.mySessionViewCount', [$personId], 'panelEdit'); },
+		publishComp_stpt => sub { my ($page, $flags, $personId) = @_; $personId ||= $page->session('user_id'); $STMTMGR_PAGE->createHtml($page, $flags, 'person.mySessionViewCount', [$personId], 'panelTransp'); },
+	},
 );
 	
 1;
