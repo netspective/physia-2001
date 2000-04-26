84e3aae4afa3ae526b9d2249a99be1aa0aa5234b
alex_hillman <alex_hillman>
2000-04-26 00:02:15 +0000

    Changes - added second menbers for log groups, size of file for TS_INDEXES

--- HealthSuite/Database/dbserver/create_db.sql
+++ HealthSuite/Database/dbserver/create_db.sql
@@ -7,38 +7,52 @@ CREATE DATABASE "SDEDBS02"
    maxlogfiles 32
    character set US7ASCII
    national character set US7ASCII
-DATAFILE '/u02/oradata/sdedbs02/system01.dbf' SIZE 175M
-logfile '/u03/oradata/sdedbs02/redo01_a.log' SIZE 10M, 
-    '/u03/oradata/sdedbs02/redo02_a.log' SIZE 10M,
-    '/u03/oradata/sdedbs02/redo03_a.log' size 10M;
+DATAFILE '/u02/oradata/SDEDBS02/SDEDBS02_system01.dbf' SIZE 175M
+logfile group 1 ('/u03/oradata/SDEDBS02/SDEDBS02_redo01_a.log',
+                 '/home/u01/app/oradata/SDEDBS02/SDEDBS02_redo01_b.log') size 10M,
+group 2 ('/u03/oradata/SDEDBS02/SDEDBS02_redo02_a.log',
+                 '/home/u01/app/oradata/SDEDBS02/SDEDBS02_redo02_b.log') size 10M,
+group 3 ('/u03/oradata/SDEDBS02/SDEDBS02_redo03_a.log',
+                 '/home/u01/app/oradata/SDEDBS02/SDEDBS02_redo03_b.log') size 10M;
 
 CREATE ROLLBACK SEGMENT r0 TABLESPACE SYSTEM
 STORAGE (INITIAL 16K NEXT 16K MINEXTENTS 2 MAXEXTENTS 20);
 ALTER ROLLBACK SEGMENT r0 ONLINE;
 
 REM ************** TABLESPACE FOR ROLLBACK *****************
-CREATE TABLESPACE RBS DATAFILE '/u04/oradata/sdedbs02/rbs01.dbf' SIZE 300M 
+CREATE TABLESPACE RBS DATAFILE '/u04/oradata/SDEDBS02/SDEDBS02_rbs01.dbf' SIZE 500M 
 DEFAULT STORAGE ( INITIAL 1M NEXT 1M  MINEXTENTS 20 MAXEXTENTS 1024 PCTINCREASE 0);
 
 REM ************** TABLESPACE FOR TEMPORARY *****************
-CREATE TABLESPACE TEMP DATAFILE '/u05/oradata/sdedbs02/temp01.dbf' SIZE 300M 
+CREATE TABLESPACE TEMP DATAFILE '/u05/oradata/SDEDBS02/SDEDBS02_temp01.dbf' SIZE 300M 
 DEFAULT STORAGE ( INITIAL 2M NEXT 2M MINEXTENTS 1 MAXEXTENTS UNLIMITED PCTINCREASE 0) TEMPORARY;
 
-create tablespace USERS datafile '/u06/oradata/sdedbs02/ts_users.dbf'
+create tablespace USERS datafile '/u06/oradata/SDEDBS02/SDEDBS02_ts_users.dbf'
 size 10M default storage (pctincrease 0);
 
-create tablespace TOOLS datafile '/u06/oradata/sdedbs02/ts_tools.dbf'
+create tablespace TOOLS datafile '/u06/oradata/SDEDBS02/SDEDBS02_ts_tools.dbf'
 size 10M default storage (pctincrease 0);
+
+create tablespace TS_DATA datafile '/u06/oradata/SDEDBS02/SDEDBS02_ts_data_1.dbf'
+size 500M minimum extent 1M default storage (initial 1M next 1M maxextents 1024 pctincrease 0);
+
+alter tablespace TS_DATA add datafile '/u06/oradata/SDEDBS02/SDEDBS02_ts_data_2.dbf' size 500M;
+
+create tablespace TS_INDEXES datafile '/u07/oradata/SDEDBS02/SDEDBS02_ts_indexes_1.dbf'
+size 1500M minimum extent 1M default storage (initial 1M next 1M maxextents 1024 pctincrease 0);
+
 
 REM **** Creating four rollback segments ****************
 CREATE ROLLBACK SEGMENT r01 TABLESPACE RBS;
 CREATE ROLLBACK SEGMENT r02 TABLESPACE RBS;
 CREATE ROLLBACK SEGMENT r03 TABLESPACE RBS;
 CREATE ROLLBACK SEGMENT r04 TABLESPACE RBS;
+
 ALTER ROLLBACK SEGMENT r01 ONLINE;
 ALTER ROLLBACK SEGMENT r02 ONLINE;
 ALTER ROLLBACK SEGMENT r03 ONLINE;
 ALTER ROLLBACK SEGMENT r04 ONLINE;
+
 ALTER ROLLBACK SEGMENT r0 OFFLINE;
 
 DROP ROLLBACK SEGMENT r0;
@@ -51,12 +65,17 @@ alter user system default tablespace TOOLS temporary tablespace TEMP;
 @$ORACLE_HOME/rdbms/admin/catalog.sql
 @$ORACLE_HOME/rdbms/admin/catproc.sql
 @$ORACLE_HOME/rdbms/admin/catblock.sql
-@$ORACLE_HOME/rdbms/admin/caths.sql
-@$ORACLE_HOME/rdbms/admin/otrcsvr.sql
-@$ORACLE_HOME/rdbms/admin/utlsampl.sql
+@$ORACLE_HOME/rdbms/admin/catparr.sql
+
+@$ORACLE_HOME/rdbms/admin/dbmspool.sql
+@$ORACLE_HOME/rdbms/admin/prvtpool.plb
 
 connect system/manager
 @$ORACLE_HOME/sqlplus/admin/pupbld.sql
+
+alter user system identified by phtem;
+alter user sys identified by phsys;
+
 
 spool off
 
