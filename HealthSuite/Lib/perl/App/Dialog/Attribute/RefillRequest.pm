8f51d94bc1b38b300f5dfa7883e39a5425a83576
moorthy_medavarapu <moorthy_medavarapu>
2000-05-07 15:05:53 +0000

    [B39] Added a refill request dialog.

---
+++ HealthSuite/Lib/perl/App/Dialog/Attribute/RefillRequest.pm
@@ -0,0 +1,82 @@
+##############################################################################
+package App::Dialog::Attribute::RefillRequest;
+##############################################################################
+
+use DBI::StatementManager;
+use App::Statements::Person;
+use App::Universal;
+use strict;
+use Carp;
+use CGI::Dialog;
+use CGI::Validator::Field;
+use App::Dialog::Field::Attribute;
+use App::Universal;
+use Date::Manip;
+use Devel::ChangeLog;
+use vars qw(@ISA @CHANGELOG);
+@ISA = qw(CGI::Dialog);
+
+sub new
+{
+	my ($self, $command) = CGI::Dialog::new(@_, id => 'refillrequest', heading => '$Command Refill Request');
+	my $schema = $self->{schema};
+
+	delete $self->{schema};  # make sure we don't store this!
+	croak 'schema parameter required' unless $schema;
+
+	$self->addContent(
+		new CGI::Dialog::Field(name => 'value_text', caption => 'Refill', type => 'memo', options => FLDFLAG_REQUIRED),
+		new CGI::Dialog::Field(name => 'value_date', caption => 'Date', type => 'date'),
+		new App::Dialog::Field::Person::ID(caption =>'Approved By', name => 'value_textb', hints => 'Physician approving the refill.'),	
+	);
+	
+	$self->{activityLog} =
+	{
+		level => 1,
+		scope =>'person_attribute',
+		key => "#param.person_id#",
+		data => "Refill Request for <a href='/person/#param.person_id#/profile'>#param.person_id#</a>"
+	};
+
+	$self->addFooter(new CGI::Dialog::Buttons(cancelUrl => $self->{cancelUrl} || undef));
+
+	return $self;
+}
+
+sub populateData
+{
+	my ($self, $page, $command, $activeExecMode, $flags) = @_;
+
+	return unless $flags & CGI::Dialog::DLGFLAG_UPDORREMOVE_DATAENTRY_INITIAL;
+	
+	my $itemId = $page->param('item_id');
+	my $data = $STMTMGR_PERSON->createFieldsFromSingleRow($page, STMTMGRFLAG_NONE, 'selAttributeById', $itemId);
+	
+}
+
+sub execute
+{
+	my ($self, $page, $command,$flags) = @_;
+	
+	
+	$page->schemaAction(
+		'Person_Attribute', $command,
+		parent_id => $page->param('person_id') || undef,
+		item_id => $page->param('item_id') || undef,
+		item_name =>'Refill Request',
+		value_type => 0,
+		value_text => $page->field('value_text') || undef,	
+		value_date => $page->field('value_date') || undef,
+		value_textB => $page->field('value_textb') || undef,		
+		_debug => 0
+	);
+	return "\u$command completed.";
+}
+
+use constant PANEDIALOG_ATTENDANCE => 'Dialog/Pane/Refill Request';
+
+@CHANGELOG =
+(
+);
+
+1;
