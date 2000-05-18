feeb158f8adc36e0df5833c62dffed1db90e1486
munir_faridi <munir_faridi>
2000-05-18 20:24:38 +0000

    [B336] Modified statement to return item information

--- HealthSuite/Lib/perl/App/Statements/Invoice.pm
+++ HealthSuite/Lib/perl/App/Statements/Invoice.pm
@@ -157,7 +157,7 @@ $STMTMGR_INVOICE = new App::Statements::Invoice(
 	'selInvoiceProcedureItems' => qq{
 		select parent_id, item_id, item_type, hcfa_service_place, hcfa_service_type, emergency, comments, caption, code, modifier,
 			unit_cost, quantity, rel_diags, data_num_c, to_char(service_begin_date, '$SQLSTMT_DEFAULTDATEFORMAT') as service_begin_date,
-			to_char(service_end_date, '$SQLSTMT_DEFAULTDATEFORMAT') as service_end_date, data_text_a
+			to_char(service_end_date, '$SQLSTMT_DEFAULTDATEFORMAT') as service_end_date, data_text_a, balance, total_adjust, extended_cost
 		from invoice_item
 		where parent_id = ?
 			and item_type in (?,?)
