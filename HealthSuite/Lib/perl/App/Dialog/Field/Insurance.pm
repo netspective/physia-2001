dd8155b8b103ad0847af1b3b95a12d7cbb257520
radha_kotagiri <radha_kotagiri>
2000-05-19 00:18:35 +0000

    [B345] Added Field::Insurance::Product And Field::Insurance::Plan

--- HealthSuite/Lib/perl/App/Dialog/Field/Insurance.pm
+++ HealthSuite/Lib/perl/App/Dialog/Field/Insurance.pm
@@ -1,5 +1,5 @@
 ##############################################################################
-package App::Dialog::Field::Insurance::ID::New;
+package App::Dialog::Field::Insurance::Product::New;
 ##############################################################################
 
 use strict;
@@ -25,7 +25,7 @@ sub new
 	# to restrict/expand the selection
 	#
 
-	$params{name} = 'ins_id' unless $params{name};
+	$params{name} = 'product_name' unless $params{name};
 
 	$params{options} = 0 unless exists $params{options};
 	$params{options} |= FLDFLAG_IDENTIFIER;
@@ -48,13 +48,16 @@ sub isValid
 	if($self->SUPER::isValid($page, $validator))
 	{
 		my $value = $page->field($self->{name});
-		my $personId = $page->param('person_id');
-		my $recordType = App::Universal::RECORDTYPE_PERSONALCOVERAGE;
+		my $ownerId = $page->param('person_id');
+		#my $recordType = App::Universal::RECORDTYPE_PERSONALCOVERAGE;
+		my $orgId = $page->field('ins_org_id');
+		my $planName = $page->field('plan_name');
+		#my $personPlanExists = $STMTMGR_INSURANCE->getSingleValue($page,STMTMGRFLAG_NONE,'selPersonPlanExists',$value, $planName, $recordType, $ownerId);
+		my $newProductExists = $STMTMGR_INSURANCE->getSingleValue($page,STMTMGRFLAG_NONE,'selNewProductExists',$value, $orgId);
 
-		my $newPersonPlanExists = $STMTMGR_INSURANCE->getSingleValue($page,STMTMGRFLAG_NONE,'selPersonPlanExists',$value, $recordType, $personId);
-		my $newPlanExists = $STMTMGR_INSURANCE->getSingleValue($page,STMTMGRFLAG_NONE,'selNewPlanExists',$value);
 
-		$self->invalidate($page, "Plan ID '$value' already exists.") if $newPlanExists ne '' || $newPersonPlanExists ne '';
+		$self->invalidate($page, "Product Name '$value' already exists.") if $newProductExists ne '';
+		#$self->invalidate($page, "This Personal Coverage already exists for '$ownerId'.") if  $personPlanExists ne '';
 	}
 
 	# return TRUE if there were no errors, FALSE (0) if there were errors
@@ -62,7 +65,7 @@ sub isValid
 }
 
 ##############################################################################
-package App::Dialog::Field::Insurance::ID;
+package App::Dialog::Field::Insurance::Product;
 ##############################################################################
 
 use strict;
@@ -91,7 +94,7 @@ sub new
 	#
 
 	$params{idEntryStyle} = IDENTRYSTYLE_TEXT unless exists $params{idEntryStyle};
-	$params{name} = 'ins_id' unless $params{name};
+	$params{name} = 'product_name' unless $params{name};
 
 	$params{options} = 0 unless exists $params{options};
 	$params{options} |= FLDFLAG_IDENTIFIER;
@@ -100,7 +103,7 @@ sub new
 	{
 		$params{type} = 'foreignKey';
 		$params{fKeyTable} = 'INSURANCE outer';
-		$params{fKeySelCols} = 'INS_ID, GROUP_NAME, ct.CAPTION';
+		$params{fKeySelCols} = 'PRODUCT_NAME, GROUP_NAME, ct.CAPTION';
 		$params{fKeyValueCol} = 0;
 		$params{fKeyDisplayCol} = 1;
 		#$params{fKeyOrderBy} = 'name_last, name_first';
@@ -109,14 +112,14 @@ sub new
 			$params{types} || $params{notTypes} ?
 				Schema::Utilities::createInclusionExclusionConds('ct.CAPTION', $params{types}, $params{notTypes}, 1) :
 				'';
-		$params{fKeyWhere} = "exists (select 1 from INSURANCE inner, CLAIM_TYPE ct where outer.INS_ID = inner.INS_ID and inner.INS_TYPE = ct.ID and $typeCond)" if $typeCond;
+		$params{fKeyWhere} = "exists (select 1 from INSURANCE inner, CLAIM_TYPE ct where outer.PRODUCT_NAME = inner.PRODUCT_NAME and inner.INS_TYPE = ct.ID and $typeCond)" if $typeCond;
 	}
 	else
 	{
 		$params{type} = 'text';
 		$params{size} = 16;
 		$params{maxLength} = 32;
-		$params{findPopup} = '/lookup/insurance/id';
+		$params{findPopup} = '/lookup/insurance/product_name';
 	}
 	return CGI::Dialog::Field::new($type, %params);
 }
@@ -134,47 +137,111 @@ sub isValid
 
 	my $personId = $page->param('person_id');
 	my $recordTypeUnique = App::Universal::RECORDTYPE_PERSONALCOVERAGE;
+
 	my $insTypeWrkCmp = App::Universal::CLAIMTYPE_WORKERSCOMP;
+	my $orgId = $page->field('ins_org_id');
+	my $planName = $page->field('plan_name');
+	my $productName = $page->field('product_name');
+	my $preFilledOrg = $page->field('ins_comp');
+	my $preFilledProduct = $page->field('product');
+	my $doesProductExist = $STMTMGR_INSURANCE->getSingleValue($page,STMTMGRFLAG_NONE,'selDoesProductExists',$productName, $orgId);
+	my $doesPreFilledProductExist = $STMTMGR_INSURANCE->getSingleValue($page,STMTMGRFLAG_NONE,'selDoesProductExists',$preFilledProduct, $preFilledOrg);
+	#my $planForOrgExists = $STMTMGR_INSURANCE->getSingleValue($page,STMTMGRFLAG_NONE,'selNewPlanExists',$productName, $planName, $orgId);
 
-	my $doesPlanExist = $STMTMGR_INSURANCE->getSingleValue($page,STMTMGRFLAG_NONE,'selDoesPlanExists',$value);
-
-	my $planForPersonExists = $STMTMGR_INSURANCE->getSingleValue($page,STMTMGRFLAG_NONE,'selDoesPlanExistsForPerson',$value, $personId);
-
-	my $isPlanUnique = $STMTMGR_INSURANCE->getSingleValue($page,STMTMGRFLAG_NONE,'selIsPlanUnique',$value, $recordTypeUnique);
+	my $isPlanUnique = $STMTMGR_INSURANCE->getSingleValue($page,STMTMGRFLAG_NONE,'selIsPlanUnique',$productName, $recordTypeUnique);
 
 	my $isPlanWorkComp = $STMTMGR_INSURANCE->getSingleValue($page,STMTMGRFLAG_NONE,'selIsPlanWorkComp',$value, $insTypeWrkCmp);
 
-	my $createInsPlanHref = "javascript:doActionPopup('/org-p/#session.org_id#/dlg-add-ins-newplan/$value');";
-	$self->invalidate($page, qq{$self->{caption} '$value' does not exist.<br><img src="/resources/icons/arrow_right_red.gif">
-			<a href="$createInsPlanHref">Create Plan '$value' now</a>
-		}) if $doesPlanExist eq '';
+	my $createInsProductHref = "javascript:doActionPopup('/org-p/$orgId/dlg-add-ins-product?_f_ins_org_id=$orgId&_f_product_name=$value');";
+	$self->invalidate($page, qq{$self->{caption} '$productName' does not exist.<br><img src="/resources/icons/arrow_right_red.gif">
+			<a href="$createInsProductHref">Create Product '$productName' now</a>
+		}) if $doesProductExist eq '' && $productName ne '';
 
-	$self->invalidate($page, "$self->{caption} '$value' already exists for '$personId'.") if $planForPersonExists ne '';
+	my $createInsProductPreHref = "javascript:doActionPopup('/org-p/$preFilledOrg/dlg-add-ins-product?_f_ins_org_id=$preFilledOrg&_f_product_name=$preFilledProduct');";
+	$self->invalidate($page, qq{$self->{caption} '$preFilledProduct' does not exist.<br><img src="/resources/icons/arrow_right_red.gif">
+			<a href="$createInsProductPreHref">Create Product '$preFilledProduct' now</a>
+		}) if $doesPreFilledProductExist eq '' &&  $preFilledProduct ne '';
 
-	$self->invalidate($page, "$self->{caption} '$value' is a unique plan.") if $isPlanUnique ne '';
-
-	$self->invalidate($page, "$self->{caption} '$value' is a workers compensation plan.") if $isPlanWorkComp ne '';
 
 	# return TRUE if there were no errors, FALSE (0) if there were errors
 	return $page->haveValidationErrors() ? 0 : 1;
 }
 
-@CHANGELOG =
-(
-	[	CHANGELOGFLAG_SDE | CHANGELOGFLAG_NOTE, '01/06/1999', 'MAF',
-		'Dialog/Insurance ID Field',
-		"Fixed the link for 'Create Plan now' for when the ins_id does not exist."],
-);
 
 ##############################################################################
-package App::Dialog::Field::Insurance::WorkersComp::ID;
+package App::Dialog::Field::Insurance::Plan::New;
+##############################################################################
+
+use strict;
+use DBI::StatementManager;
+use App::Statements::Insurance;
+use Carp;
+use CGI::Validator::Field;
+use CGI::Dialog;
+use App::Universal;
+use Schema::Utilities;
+use Devel::ChangeLog;
+
+use vars qw(@ISA @CHANGELOG);
+
+@ISA = qw(CGI::Dialog::Field);
+
+sub new
+{
+	my ($type, %params) = @_;
+
+	#
+	# you can pass in a "types => ['x', 'y']" and "notTypes => ['a']"
+	# to restrict/expand the selection
+	#
+
+	$params{name} = 'plan_name' unless $params{name};
+
+	$params{options} = 0 unless exists $params{options};
+	$params{options} |= FLDFLAG_IDENTIFIER;
+
+	$params{type} = 'text';
+	$params{size} = 16;
+	$params{maxLength} = 32;
+
+	return CGI::Dialog::Field::new($type, %params);
+}
+
+sub isValid
+{
+	my ($self, $page, $validator) = @_;
+
+	my $command = $page->property(CGI::Dialog::PAGEPROPNAME_COMMAND . '_' . $validator->id());
+
+	return () if $command ne 'add';
+
+	if($self->SUPER::isValid($page, $validator))
+	{
+		my $value = $page->field($self->{name});
+
+		my $orgId = $page->field('ins_org_id');
+		my $productName = $page->field('product_name');
+		my $planName = $page->field('plan_name');
+		my $orgPlanExists = $STMTMGR_INSURANCE->getSingleValue($page,STMTMGRFLAG_NONE,'selNewPlanExists',$productName, $planName, $orgId);
+
+
+		$self->invalidate($page, "Plan Name '$value' already exists.") if $orgPlanExists ne '';
+		#$self->invalidate($page, "This Personal Coverage already exists for '$ownerId'.") if  $personPlanExists ne '';
+	}
+
+	# return TRUE if there were no errors, FALSE (0) if there were errors
+	return $page->haveValidationErrors() ? 0 : 1;
+}
+
+##############################################################################
+package App::Dialog::Field::Insurance::Plan;
 ##############################################################################
 
 use strict;
 use Carp;
 use CGI::Validator::Field;
 use DBI::StatementManager;
-#use App::Statements::Org;
+use App::Statements::Org;
 use App::Statements::Insurance;
 use CGI::Dialog;
 use Schema::Utilities;
@@ -196,7 +263,7 @@ sub new
 	#
 
 	$params{idEntryStyle} = IDENTRYSTYLE_TEXT unless exists $params{idEntryStyle};
-	$params{name} = 'ins_id' unless $params{name};
+	$params{name} = 'plan_name' unless $params{name};
 
 	$params{options} = 0 unless exists $params{options};
 	$params{options} |= FLDFLAG_IDENTIFIER;
@@ -205,7 +272,7 @@ sub new
 	{
 		$params{type} = 'foreignKey';
 		$params{fKeyTable} = 'INSURANCE outer';
-		$params{fKeySelCols} = 'INS_ID, GROUP_NAME, ct.CAPTION';
+		$params{fKeySelCols} = 'PRODUCT_NAME, GROUP_NAME, ct.CAPTION';
 		$params{fKeyValueCol} = 0;
 		$params{fKeyDisplayCol} = 1;
 		#$params{fKeyOrderBy} = 'name_last, name_first';
@@ -214,14 +281,14 @@ sub new
 			$params{types} || $params{notTypes} ?
 				Schema::Utilities::createInclusionExclusionConds('ct.CAPTION', $params{types}, $params{notTypes}, 1) :
 				'';
-		$params{fKeyWhere} = "exists (select 1 from INSURANCE inner, CLAIM_TYPE ct where outer.INS_ID = inner.INS_ID and inner.INS_TYPE = ct.ID and $typeCond)" if $typeCond;
+		$params{fKeyWhere} = "exists (select 1 from INSURANCE inner, CLAIM_TYPE ct where outer.PRODUCT_NAME = inner.PRODUCT_NAME and inner.INS_TYPE = ct.ID and $typeCond)" if $typeCond;
 	}
 	else
 	{
 		$params{type} = 'text';
 		$params{size} = 16;
 		$params{maxLength} = 32;
-		$params{findPopup} = '/lookup/insurance/id';
+		$params{findPopup} = '/lookup/insurance/plan_name';
 	}
 	return CGI::Dialog::Field::new($type, %params);
 }
@@ -237,64 +304,29 @@ sub isValid
 
 	return 0 unless $self->SUPER::isValid($page, $validator);
 
-	my $createWrkCompHref = "javascript:doActionPopup('/org-p/#session.org_id#/dlg-add-ins-workerscomp/$value');";
-	if(my $orgId = $page->param('org_id'))
-	{
-		my $items = $STMTMGR_INSURANCE->getRowAsHash($page, STMTMGRFLAG_NONE, 'selGroupInsurance', $value);
-		my $claimType = App::Universal::CLAIMTYPE_WORKERSCOMP;
+	my $personId = $page->param('person_id');
 
-		if($items->{ins_internal_id} eq '')
-		{
-			$self->invalidate($page, qq{$self->{caption} '$value' does not exist.<br><img src="/resources/icons/arrow_right_red.gif">
-					<a href="$createWrkCompHref">Create Workers Comp Plan '$value' now</a>
-				});
-		}
-		elsif($items->{ins_type} != $claimType)
-		{
-			$self->invalidate($page, "ID '$value' is not a Workers Compensation Plan.");
-		}
-		elsif($STMTMGR_INSURANCE->recordExists($page,STMTMGRFLAG_NONE, 'selSpecificWrkCmpAttr', $orgId, $items->{ins_internal_id}, App::Universal::ATTRTYPE_INSGRPWORKCOMP))
-		{
-			$self->invalidate($page, "$self->{caption} '$value' already exists for '$orgId'.");
+	my $insTypeWrkCmp = App::Universal::CLAIMTYPE_WORKERSCOMP;
+	my $orgId = $page->field('ins_org_id');
+	my $planName = $page->field('plan_name');
+	my $productName = $page->field('product_name');
+	my $preFilledOrg = $page->field('ins_comp');
+	my $preFilledProduct = $page->field('product');
+	my $preFilledPlan =  $page->field('plan');
+	my $planForOrgExists = $STMTMGR_INSURANCE->getSingleValue($page,STMTMGRFLAG_NONE,'selNewPlanExists',$productName, $planName, $orgId);
+	my $preFilledplanExists = $STMTMGR_INSURANCE->getSingleValue($page,STMTMGRFLAG_NONE,'selNewPlanExists',$preFilledProduct, $preFilledPlan, $preFilledOrg);
 
-		}
-	}
-	else
-	{
-		my $personId = $page->param('person_id');
-		my $insTypeWrkCmp = App::Universal::CLAIMTYPE_WORKERSCOMP;
+	my $createInsPlanPreHref = "javascript:doActionPopup('/org-p/$orgId/dlg-add-ins-plan?_f_ins_org_id=$orgId&_f_product_name=$productName&_f_plan_name=$planName');";
+	$self->invalidate($page, qq{ Plan Name '$planName' does not exist.<br><img src="/resources/icons/arrow_right_red.gif">
+			<a href="$createInsPlanPreHref">Create Plan '$planName' now</a>
+		}) if $planForOrgExists eq '' && $planName ne '';
 
-		#CHECK TO SEE IF PLAN EVEN EXISTS
-		my $doesPlanExist = $STMTMGR_INSURANCE->getSingleValue($page,STMTMGRFLAG_NONE,'selDoesPlanExists',$value);
-
-		$self->invalidate($page, qq{$self->{caption} '$value' does not exist.<br><img src="/resources/icons/arrow_right_red.gif">
-				<a href="$createWrkCompHref">Create Workers Comp Plan '$value' now</a>
-			}) if $doesPlanExist eq '';
-
-
-		if($doesPlanExist ne '')
-		{
-			#CHECK TO SEE IF WORK COMP PLAN ALREADY EXISTS FOR THIS PERSON
-			my $workCompExists = $STMTMGR_INSURANCE->getSingleValue($page,STMTMGRFLAG_NONE,'selPatientHasPlan', $value, $personId, $insTypeWrkCmp);
-
-			$self->invalidate($page, "$self->{caption} '$value' already exists for '$personId'.") if $workCompExists ne '';
-
-			#CHECK TO SEE IF IT IS A VALID WORK COMP PLAN
-			my $isPlanWorkComp = $STMTMGR_INSURANCE->getSingleValue($page,STMTMGRFLAG_NONE,'selIsPlanWorkComp',$value, $insTypeWrkCmp);
-
-			$self->invalidate($page, "ID '$value' is not a workers compensation plan.") if $isPlanWorkComp eq '';
-		}
-	}
-
-	# return TRUE if there were no errors, FALSE (0) if there were errors
+	my $createPreInsPlanPreHref = "javascript:doActionPopup('/org-p/$preFilledOrg/dlg-add-ins-plan?_f_ins_org_id=$preFilledOrg&_f_product_name=$preFilledProduct&_f_plan_name=$preFilledPlan');";
+	$self->invalidate($page, qq{ Plan Name '$preFilledPlan' does not exist.<br><img src="/resources/icons/arrow_right_red.gif">
+			<a href="$createPreInsPlanPreHref">Create Plan '$preFilledPlan' now</a>
+		}) if $preFilledplanExists eq '' && $preFilledPlan ne '';
 	return $page->haveValidationErrors() ? 0 : 1;
 }
 
-@CHANGELOG =
-(
-	[	CHANGELOGFLAG_SDE | CHANGELOGFLAG_NOTE, '12/29/1999', 'MAF',
-		'Dialog/Lookups',
-		'Fixed create link and lookup for workers comp.'],
-);
 
 1;
