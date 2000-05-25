833763f49d19faa9fea191a6549eb70b1132e778
robert_jenks <robert_jenks>
2000-05-25 17:03:53 +0000

    Changes to handle Apache per-child initialization of DB connection and News articles

--- HealthSuite/Conf/apache/startup.pl
+++ HealthSuite/Conf/apache/startup.pl
@@ -9,32 +9,6 @@ use strict;
 use CGI;
 use CGI::Page;
 use Apache::HealthSuite::PracticeManagement;
-
-print "Caching iSyndicate News...";
-use App::Component::News;
-eval{
-	my $news;
-	$news = new App::Component::News(
-			id => 'news-top',
-			heading => 'Top News',
-			source => 'topnews',
-		);
-	$news->getNews();
-	$news = new App::Component::News(
-			id => 'news-health',
-			heading => 'Health News',
-			source => 'healthnews',
-		);
-	$news->getNews();
-};
-print "done\n";
-
-print "Connecting to Database...";
-{
-	my $page = new CGI::Page;
-	$page = undef;
-}
-print "done\n";
 print "Leaving startup.pl\n";
 
 1;
--- HealthSuite/Lib/perl/Apache/HealthSuite/PracticeManagement.pm
+++ HealthSuite/Lib/perl/Apache/HealthSuite/PracticeManagement.pm
@@ -1,4 +1,6 @@
-package Apache::HealthSuite::PracticeManagement;
+##############################################################################
+package Apache::HealthSuite::PracticeManagement::PerlHandler;
+##############################################################################
 
 use strict;
 use Apache;
@@ -23,10 +25,41 @@ sub handler
 	if ($@ && $DEBUG)
 	{
 		$r->content_type('text/html');
-    	$r->send_http_header();
-    	$r->print("<h1>Perl Runtime Errors:</h1><font color=red>$@</font>");
+    		$r->send_http_header();
+    		$r->print("<h1>Perl Runtime Errors:</h1><font color=red>$@</font>");
 	}
 }
+
+
+##############################################################################
+package Apache::HealthSuite::PracticeManagement::PerlChildInitHandler;
+##############################################################################
+
+use strict;
+use App::Component::News;
+use CGI::Page;
+
+sub handler
+{
+	# Pre-fetch iSyndicate news articles
+	my $news;
+	$news = new App::Component::News(
+		id => 'news-top',
+		heading => 'Top News',
+		source => 'topnews',
+	);
+	$news->getNews();
+	$news = new App::Component::News(
+		id => 'news-health',
+		heading => 'Health News',
+		source => 'healthnews',
+	);
+	$news->getNews();
+
+	# Pre-connect to the database
+	my $page = new CGI::Page;
+}
+
 
 1;
 
