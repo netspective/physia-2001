1be24940841b84ddfd2ad1dc78e04460f10cc2d6
radha_kotagiri <radha_kotagiri>
2000-05-26 17:48:43 +0000

    [B] Fixed the code for FeeSchedule due to the change in the 'Offering Catalog' table in the database

--- HealthSuite/Lib/perl/App/Data/MDL/FeeSchedule.pm
+++ HealthSuite/Lib/perl/App/Data/MDL/FeeSchedule.pm
@@ -19,15 +19,15 @@ sub new
 
 sub importEntry
 {
-	my ($self, $flags, $catalog, $entryList, $parentEntry) = @_;	
+	my ($self, $flags, $catalog, $entryList, $internalId, $parentEntry) = @_;
 
-	$entryList = [$entryList] if ref $entryList eq 'HASH';	
+	$entryList = [$entryList] if ref $entryList eq 'HASH';
 	foreach my $entry (@$entryList)
 	{
 		#my $dv = new Dumpvalue;
 		#$dv->dumpValue($parentEntry);
 		my $parentEntry = $self->schemaAction($flags, "Offering_Catalog_Entry", 'add',
-					catalog_id =>  $catalog->{id},
+					catalog_id =>  $internalId,
 					entry_type => $self->translateEnum($flags, "Catalog_Entry_Type", $entry->{type}),
 					status => $self->translateEnum($flags, "Catalog_Entry_Status", $entry->{status}),
 					unit_cost => $entry->{price},
@@ -35,15 +35,15 @@ sub importEntry
 					code => $entry->{'cat-code'},
 					parent_entry_id => $parentEntry || undef,
 					modifier =>  $entry->{modifier},
-					description => $entry->{description});					
-		
+					description => $entry->{description});
+
 		my $entryData = [$entry] if ref $entry eq 'HASH';
-		
+
 		foreach my $item (@$entryData)
-		{					
+		{
 			if(my $entry = $item->{'schedule-entry'})
-			{				
-				$self->importEntry($flags, $catalog, $entry, $parentEntry);
+			{
+				$self->importEntry($flags, $catalog, $entry, $internalId, $parentEntry);
 
 			}
 
@@ -57,30 +57,32 @@ sub importCatalog
 	my ($self, $flags, $catalog, $parentCatalog) = @_;
 
 	#my $dv = new Dumpvalue;
-	#$dv->dumpValue($catalog);	
-	$self->schemaAction($flags|MDLFLAG_LOGACTIVITY, 'Offering_Catalog', 'add',
-			catalog_id => $catalog->{id},
-			catalog_type =>  $self->translateEnum($flags,"Offering_Catalog_Type", $catalog->{type} || 'Fee Schedule'),
-			caption => $catalog->{'fee-caption'},
-			parent_catalog_id => defined $parentCatalog ? $parentCatalog->{id} : undef,
-			description => $catalog->{description});
-			
+	#$dv->dumpValue($catalog);
+	#my $catId = $catalog->{id};
+	my $internalId = $self->schemaAction($flags, 'Offering_Catalog', 'add',
+				catalog_id => $catalog->{'id'} || undef,
+				catalog_type =>  $self->translateEnum($flags,"Offering_Catalog_Type", $catalog->{type} || 'Fee Schedule') || undef,
+				caption => $catalog->{'fee-caption'} || undef,
+				parent_catalog_id => $parentCatalog || undef,
+				description => $catalog->{description} || undef,
+				rvrbs_multiplier => $catalog->{multiplier} || undef);
+
 	if (my $childEntry = $catalog->{'schedule-entry'} )
 	{
-		$self->importEntry($flags, $catalog, $childEntry);
+		$self->importEntry($flags, $catalog, $childEntry, $internalId);
 	}
 
-	my $catalogData = [$catalog] if ref $catalog eq 'HASH';	
+	my $catalogData = [$catalog] if ref $catalog eq 'HASH';
 	foreach my $item (@$catalogData)
-	{	
-		
+	{
+
 		if(my $childSchedule = $item->{'fee-schedule'})
-		{				
-			$self->importCatalog($flags, $childSchedule, $item);
-			
+		{
+			$self->importCatalog($flags, $childSchedule, $internalId);
+
 		}
 		#elsif(my $childEntry = $item->{feeentry})
-		#{			
+		#{
 		#	$self->importEntry($flags, $item, $childEntry);
 		#}
 	}
@@ -94,10 +96,10 @@ sub importStruct
 
 	$list = [$list] if ref $list eq 'HASH';
 	foreach my $item (@$list)
-	{			
+	{
 		if(my $childSchedule = $item->{'fee-schedule'})
 		{
-			$self->importCatalog($flags, $childSchedule);			
+			$self->importCatalog($flags, $childSchedule);
 		}
 	}
 }
