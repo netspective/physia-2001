d0c1d27db391076d4136a9391dc546f13b0ad999
radha_kotagiri <radha_kotagiri>
2000-06-02 23:11:32 +0000

    [B429] Added a new file

---
+++ HealthSuite/Lib/perl/App/Dialog/Transaction/ReferralWorkFlow.pm
@@ -0,0 +1,36 @@
+##############################################################################
+package App::Dialog::Transaction::ReferralWorkFlow;
+##############################################################################
+
+use strict;
+use Carp;
+use CGI::Dialog;
+use CGI::Validator::Field;
+use App::Universal;
+use vars qw(@ISA);
+
+use DBI::StatementManager;
+use App::Statements::Person;
+use App::Statements::Org;
+
+@ISA = qw(CGI::Dialog);
+
+sub initialize
+{
+	#my $self = CGI::Dialog::new(@_, id => 'referral', heading => 'Add Referral');
+	my $self = shift;
+
+	$self->{activityLog} =
+	{
+			level => 1,
+			scope =>'transaction',
+			key => "#param.person_id#",
+			data => "Referral'to <a href='/person/#param.person_id#/profile'>#param.person_id#</a>"
+	};
+
+	$self->addFooter(new CGI::Dialog::Buttons);
+
+	return $self;
+}
+
+1;
