53270e7678cad97fa16d1fb91ee8c19f2345e044
yousuf_sayed <yousuf_sayed>
2000-06-08 21:12:23 +0000

    Changes in Eligibility.pm -- copied from snshah/temp directory

--- HealthSuite/Lib/perl/App/Dialog/Eligibility.pm
+++ HealthSuite/Lib/perl/App/Dialog/Eligibility.pm
@@ -24,7 +24,7 @@ use vars qw(@ISA);
 
 sub new
 {
-	my $self = CGI::Dialog::new(@_, id => 'eligibility', heading => '$Command Eligibility'); #, holdInput => 1);
+	my $self = CGI::Dialog::new(@_, id => 'eligibility', heading => 'Select Payer'); #, holdInput => 1);
 
 	my $schema = $self->{schema};
 	delete $self->{schema};  # make sure we don't store this!
@@ -33,10 +33,10 @@ sub new
 
 	$self->addContent(
 
-		new CGI::Dialog::Subhead(heading => 'Carrier', name => 'carrier_plan_heading'),
+#		new CGI::Dialog::Subhead(heading => 'Carrier', name => 'carrier_plan_heading'),
 		new App::Dialog::Field::Organization::ID(caption => 'Insurance Org Id', name => 'ins_org_id', options => FLDFLAG_REQUIRED),
-		new App::Dialog::Field::Insurance::Product(caption => 'Product Name', name => 'product_name', options => FLDFLAG_REQUIRED,
-			findPopup => '/lookup/insproduct/insorgid/itemValue', findPopupControlField => '_f_ins_org_id'),
+#		new App::Dialog::Field::Insurance::Product(caption => 'Product Name', name => 'product_name', options => FLDFLAG_REQUIRED,
+#			findPopup => '/lookup/insproduct/insorgid/itemValue', findPopupControlField => '_f_ins_org_id'),
 #		new App::Dialog::Field::Insurance::Product(caption => 'Product Name', name => 'product_name', options => FLDFLAG_REQUIRED,
 #			findPopup => '/lookup/insproduct/insorgid', findPopupControlField => '_f_ins_org_id')
 
@@ -47,18 +47,24 @@ sub new
 	return $self;
 }
 
-
-###############################
-# execute function
-###############################
-
 sub execute
 {
 	my ($self, $page, $command, $flags) = @_;
 
 	my $ins_org_id = $page->field('ins_org_id');
-	my $product_name = $page->field('product_name');
-	$page->param('_dialogreturnurl', "/org/@{[$page->param('org_id')]}/dlg-check-eligibilitypatient/$ins_org_id/$product_name");
+#	my $product_name = $page->field('product_name');
+	if($ins_org_id eq 'AETNA')
+	{
+		$page->param('_dialogreturnurl', "/org/@{[$page->param('org_id')]}/dlg-check-eligibility-aetna/$ins_org_id");
+	}
+	elsif($ins_org_id eq'BCBS')
+	{
+		$page->param('_dialogreturnurl', "/org/@{[$page->param('org_id')]}/dlg-check-eligibility-bcbs");
+	}
+	else
+	{
+		$page->param('_dialogreturnurl', "/org/@{[$page->param('org_id')]}/dlg-check-eligibility-other/$ins_org_id");
+	}
 
 	$self->handlePostExecute($page, $command, $flags);
 }
