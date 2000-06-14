17eb1ead2e9334badb4d508f687ecbd7d8f5c743
munir_faridi <munir_faridi>
2000-06-14 19:45:50 +0000

    [B479,525] Changed heading based on whether batch payment or not. Changed dialog id according to change made to resourcedirectory

--- HealthSuite/Lib/perl/App/Dialog/PostGeneralPayment.pm
+++ HealthSuite/Lib/perl/App/Dialog/PostGeneralPayment.pm
@@ -11,6 +11,7 @@ use App::Statements::Catalog;
 use App::Statements::Insurance;
 use CGI::Dialog;
 use CGI::Validator::Field;
+use App::Dialog::Field::Person;
 use App::Dialog::Field::Invoice;
 use App::Universal;
 use Date::Manip;
@@ -20,12 +21,12 @@ use vars qw(@ISA %RESOURCE_MAP);
 @ISA = qw(CGI::Dialog);
 
 %RESOURCE_MAP = (
-	'postpayment' => {},
+	'postpersonalpayment' => {},
 );
 
 sub new
 {
-	my $self = CGI::Dialog::new(@_, id => 'postpayment', heading => 'Post Personal Payment');
+	my $self = CGI::Dialog::new(@_);
 
 	my $schema = $self->{schema};
 	delete $self->{schema};  # make sure we don't store this!
@@ -33,7 +34,7 @@ sub new
 	croak 'schema parameter required' unless $schema;
 
 	$self->addContent(
-		new CGI::Dialog::Field(type => 'hidden', name => 'payer_id'),
+		new App::Dialog::Field::Person::ID(caption => 'Patient/Person Id', name => 'payer_id', options => FLDFLAG_REQUIRED),
 
 		new CGI::Dialog::Field(type => 'currency',
 					caption => 'Total Payment Received',
@@ -41,7 +42,11 @@ sub new
 					readOnlyWhen => CGI::Dialog::DLGFLAG_UPDORREMOVE,
 					options => FLDFLAG_REQUIRED),
 
-		new CGI::Dialog::Field::TableColumn(caption => 'Payment Type', schema => $schema,	column => 'Invoice_Item_Adjust.pay_type', readOnlyWhen => CGI::Dialog::DLGFLAG_UPDORREMOVE),
+		new CGI::Dialog::Field::TableColumn(
+					caption => 'Payment Type', 
+					schema => $schema, 
+					column => 'Invoice_Item_Adjust.pay_type', 
+					readOnlyWhen => CGI::Dialog::DLGFLAG_UPDORREMOVE),
 
 		new CGI::Dialog::MultiField(caption =>'Payment Method/Check Number', name => 'pay_method_fields',
 			fields => [
@@ -54,19 +59,6 @@ sub new
 
 		new CGI::Dialog::Field(caption => 'Authorization/Reference Number', name => 'auth_ref'),
 
-
-
-		new CGI::Dialog::Subhead(heading => 'This Visit', name => 'visit_heading'),
-
-		new CGI::Dialog::Field(caption => 'Claim Number', name => 'invoice_id', options => FLDFLAG_READONLY),
-		new CGI::Dialog::MultiField(caption =>'Service Date(s)/Balance', options => FLDFLAG_READONLY, name => 'dates_and_balance',
-			fields => [
-				new CGI::Dialog::Field(caption => 'Dates of Service', name => 'service_dates', options => FLDFLAG_READONLY),
-				new CGI::Dialog::Field(type => 'currency', caption => 'Balance', name => 'balance', options => FLDFLAG_READONLY),
-			]),
-
-		new CGI::Dialog::Field(caption => 'Office Visit Co-pay Due', name => 'copay_due', options => FLDFLAG_READONLY),
-		new CGI::Dialog::Field(type => 'currency', caption => "Payment for Today's Visit", name => 'adjustment_amount'),
 
 		new CGI::Dialog::Subhead(heading => 'Outstanding Invoices', name => 'outstanding_heading'),
 		new App::Dialog::Field::OutstandingInvoices(name =>'outstanding_invoices_list'),
@@ -88,16 +80,20 @@ sub makeStateChanges
 	my ($self, $page, $command, $dlgFlags) = @_;
 	$self->SUPER::makeStateChanges($page, $command, $dlgFlags);
 
-	my $invoiceId = $page->param('invoice_id');
-	my $invoiceInfo = $STMTMGR_INVOICE->getRowAsHash($page,STMTMGRFLAG_NONE, 'selInvoice', $invoiceId);
-	#if param invoice id is NULL, don't want to show invoice-specific fields
-	unless($invoiceId && $invoiceInfo->{invoice_status} != App::Universal::INVOICESTATUS_VOID)
+	$page->param('batch_id') ? $self->heading('Add Batch Personal Payments') : $self->heading('Add Personal Payment');
+
+	#make payment fields and list of invoices invisible unless person id exists
+	$self->setFieldFlags('payer_id', FLDFLAG_READONLY, 1);
+	unless(my $personId = $page->param('person_id') || $page->field('payer_id'))
 	{
-		$self->updateFieldFlags('visit_heading', FLDFLAG_INVISIBLE, 1);
-		$self->updateFieldFlags('invoice_id', FLDFLAG_INVISIBLE, 1);
-		$self->updateFieldFlags('dates_and_balance', FLDFLAG_INVISIBLE, 1);
-		$self->updateFieldFlags('copay_due', FLDFLAG_INVISIBLE, 1);
-		$self->updateFieldFlags('adjustment_amount', FLDFLAG_INVISIBLE, 1);
+		$self->updateFieldFlags('payer_id', FLDFLAG_READONLY, 0);
+		
+		$self->setFieldFlags('total_amount', FLDFLAG_INVISIBLE, 1);
+		$self->setFieldFlags('pay_type', FLDFLAG_INVISIBLE, 1);
+		$self->setFieldFlags('pay_method_fields', FLDFLAG_INVISIBLE, 1);
+		$self->setFieldFlags('auth_ref', FLDFLAG_INVISIBLE, 1);	
+		$self->setFieldFlags('outstanding_heading', FLDFLAG_INVISIBLE, 1);
+		$self->setFieldFlags('outstanding_invoices_list', FLDFLAG_INVISIBLE, 1);
 	}
 }
 
@@ -105,28 +101,7 @@ sub populateData
 {
 	my ($self, $page, $command, $activeExecMode, $flags) = @_;
 
-	if(my $invoiceId = $page->param('invoice_id'))
-	{
-		my $invoiceInfo = $STMTMGR_INVOICE->getRowAsHash($page,STMTMGRFLAG_NONE, 'selInvoice', $invoiceId);
-		$page->field('invoice_id', $invoiceId);
-		my $balanceDisplay = $invoiceInfo->{balance} ? "\$$invoiceInfo->{balance}" : "\$0";
-		$page->field('balance', $balanceDisplay);
-		$page->field('payer_id', $invoiceInfo->{client_id});
-
-		my $itemServDates = $STMTMGR_INVOICE->getRowAsHash($page, STMTMGRFLAG_NONE, 'selServiceDateRangeForAllItems', $invoiceId);
-		my $endDateDisplay = '';
-		if($itemServDates->{service_end_date})
-		{
-			$endDateDisplay = $itemServDates->{service_end_date} ne  $itemServDates->{service_begin_date} ? "- $itemServDates->{service_end_date}" : '';
-		}
-		my $dateDisplay = "$itemServDates->{service_begin_date} $endDateDisplay";
-		$page->field('service_dates', $dateDisplay);
-
-		my $invoiceCopayItem = $STMTMGR_INVOICE->getRowAsHash($page, STMTMGRFLAG_NONE, 'selInvoiceItemsByType', $invoiceId, App::Universal::INVOICEITEMTYPE_COPAY);
-		my $copayDisplay = $invoiceCopayItem->{balance} ? "\$$invoiceCopayItem->{balance}" : "\$0";
-		$page->field('copay_due', $copayDisplay);
-	}
-	elsif(my $personId = $page->param('person_id'))
+	if(my $personId = $page->param('person_id'))
 	{
 		$page->field('payer_id', $personId);	
 	}
@@ -137,97 +112,13 @@ sub execute
 	my ($self, $page, $command, $flags) = @_;
 	$command = 'add';
 
-	my $invoiceId = $page->param('invoice_id');
 	my $todaysDate = $page->getDate();
 	my $itemType = App::Universal::INVOICEITEMTYPE_ADJUST;
 	my $historyValueType = App::Universal::ATTRTYPE_HISTORY;
 
 	if($page->field('total_amount') > 0)
 	{
-		#APPLY PAYMENT TO "THIS VISIT" INVOICE
-		my $payment = $page->field('adjustment_amount');
-		if($payment > 0)
-		{
-			my $totalAdjustForItemAndItemAdjust = 0 - $payment;
-
-			my $totalDummyItems = $STMTMGR_INVOICE->getRowCount($page, STMTMGRFLAG_NONE, 'selInvoiceItemCountByType', $invoiceId, $itemType);
-			my $itemSeq = $totalDummyItems + 1;
-
-			my $itemBalance = $totalAdjustForItemAndItemAdjust;	# because this is a "dummy" item that is made for the sole purpose of applying a general
-																# payment, there is no charge and the balance should be negative.
-			my $itemId = $page->schemaAction(
-					'Invoice_Item', 'add',
-					parent_id => $invoiceId,
-					item_type => defined $itemType ? $itemType : undef,
-					total_adjust => defined $totalAdjustForItemAndItemAdjust ? $totalAdjustForItemAndItemAdjust : undef,
-					balance => defined $itemBalance ? $itemBalance : undef,
-					data_num_c => $itemSeq,
-					_debug => 0
-				);
-
-
-			# Create adjustment for the item
-			my $payerType = App::Universal::ENTITYTYPE_PERSON;
-			my $adjType = App::Universal::ADJUSTMENTTYPE_PAYMENT;
-			my $payMethod = $page->field('pay_method');
-			my $payerId = $page->field('payer_id');			#this is a hidden field for now, it is populated with invoice.client_id
-			my $payType = $page->field('pay_type');
-
-			$page->schemaAction(
-					'Invoice_Item_Adjust', 'add',
-					adjustment_type => defined $adjType ? $adjType : undef,
-					adjustment_amount => $payment || undef,
-					parent_id => $itemId || undef,
-					pay_date => $todaysDate || undef,
-					pay_method => defined $payMethod ? $payMethod : undef,
-					pay_ref => $page->field('pay_ref') || undef,
-					payer_type => $payerType || 0,
-					payer_id => $payerId || undef,
-					net_adjust => defined $totalAdjustForItemAndItemAdjust ? $totalAdjustForItemAndItemAdjust : undef,
-					data_text_a => $page->field('auth_ref') || undef,
-					pay_type => defined $payType ? $payType : undef,
-					#plan_allow => undef,
-					#plan_paid => undef,
-					#writeoff_code => undef,
-					#writeoff_amount => undef,
-					#adjust_codes => undef,
-					#comments => undef,
-					_debug => 0
-				);
-
-
-			#Update the invoice
-
-			my $invoice = $STMTMGR_INVOICE->getRowAsHash($page, STMTMGRFLAG_CACHE, 'selInvoice', $invoiceId);
-
-			my $totalAdjustForInvoice = $invoice->{total_adjust} + $totalAdjustForItemAndItemAdjust;
-			my $invoiceBalance = $invoice->{total_cost} + $totalAdjustForInvoice;
-
-			$page->schemaAction(
-					'Invoice', 'update',
-					invoice_id => $invoiceId || undef,
-					total_adjust => defined $totalAdjustForInvoice ? $totalAdjustForInvoice : undef,
-					balance => defined $invoiceBalance ? $invoiceBalance : undef,
-					_debug => 0
-				);
-
-
-			#Create history attribute for this adjustment
-			my $description = "Personal payment made by $payerId";
-			$page->schemaAction(
-					'Invoice_Attribute', 'add',
-					parent_id => $invoiceId || undef,
-					item_name => 'Invoice/History/Item',
-					value_type => defined $historyValueType ? $historyValueType : undef,
-					value_text => $description,
-					value_textB => $page->field('comments') || undef,
-					value_date => $todaysDate,
-					_debug => 0
-				);
-		}
-
-
-		#APPLY PAYMENTS TO LIST OF OUTSTANDING INVOICES
+		#apply payments to list of outstanding invoices
 
 		my $lineCount = $page->param('_f_line_count');
 		for(my $line = 1; $line <= $lineCount; $line++)
@@ -311,8 +202,19 @@ sub execute
 		}
 	}
 	
-	$page->redirect("/invoice/$invoiceId/summary");
-
+	if(my $returnToInvoiceId = $page->param('invoice_id'))
+	{
+		$page->redirect("/invoice/$returnToInvoiceId/summary");
+	}
+	else
+	{
+		$self->handlePostExecute($page, $command, $flags);
+	}
 }
+
+#sub customValidate
+#{
+#	my ($self, $page) = @_;
+#}
 
 1;
