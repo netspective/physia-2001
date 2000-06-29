4c2cb921b803f558c0b1375067decfb8400c87c8
robert_jenks <robert_jenks>
2000-06-29 17:00:01 +0000

    [B596] Commented out the insurance coverage related fields.  Reformated the perl code.

--- HealthSuite/Lib/perl/App/Dialog/Person/Patient.pm
+++ HealthSuite/Lib/perl/App/Dialog/Person/Patient.pm
@@ -37,77 +37,123 @@ sub initialize
 	my $self = shift;
 
 	my $postHtml = "<a href=\"javascript:doActionPopup('/lookup/person');\">Lookup existing person</a>";
-	#$self->heading('$Command Patient');
 	$self->addContent(
-			new CGI::Dialog::Field(type => 'hidden', name => 'resp_self'),
-			new App::Dialog::Field::Person::ID::New(caption => 'Patient/Person ID',
-							name => 'person_id',
-							types => ['Person'],
-							options => FLDFLAG_REQUIRED,
-							readOnlyWhen => CGI::Dialog::DLGFLAG_UPDORREMOVE,
-						postHtml => $postHtml),
-			);
+		new CGI::Dialog::Field(
+			type => 'hidden',
+			name => 'resp_self'
+		),
+		new App::Dialog::Field::Person::ID::New(
+			caption => 'Patient/Person ID',
+			name => 'person_id',
+			types => ['Person'],
+			options => FLDFLAG_REQUIRED,
+			readOnlyWhen => CGI::Dialog::DLGFLAG_UPDORREMOVE,
+			postHtml => $postHtml
+		),
+	);
 	$self->SUPER::initialize();
 	$self->addContent(
-
-		#OCCUPATION
-		new CGI::Dialog::Subhead(heading => 'Employment', name => 'occup_heading', invisibleWhen => CGI::Dialog::DLGFLAG_UPDORREMOVE),
-
-		new App::Dialog::Field::Organization::ID(caption =>'Employer ID', name => 'rel_id', invisibleWhen => CGI::Dialog::DLGFLAG_UPDORREMOVE),
-
-		new CGI::Dialog::Field(caption => 'Employment Status',
-									name => 'value_type',
-									fKeyStmtMgr => $STMTMGR_PERSON,
-									fKeyDisplayCol => 1,
-									fKeyValueCol => 0,
-									fKeyStmt => 'selEmpStatus',
-									defaultValue => '226',
-									invisibleWhen => CGI::Dialog::DLGFLAG_UPDORREMOVE),
-
-		new CGI::Dialog::Field(caption => 'Occupation', name => 'occupation', invisibleWhen => CGI::Dialog::DLGFLAG_UPDORREMOVE),
-
-		new CGI::Dialog::Field(type => 'phone', caption => 'Phone Number', name => 'phone_number', invisibleWhen => CGI::Dialog::DLGFLAG_UPDORREMOVE),
-		#new CGI::Dialog::Field(type => 'date', caption => 'Begin Date', name => 'begin_date', defaultValue => '', invisibleWhen => CGI::Dialog::DLGFLAG_UPDORREMOVE),
-
-		new CGI::Dialog::Subhead(heading => 'Insurance', name => 'insur_heading', invisibleWhen => CGI::Dialog::DLGFLAG_UPDORREMOVE),
-		#new CGI::Dialog::Field(
-		#						type => 'bool',
-		#						name => 'add_insurance',
-		#						caption => 'Add Personal Insurance Coverage?',
-		#						style => 'check',
-		#						invisibleWhen => CGI::Dialog::DLGFLAG_UPDORREMOVE),
-
-		new CGI::Dialog::MultiField(caption =>'Ins CompanyID/Product Name/Plan Name', name => 'insplan',invisibleWhen => CGI::Dialog::DLGFLAG_UPDORREMOVE,
-				fields => [
-							new App::Dialog::Field::Organization::ID(caption => 'Ins Company ID', name => 'ins_org_id',invisibleWhen => CGI::Dialog::DLGFLAG_UPDORREMOVE),
-							new App::Dialog::Field::Insurance::Product(caption => 'Product Name', name => 'product_name', findPopup => '/lookup/insurance/product_name', invisibleWhen => CGI::Dialog::DLGFLAG_UPDORREMOVE),
-							new App::Dialog::Field::Insurance::Plan(caption => 'Plan Name', name => 'plan_name', findPopup => '/lookup/insurance/plan_name', invisibleWhen => CGI::Dialog::DLGFLAG_UPDORREMOVE)
-						]),
-
+		new CGI::Dialog::Subhead(
+			heading => 'Employment',
+			name => 'occup_heading',
+			invisibleWhen => CGI::Dialog::DLGFLAG_UPDORREMOVE,
+		),
+		new App::Dialog::Field::Organization::ID(
+			caption =>'Employer ID',
+			name => 'rel_id',
+			invisibleWhen => CGI::Dialog::DLGFLAG_UPDORREMOVE,
+		),
 		new CGI::Dialog::Field(
-						type => 'bool',
-						name => 'delete_record',
-						caption => 'Delete record?',
-						style => 'check',
-						invisibleWhen => CGI::Dialog::DLGFLAG_ADD,
-						readOnlyWhen => CGI::Dialog::DLGFLAG_REMOVE),
+			caption => 'Employment Status',
+			name => 'value_type',
+			fKeyStmtMgr => $STMTMGR_PERSON,
+			fKeyDisplayCol => 1,
+			fKeyValueCol => 0,
+			fKeyStmt => 'selEmpStatus',
+			defaultValue => '226',
+			invisibleWhen => CGI::Dialog::DLGFLAG_UPDORREMOVE,
+		),
+		new CGI::Dialog::Field(
+			caption => 'Occupation',
+			name => 'occupation',
+			invisibleWhen => CGI::Dialog::DLGFLAG_UPDORREMOVE,
+		),
+		new CGI::Dialog::Field(
+			type => 'phone',
+			caption => 'Phone Number',
+			name => 'phone_number',
+			invisibleWhen => CGI::Dialog::DLGFLAG_UPDORREMOVE,
+		),
+#		new CGI::Dialog::Field(
+#			type => 'date',
+#			caption => 'Begin Date',
+#			name => 'begin_date',
+#			defaultValue => '',
+#			invisibleWhen => CGI::Dialog::DLGFLAG_UPDORREMOVE,
+#		),
+#		new CGI::Dialog::Subhead(
+#			heading => 'Insurance',
+#			name => 'insur_heading',
+#			invisibleWhen => CGI::Dialog::DLGFLAG_UPDORREMOVE,
+#		),
+#		new CGI::Dialog::Field(
+#			type => 'bool',
+#			name => 'add_insurance',
+#			caption => 'Add Personal Insurance Coverage?',
+#			style => 'check',
+#			invisibleWhen => CGI::Dialog::DLGFLAG_UPDORREMOVE,
+#		),
+#		new CGI::Dialog::MultiField(
+#			caption =>'Ins CompanyID/Product Name/Plan Name',
+#			name => 'insplan',
+#			invisibleWhen => CGI::Dialog::DLGFLAG_UPDORREMOVE,
+#			fields => [
+#				new App::Dialog::Field::Organization::ID(
+#					caption => 'Ins Company ID',
+#					name => 'ins_org_id',
+#					invisibleWhen => CGI::Dialog::DLGFLAG_UPDORREMOVE,
+#				),
+#				new App::Dialog::Field::Insurance::Product(
+#					caption => 'Product Name',
+#					name => 'product_name',
+#					findPopup => '/lookup/insurance/product_name',
+#					invisibleWhen => CGI::Dialog::DLGFLAG_UPDORREMOVE,
+#				),
+#				new App::Dialog::Field::Insurance::Plan(
+#					caption => 'Plan Name',
+#					name => 'plan_name',
+#					findPopup => '/lookup/insurance/plan_name',
+#					invisibleWhen => CGI::Dialog::DLGFLAG_UPDORREMOVE,
+#				),
+#			],
+#		),
+		new CGI::Dialog::Field(
+			caption => 'Delete record?',
+			type => 'bool',
+			name => 'delete_record',
+			style => 'check',
+			invisibleWhen => CGI::Dialog::DLGFLAG_ADD,
+			readOnlyWhen => CGI::Dialog::DLGFLAG_REMOVE
+		),
 	);
 
-	$self->addFooter(new CGI::Dialog::Buttons(
-						nextActions_add => [
-							['Add Insurance Coverage', "/person/%field.person_id%/dlg-add-ins-coverage?_f_product_name=%field.product_name%&_f_ins_org_id=%field.ins_org_id%&_f_plan_name=%field.plan_name%", 1],
-							['View Patient Summary', "/person/%field.person_id%/profile"],
-							['Add Referral', '/person/%field.person_id%/dlg-add-referral'],
-							['Add Another Patient', '/org/#session.org_id#/dlg-add-patient'],
-							['Go to Search', "/search/person/id/%field.person_id%"],
-							['Return to Home', "/person/#session.user_id#/home"],
-							['Go to Work List', "person/worklist"],
-							],
-						cancelUrl => $self->{cancelUrl} || undef)
+	$self->addFooter(
+		new CGI::Dialog::Buttons(
+			nextActions_add => [
+#				['Add Insurance Coverage', "/person/%field.person_id%/dlg-add-ins-coverage?_f_product_name=%field.product_name%&_f_ins_org_id=%field.ins_org_id%&_f_plan_name=%field.plan_name%", 1],
+				['Add Insurance Coverage', "/person/%field.person_id%/dlg-add-ins-coverage", 1],
+				['View Patient Summary', "/person/%field.person_id%/profile"],
+				['Add Referral', '/person/%field.person_id%/dlg-add-referral'],
+				['Add Another Patient', '/org/#session.org_id#/dlg-add-patient'],
+				['Go to Search', "/search/person/id/%field.person_id%"],
+				['Return to Home', "/person/#session.user_id#/home"],
+				['Go to Work List', "person/worklist"],
+			],
+			cancelUrl => $self->{cancelUrl} || undef,
+		),
 	);
 
-	$self->{activityLog} =
-	{
+	$self->{activityLog} = {
 		scope =>'person',
 		key => "#field.person_id#",
 		data => "Person '#field.person_id#' <a href='/person/#field.person_id#/profile'>#field.name_first# #field.name_last#</a>"
@@ -140,9 +186,9 @@ sub customValidate
 {
 	my ($self, $page) = @_;
 
-	my $insOrg = $self->getField('insplan')->{fields}->[0];
-	my $productName = $self->getField('insplan')->{fields}->[1];
-	my $PlanName = $self->getField('insplan')->{fields}->[2];
+	#my $insOrg = $self->getField('insplan')->{fields}->[0];
+	#my $productName = $self->getField('insplan')->{fields}->[1];
+	#my $PlanName = $self->getField('insplan')->{fields}->[2];
 	my $relationship = $self->getField('party_name');
 	#my $relationSelf = $self->getField('responsible')->{fields}->[1];
 
