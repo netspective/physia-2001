2b8e378e2e9dd3da82480b35e74e5aca2bd555b1
snshah <snshah>
2000-07-10 16:50:08 +0000

    *** empty log message ***

---
+++ HealthSuite/Lib/perl/App/Dialog/Directory.pm
@@ -0,0 +1,28 @@
+##############################################################################
+package App::Dialog::Directory;
+##############################################################################
+
+use strict;
+use Carp;
+use CGI::Dialog;
+use App::Universal;
+use vars qw(@ISA %DIRECTORY %RESOURCE_MAP);
+%RESOURCE_MAP = ();
+
+@ISA = qw(CGI::Dialog);
+%DIRECTORY = ();
+
+sub new
+{
+	my $self = CGI::Dialog::new(@_);
+
+	my $id = $self->{id} || die 'id is required';
+	$DIRECTORY{$id} = $self;
+	return $self;
+}
+
+sub description
+{
+}
+
+1;
