acffbecefe6fd7bd9ac95102261de826469d2f7c
frank_major <frank_major>
2000-07-20 19:38:00 +0000

    [226] Added SQL for monthly audit report

--- HealthSuite/Lib/perl/App/Statements/Report/Accounting.pm
+++ HealthSuite/Lib/perl/App/Statements/Report/Accounting.pm
@@ -129,8 +129,8 @@ $STMTMGR_REPORT_ACCOUNTING = new App::Statements::Report::Accounting(
 		code, 
 		caption,
 		rel_diags,
-		units,
-		unit_cost,		
+		decode(item_type,7,0,units) as units,
+		decode(item_type,7,0,unit_cost) as unit_cost,		
 		sum(total_charges) total_charges, 
 		sum(misc_charges) misc_charges ,
 		sum(person_pay) person_pay,
@@ -155,8 +155,8 @@ $STMTMGR_REPORT_ACCOUNTING = new App::Statements::Report::Accounting(
 		provider ,
 		code, 
 		caption,
-		units,
-		unit_cost,
+		decode(item_type,7,0,units) ,
+		decode(item_type,7,0,unit_cost) ,		
 		rel_diags,
 		client_id
 	},
@@ -180,6 +180,69 @@ $STMTMGR_REPORT_ACCOUNTING = new App::Statements::Report::Accounting(
 		
 	group by invoice_date
 },			
+
+
+	'sel_monthly_audit' => qq{
+	SELECT	to_char(invoice_date,'MM/YYYY') invoice_date ,
+		sum(total_charges) as total_charges ,
+		sum(misc_charges) misc_charges,
+		sum(charge_adjust) charge_adjust,
+		sum(insurance_write_off) insurance_write_off,					
+		sum(balance_transfer) balance_transfer,											
+		sum(person_pay) person_pay,
+		sum(insurance_pay) insurance_pay ,	
+		sum(person_write_off) person_write_off
+	FROM 	invoice_charges
+	WHERE   invoice_date between to_date(:1,'$SQLSTMT_DEFAULTDATEFORMAT') 
+		AND to_date(:2,'$SQLSTMT_DEFAULTDATEFORMAT')
+		AND (facility = :3 OR :3 is NULL)
+		AND (provider >=:4 OR :4 is NULL)
+		AND (batch_id >= :5 OR :5 is NULL)
+		AND (batch_id <= :6 OR :6 is NULL)
+		
+	group by to_char(invoice_date,'MM/YYYY') },
+
+	'sel_monthly_audit_detail' => qq{
+	SELECT	invoice_id ,
+		to_char(invoice_date,'MM/YYYY') as invoice_batch_date, 		
+		service_begin_date,
+		service_end_date,
+		provider as care_provider_id ,
+		client_id as patient_id,
+		code, 
+		caption,
+		rel_diags,
+		decode(item_type,7,0,units) as units,
+		decode(item_type,7,0,unit_cost) as unit_cost,		
+		sum(total_charges) total_charges, 
+		sum(misc_charges) misc_charges ,
+		sum(person_pay) person_pay,
+		sum(insurance_pay) insurance_pay,		
+		sum(insurance_write_off) insurance_write_off,			
+		sum(balance_transfer) balance_transfer,
+		sum(charge_adjust) as  charge_adjust
+	FROM 	invoice_charges
+	WHERE 	to_char(invoice_date,'MM/YYYY') = :1								
+		AND (facility = :2 or :2 IS NULL )
+		AND (provider = :3 or :3 IS NULL)
+		AND
+		( 
+			(batch_id >= :4 or :4 is NULL) 
+			AND
+			(batch_id <= :5  or :5 is NULL) 		
+		)		
+	group by invoice_id ,
+		to_char(invoice_date,'MM/YYYY') ,		
+		service_begin_date,
+		service_end_date,
+		provider ,
+		code, 
+		caption,
+		decode(item_type,7,0,units) ,
+		decode(item_type,7,0,unit_cost) ,		
+		rel_diags,
+		client_id
+	},
 		
 	
 );
