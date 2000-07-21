0bebb6dce75a02973aa6d66c9eef43e8a60712fe
robert_jenks <robert_jenks>
2000-07-21 23:41:13 +0000

    Updated the search base class to pass control to App::Page to render the header line (icon, title)

--- HealthSuite/Lib/perl/App/Page/Search.pm
+++ HealthSuite/Lib/perl/App/Page/Search.pm
@@ -6,7 +6,7 @@ use strict;
 use App::Page;
 use App::Universal;
 use Exporter;
-use App::ImageManager;
+use CGI::ImageManager;
 
 use enum qw(BITMASK:SEARCHFLAG_ LOOKUPWINDOW SEARCHBAR);
 
@@ -18,7 +18,11 @@ my $LIMIT = App::Universal::SEARCH_RESULTS_LIMIT;
 
 %RESOURCE_MAP = (
 	'search' => {
-		_idSynonym => ['lookup'],
+		_idSynonym => ['lookup', 'menu'],
+		_title => 'Lookup',
+		_iconSmall => 'icons/search',
+		_iconMedium => 'icons/search',
+		_iconLarge => 'icons/search',
 		},
 	);
 
@@ -26,114 +30,32 @@ sub prepare_page_content_header
 {
 	my $self = shift;
 	my $flags = 0;
-	$flags |= SEARCHFLAG_LOOKUPWINDOW if $self->param('_islookup');
 	my ($heading, $searchForm) = $self->getForm($flags);
-	if(($flags & SEARCHFLAG_LOOKUPWINDOW) && $searchForm)
-	{
-		push(@{$self->{page_content_header}}, qq{
-		<STYLE>
-			body { margin: 0; }
+	$self->property('_title', $heading) unless $self->property('_title');
+	$self->property('_iconMedium', 'icons/search') unless $self->property('_iconMedium');
+	
+	unshift(@{$self->{page_content_header}}, q{
+		<style>
 			select { font-size:8pt; font-family: Tahoma, Arial, Helvetica }
 			input { font-size:8pt; font-family: Tahoma, Arial, Helvetica }
-		</STYLE>
-		<TABLE BGCOLOR=LIGHTSTEELBLUE BORDER=0 CELLSPACING=1 CELLPADDING=5 WIDTH=100%>
-			<TR>
-			<FORM NAME="search_form" METHOD=POST>
-			<TD>
-			<CENTER><B><FONT FACE="Arial,Helvetica" SIZE=2 COLOR=NAVY>$heading</FONT></B></CENTER>
-			<FONT FACE='Verdana,Arial,Helvetica' SIZE=2>
-			$searchForm
-			</FONT>
-			</TD>
-			</FORM>
-			</TR>
-		</TABLE>
+		</style>
 		});
-		return 1;
-	}
+		
+	$heading = $self->flagIsSet(PAGEFLAG_ISPOPUP) ? qq{<center><b><font face="arial,helvetica" size="2" color="navy">$heading</font></b></center>} : '';
+	
+	my $formHtml = $searchForm ? qq{
+		<table bgcolor="#EEEEEE" border="0" cellspacing="0" cellpadding="5" width="100%"><tr><td>
+			<form name="search_form" method="post">
+				$heading \&nbsp;
+				<input type="hidden" name="arl" value="@{[$self->param('arl')]}">
+				<font face='arial,helvetica' size="2">$searchForm</font>
+			</form>
+		</td></tr></table>
+		} : '';	
+	push(@{$self->{page_content_header}}, $formHtml);
+	return 1 if $self->flagIsSet(PAGEFLAG_ISPOPUP);
 
 	$self->SUPER::prepare_page_content_header(@_);
-	my ($colors, $fonts) = ($self->getThemeColors(), $self->getThemeFontTags());
-	my $urlPrefix = "/search";
-	my $functions = $self->getMenu_ComboBox(App::Page::MENUFLAG_SELECTEDISLARGER,
-		'_pm_view',
-		[
-			['Lookup...'],
-			['All Persons', "$urlPrefix/person", 'person'],
-			['Patients', "$urlPrefix/patient", 'patient'],
-			['Claims', "$urlPrefix/claim", 'claim'],
-			['Appointments', "$urlPrefix/appointment", 'appointment'],
-			['Appointment Types', "$urlPrefix/appttype", 'appttype'],
-			['Available Slots', "$urlPrefix/apptslot", 'apptslot'],
-			['Organizations', "$urlPrefix/org", 'org'],
-			['Insurance Products', "$urlPrefix/insproduct", 'insproduct'],
-			['Insurance Plans', "$urlPrefix/insplan", 'plan'],
-			['Fee Schedules', "$urlPrefix/catalog", 'catalog'],
-			['ICD', "$urlPrefix/icd", 'icd'],
-			['CPT', "$urlPrefix/cpt", 'cpt'],
-			['HCPCS', "$urlPrefix/cpt", 'hcpcs'],
-			['EPSDT', "$urlPrefix/epsdt", 'epsdt'],
-			['Misc Procedure Code', "$urlPrefix/miscprocedure", 'miscprocedure'],
-			['Schedule Template', "$urlPrefix/template", 'template'],
-			['User Sessions', "$urlPrefix/session", 'session'],
-		]);
-
-	my $addFunctions = $self->getMenu_ComboBox(App::Page::MENUFLAG_SELECTEDISLARGER,
-		'_pm_view',
-		[
-			['Add...'],
-			['Patient', "/org/#session.org_id#/dlg-add-patient", 'patient'],
-			['Claim', "/org/#session.org_id#/dlg-add-claim", 'claim'],
-			['Appointment', "/org/#session.org_id#/dlg-add-appointment", 'appointment'],
-			['Appointment Type', "/org/#session.org_id#/dlg-add-appttype", 'appttype'],
-			['Insurance Org', "/org/#session.org_id#/dlg-add-org-insurance", 'insurance'],
-			['Insurance Product', "/org/#session.org_id#/dlg-add-ins-product", 'insproduct'],
-			['Insurance Plan', "/org/#session.org_id#/dlg-add-ins-plan", 'insplan'],
-			['Fee Schedule', "/org/#session.org_id#/dlg-add-catalog", 'catalog'],
-			['Schedule Template', "/org/#session.org_id#/dlg-add-template", 'template'],
-		]);
-
-	my $formHtml = $searchForm ? qq{
-		<TABLE BGCOLOR='#EEEEEE' BORDER=0 CELLSPACING=1 CELLPADDING=5 WIDTH=100%>
-			<TR>
-			<FORM NAME="search_form" METHOD="post">
-			<TD ><FONT FACE='Arial,Helvetica' SIZE=2>
-			<INPUT TYPE="HIDDEN" NAME="arl" VALUE="@{[$self->param('arl')]}">
-			$searchForm
-			</FONT>
-			</TD>
-			</FORM>
-			</TR>
-		</TABLE>
-		} : '';
-
-	push(@{$self->{page_content_header}}, qq{
-		<STYLE>
-			select { font-size:8pt; font-family: Tahoma, Arial, Helvetica }
-			input { font-size:8pt; font-family: Tahoma, Arial, Helvetica }
-		</STYLE>
-		<TABLE WIDTH=100% BGCOLOR=LIGHTSTEELBLUE BORDER=0 CELLPADDING=0 CELLSPACING=1>
-		<TR><TD BGCOLOR=BEIGE>
-		<TABLE WIDTH=100% BGCOLOR=LIGHTSTEELBLUE CELLSPACING=0 CELLPADDING=3 BORDER=0>
-			<TR>
-			<TD>
-				<FONT FACE="Arial,Helvetica" SIZE=4 COLOR=DARKRED>
-					$IMAGETAGS{'icon-l/search'} <B>$heading</B>
-				</FONT>
-			</TD>
-			<TD ALIGN=RIGHT>
-				<FONT FACE="Arial,Helvetica" SIZE=2>
-				$functions $addFunctions
-				</FONT>
-			</TD>
-			</TR>
-		</TABLE>
-		$formHtml
-		</TD></TR>
-		</TABLE>
-		<FONT SIZE=1>&nbsp;<BR></FONT>
-		});
-
 	return 1;
 }
 
@@ -191,7 +113,7 @@ sub initialize
 	my $self = shift;
 	$self->SUPER::initialize(@_);
 	$self->addLocatorLinks(
-			['Search', '/search'],
+			['Main Menu', '/menu'],
 		);
 		
 	# Check user's permission to page
@@ -220,7 +142,8 @@ sub handleARL
 	return 0 if $self->SUPER::handleARL($arl, $params, $rsrc, $pathItems) == 0;
 	$handleExec = 1 unless defined $handleExec;
 
-	$self->param('_islookup', 1) if $rsrc eq 'lookup';
+	$self->setFlag(PAGEFLAG_ISPOPUP) if $rsrc =~ /^lookup/;
+	$self->param('_islookup', 1) if $rsrc =~ /^lookup/;
 	$self->param('_pm_view', $pathItems->[0]);
 	$self->param('search_type', $pathItems->[1]) unless $self->param('search_type');
 	$self->param('search_expression', $pathItems->[2]) unless $self->param('search_expression');
