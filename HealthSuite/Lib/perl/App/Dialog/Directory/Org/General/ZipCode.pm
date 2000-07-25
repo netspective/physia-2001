b6c6027f8c5237067076cbba02fc3d125e13cff1
lloyd_brodsky <lloyd_brodsky>
2000-07-25 21:42:47 +0000

    new lookup function for HDS provider directory

---
+++ HealthSuite/Lib/perl/App/Dialog/Directory/Org/General/ZipCode.pm
@@ -0,0 +1,63 @@
+##############################################################################
+package App::Dialog::Directory::Org::General::ZipCodeLookup;
+##############################################################################
+
+use strict;
+use Carp;
+use App::Dialog::Directory;
+use App::Universal;
+
+use CGI::Dialog;
+use CGI::Validator::Field;
+use DBI::StatementManager;
+
+use App::Statements::Component::Invoice;
+
+use vars qw(@ISA $INSTANCE);
+
+@ISA = qw(App::Dialog::Directory);
+
+sub new
+{
+	my $self = App::Dialog::Directory::new(@_, id => 'provider-zip-code', heading => 'Zip Code');
+
+	$self->addContent(
+			new CGI::Dialog::Field(caption => 'Zip Code',  name => 'zipcode'),
+			);
+	$self->addFooter(new CGI::Dialog::Buttons);
+
+	$self;
+}
+
+#sub populateData
+#{
+#	my ($self, $page, $command, $activeExecMode, $flags) = @_;
+#
+#	$page->field('person_id', $page->session('person_id'));
+#}
+
+
+sub execute
+{
+	my ($self, $page, $command, $flags) = @_;
+
+	my $personId = $page->field('person_id');
+
+	if ( $personId ne '')
+	{
+		return $STMTMGR_COMPONENT_INVOICE->createHtml($page, STMTMGRFLAG_NONE, 'invoice.procAnalysis', [$personId]);
+	}
+	else
+	{
+		return $STMTMGR_COMPONENT_INVOICE->createHtml($page, STMTMGRFLAG_NONE, 'invoice.procAnalysisAll');
+	}
+
+
+
+}
+
+
+# create a new instance which will automatically add it to the directory of
+# reports
+#
+$INSTANCE = new __PACKAGE__;
