50c61e9642071dea40dcae192fe8609aeec3c7c3
munir_faridi <munir_faridi>
2000-07-28 23:24:49 +0000

    [B125,676] Implemented use of triggers. Added batch id and date

--- HealthSuite/Lib/perl/App/Dialog/PostRefund.pm
+++ HealthSuite/Lib/perl/App/Dialog/PostRefund.pm
@@ -12,6 +12,7 @@ use App::Statements::Insurance;
 use CGI::Dialog;
 use CGI::Validator::Field;
 use App::Dialog::Field::Invoice;
+use App::Dialog::Field::BatchDateID;
 use App::Universal;
 use Date::Manip;
 
@@ -33,6 +34,13 @@ sub new
 	croak 'schema parameter required' unless $schema;
 
 	$self->addContent(
+		#new App::Dialog::Field::BatchDateID(caption => 'Batch ID Date', name => 'batch_fields',orgInternalIdFieldName=>'service_facility_id'),
+		new CGI::Dialog::MultiField(caption =>'Batch ID/Date', name => 'batch_fields',
+			fields => [
+				new CGI::Dialog::Field(caption => 'Batch ID', name => 'batch_id', size => 12, options => FLDFLAG_REQUIRED),
+				new CGI::Dialog::Field(type => 'date', caption => 'Batch Date', name => 'batch_date', options => FLDFLAG_REQUIRED),
+			]),
+
 		new CGI::Dialog::Subhead(heading => 'Overpaid Invoices', name => 'credit_heading'),
 		new App::Dialog::Field::CreditInvoices(name =>'credit_invoices_list'),
 	);
@@ -47,17 +55,6 @@ sub new
 	return $self;
 }
 
-#sub makeStateChanges
-#{
-#	my ($self, $page, $command, $dlgFlags) = @_;
-#	$self->SUPER::makeStateChanges($page, $command, $dlgFlags);
-#}
-
-#sub populateData
-#{
-#	my ($self, $page, $command, $activeExecMode, $flags) = @_;
-#}
-
 sub execute
 {
 	my ($self, $page, $command, $flags) = @_;
@@ -66,19 +63,20 @@ sub execute
 	my $todaysDate = $page->getDate();
 	my $itemType = App::Universal::INVOICEITEMTYPE_ADJUST;
 	my $historyValueType = App::Universal::ATTRTYPE_HISTORY;
+	my $textValueType = App::Universal::ATTRTYPE_TEXT;
+	my $batchId = $page->field('batch_id');
+	my $batchDate = $page->field('batch_date');
 
 	#apply refund
 	my $lineCount = $page->param('_f_line_count');
 	for(my $line = 1; $line <= $lineCount; $line++)
 	{
-		my $refundAmt = $page->param("_f_invoice_$line\_refund");
+		my $refundAmt = 0 - $page->param("_f_invoice_$line\_refund");
 		next if $refundAmt eq '';
 
 		my $totalAdjustForItemAndItemAdjust = $refundAmt;
 
 		my $invoiceId = $page->param("_f_invoice_$line\_invoice_id");
-		my $totalDummyItems = $STMTMGR_INVOICE->getRowCount($page, STMTMGRFLAG_NONE, 'selInvoiceItemCountByType', $invoiceId, $itemType);
-		my $itemSeq = $totalDummyItems + 1;
 
 		my $itemBalance = $totalAdjustForItemAndItemAdjust;	# because this is a "dummy" item that is made for the sole purpose of applying a general
 															# payment, there is no charge and the balance should be negative.
@@ -87,8 +85,7 @@ sub execute
 				parent_id => $invoiceId,
 				item_type => defined $itemType ? $itemType : undef,
 				total_adjust => defined $totalAdjustForItemAndItemAdjust ? $totalAdjustForItemAndItemAdjust : undef,
-				balance => defined $itemBalance ? $itemBalance : undef,
-				data_num_c => $itemSeq,
+				#balance => defined $itemBalance ? $itemBalance : undef,
 				_debug => 0
 			);
 
@@ -99,13 +96,13 @@ sub execute
 
 		my $adjType = App::Universal::ADJUSTMENTTYPE_REFUND;
 		my $comments = $page->param("_f_invoice_$line\_comments");
-		$page->schemaAction(
+		my $adjItemId = $page->schemaAction(
 				'Invoice_Item_Adjust', 'add',
 				adjustment_type => defined $adjType ? $adjType : undef,
 				adjustment_amount => defined $refundAmt ? $refundAmt : undef,
 				parent_id => $itemId || undef,
 				pay_date => $todaysDate || undef,
-				net_adjust => defined $totalAdjustForItemAndItemAdjust ? $totalAdjustForItemAndItemAdjust : undef,
+				#net_adjust => defined $totalAdjustForItemAndItemAdjust ? $totalAdjustForItemAndItemAdjust : undef,
 				comments => $comments || undef,
 				_debug => 0
 			);
@@ -114,40 +111,49 @@ sub execute
 
 		#Update the invoice
 
-		my $invoice = $STMTMGR_INVOICE->getRowAsHash($page, STMTMGRFLAG_CACHE, 'selInvoice', $invoiceId);
+		#my $invoice = $STMTMGR_INVOICE->getRowAsHash($page, STMTMGRFLAG_CACHE, 'selInvoice', $invoiceId);
 
-		my $totalAdjustForInvoice = $invoice->{total_adjust} + $totalAdjustForItemAndItemAdjust;
-		my $invoiceBalance = $invoice->{total_cost} + $totalAdjustForInvoice;
+		#my $totalAdjustForInvoice = $invoice->{total_adjust} + $totalAdjustForItemAndItemAdjust;
+		#my $invoiceBalance = $invoice->{total_cost} + $totalAdjustForInvoice;
 
+		#$page->schemaAction(
+		#		'Invoice', 'update',
+		#		invoice_id => $invoiceId || undef,
+		#		total_adjust => defined $totalAdjustForInvoice ? $totalAdjustForInvoice : undef,
+		#		balance => defined $invoiceBalance ? $invoiceBalance : undef,
+		#		_debug => 0
+		#	);
+
+
+		#Add batch attribute
 		$page->schemaAction(
-				'Invoice', 'update',
-				invoice_id => $invoiceId || undef,
-				total_adjust => defined $totalAdjustForInvoice ? $totalAdjustForInvoice : undef,
-				balance => defined $invoiceBalance ? $invoiceBalance : undef,
-				_debug => 0
-			);
-
-
-
+			'Invoice_Attribute', 'add',
+			parent_id => $invoiceId || undef,
+			item_name => 'Invoice/Payment/Batch ID',
+			value_type => defined $textValueType ? $textValueType : undef,
+			value_text => $batchId || undef,
+			value_date => $batchDate || undef,
+			value_int => $adjItemId || undef,
+			_debug => 0
+		);
 
 
 		#Create history attribute for this adjustment
-
-		my $description = "Refund in the amount of \$$refundAmt";
-
 		$page->schemaAction(
 				'Invoice_Attribute', 'add',
 				parent_id => $invoiceId || undef,
 				item_name => 'Invoice/History/Item',
 				value_type => defined $historyValueType ? $historyValueType : undef,
-				value_text => $description,
+				value_text => "Refund in the amount of \$$refundAmt",
 				value_textB => $comments || undef,
 				value_date => $todaysDate,
 				_debug => 0
 			);
 	}
 
-	$self->handlePostExecute($page, $command, $flags);
+
+	my $personId = $page->param('person_id');
+	$page->redirect("/person/$personId/account");
 }
 
 1;
