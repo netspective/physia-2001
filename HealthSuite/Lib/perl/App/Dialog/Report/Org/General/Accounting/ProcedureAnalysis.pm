c9361b569a1579fb5a2448644444ca68f206cd5e
frank_major <frank_major>
2000-08-01 22:27:12 +0000

    [B688] Added more filter options to report

--- HealthSuite/Lib/perl/App/Dialog/Report/Org/General/Accounting/ProcedureAnalysis.pm
+++ HealthSuite/Lib/perl/App/Dialog/Report/Org/General/Accounting/ProcedureAnalysis.pm
@@ -10,7 +10,7 @@ use App::Universal;
 use CGI::Dialog;
 use CGI::Validator::Field;
 use DBI::StatementManager;
-
+use App::Statements::Org;
 use App::Statements::Component::Invoice;
 
 use vars qw(@ISA $INSTANCE);
@@ -22,19 +22,24 @@ sub new
 	my $self = App::Dialog::Report::new(@_, id => 'rpt-acct-proc-receipt-analysis', heading => 'Procedure Analysis');
 
 	$self->addContent(
-			new App::Dialog::Field::Person::ID(caption =>'Provider ID', name => 'person_id', invisibleWhen => CGI::Dialog::DLGFLAG_UPDORREMOVE),
+			new CGI::Dialog::Field(
+				name => 'batch_date',
+				caption => 'Batch Report Date',
+				type =>'date',
+				options=>FLDFLAG_REQUIRED,
+				),
+			new App::Dialog::Field::Organization::ID(caption =>'Site Organization ID', name => 'org_id'),
+			new App::Dialog::Field::Person::ID(caption =>'Physican ID', name => 'person_id', ),
+			new CGI::Dialog::MultiField(caption => 'CPT From/To', name => 'cpt_field', 
+						fields => [
+			new CGI::Dialog::Field(caption => 'CPT From', name => 'cpt_from', size => 12),
+			new CGI::Dialog::Field(caption => 'CPT To', name => 'cpt_to', size => 12),
+				]),
 			);
 	$self->addFooter(new CGI::Dialog::Buttons);
 
 	$self;
 }
-
-#sub populateData
-#{
-#	my ($self, $page, $command, $activeExecMode, $flags) = @_;
-#
-#	$page->field('person_id', $page->session('person_id'));
-#}
 
 
 sub execute
@@ -42,15 +47,14 @@ sub execute
 	my ($self, $page, $command, $flags) = @_;
 
 	my $personId = $page->field('person_id');
-
-	if ( $personId ne '')
-	{
-		return $STMTMGR_COMPONENT_INVOICE->createHtml($page, STMTMGRFLAG_NONE, 'invoice.procAnalysis', [$personId]);
-	}
-	else
-	{
-		return $STMTMGR_COMPONENT_INVOICE->createHtml($page, STMTMGRFLAG_NONE, 'invoice.procAnalysisAll');
-	}
+	my $batchDate = $page->field('batch_date');
+	my $orgId = $page->field('org_id');
+	my $cptFrom = $page->field('cpt_from');
+	my $cptTo = $page->field('cpt_to');
+	my $orgIntId='';
+	$orgIntId = $STMTMGR_ORG->getSingleValue($page, STMTMGRFLAG_NONE, 'selOrgId', $page->session('org_internal_id'), $orgId) if $orgId;
+	return $STMTMGR_COMPONENT_INVOICE->createHtml($page, STMTMGRFLAG_NONE, 'invoice.procAnalysis', [$personId,$batchDate,	
+		$orgIntId,$cptFrom,$cptTo]);
 
 
 
