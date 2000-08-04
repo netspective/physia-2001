294290659bff23fc16b5e896f74322a012ad361f
radha_kotagiri <radha_kotagiri>
2000-08-04 21:58:11 +0000

    [B725] Changed the validation for Product and Plan

--- HealthSuite/Lib/perl/App/Dialog/Field/Insurance.pm
+++ HealthSuite/Lib/perl/App/Dialog/Field/Insurance.pm
@@ -51,12 +51,12 @@ sub isValid
 		my $value = $page->field($self->{name});
 		my $ownerId = $page->param('person_id');
 		my $recordType = App::Universal::RECORDTYPE_PERSONALCOVERAGE;
-		my $insOrg = $page->field('ins_org_id');
-		my $ownerOrgId = $page->session('org_internal_id');
-		my $orgId = $STMTMGR_ORG->getSingleValue($page, STMTMGRFLAG_NONE, 'selOrgId', $ownerOrgId, $insOrg);
-		my $planName = $page->field('plan_name');
-		my $personPlanExists = $STMTMGR_INSURANCE->getSingleValue($page,STMTMGRFLAG_NONE,'selPersonPlanExists',$value, $planName, $recordType, $ownerId, $orgId);
-		my $newProductExists = $STMTMGR_INSURANCE->getSingleValue($page,STMTMGRFLAG_NONE,'selNewProductExists',$value, $orgId);
+		#my $insOrg = $page->field('ins_org_id');
+		#my $ownerOrgId = $page->session('org_internal_id');
+		#my $orgId = $STMTMGR_ORG->getSingleValue($page, STMTMGRFLAG_NONE, 'selOrgId', $ownerOrgId, $insOrg);
+		#my $planName = $page->field('plan_name');
+		#my $personPlanExists = $STMTMGR_INSURANCE->getSingleValue($page,STMTMGRFLAG_NONE,'selPersonPlanExists',$value, $planName, $recordType, $ownerId, $orgId);
+		my $newProductExists = $STMTMGR_INSURANCE->getSingleValue($page,STMTMGRFLAG_NONE,'selNewProductExists',$value);
 
 
 		$self->invalidate($page, "Product Name '$value' already exists.") if $newProductExists ne '';
@@ -179,13 +179,13 @@ sub isValid
 	{
 		my $value = $page->field($self->{name});
 
-		my $orgId = $page->field('ins_org_id');
-		my $ownerOrgId = $page->session('org_internal_id');
-		my $insOrgInternalId = $STMTMGR_ORG->getSingleValue($page, STMTMGRFLAG_NONE, 'selOrgId', $ownerOrgId, $orgId);
+		#my $orgId = $page->field('ins_org_id');
+		#my $ownerOrgId = $page->session('org_internal_id');
+		#my $insOrgInternalId = $STMTMGR_ORG->getSingleValue($page, STMTMGRFLAG_NONE, 'selOrgId', $ownerOrgId, $orgId);
 
 		my $productName = $page->field('product_name');
 		my $planName = $page->field('plan_name');
-		my $orgPlanExists = $STMTMGR_INSURANCE->getSingleValue($page,STMTMGRFLAG_NONE,'selNewPlanExists',$productName, $planName, $insOrgInternalId);
+		my $orgPlanExists = $STMTMGR_INSURANCE->getSingleValue($page,STMTMGRFLAG_NONE,'selNewPlanExists', $planName);
 
 		$self->invalidate($page, "Plan Name '$value' already exists.") if $orgPlanExists ne '';	}
 
