627d6eda591f3d4119a8f03609ef7560a3d51521
thai_nguyen <thai_nguyen>
2000-08-09 22:22:37 +0000

    Checkin claims processing scripts

---
+++ HealthSuite/Scripts/Per-Se/create_nsf_file.pl
@@ -0,0 +1,117 @@
+use strict;
+
+use constant NSFDEST_ARRAY => 0;
+use constant NSFDEST_FILE => 1;
+use constant NSF_HALLEY => '0';
+use constant NSF_ENVOY => '1';
+use constant DEFAULT_VFLAGS => 0;
+
+use App::Billing::Claims;
+use App::Billing::Input::DBI;
+use App::Billing::Output::NSF;
+use App::Billing::Validators;
+use App::Billing::Output::PDF;
+use DBI;
+
+use constant INVOICESTATUS_SUBMITTED => 4;
+use constant INVOICESTATUS_TRANSMITTED => 5;
+
+use Date::Manip;
+
+sub main
+{
+	my @ARGV = @_;
+	
+	my $today = UnixDate('today', '%m%d%Y_%H%M');
+	
+	my $cs = shift;
+	die "Usage Example:  $0 sde_prime/sde\@sdedbs02" unless $cs;
+	
+	$cs =~ /(.*?)\/(.*?)\@(.*)/;
+	my ($userName, $password, $twoTask) =  ($1, $2, $3);
+
+	my $NSF_FILE_NAME = 'phy169_' . $today . '.nsf';
+	
+	my $claimList = new App::Billing::Claims;
+	my $valMgr = new App::Billing::Validators;
+	my $input = new App::Billing::Input::DBI;
+
+	$input->registerValidators($valMgr);
+
+	if($input->populateClaims($claimList, valMgr => $valMgr,
+		UID => $userName, PWD => $password, connectStr => 'dbi:Oracle:' . $twoTask,
+		invoiceIds => ''))
+	{
+		if($valMgr->haveErrors() == 0)
+		{
+			my $output = new App::Billing::Output::NSF();
+			$output->registerValidators($valMgr);
+
+			my $claims = $claimList->getClaim();
+
+			# $valMgr->validateClaim('Output', DEFAULT_VFLAGS, $claimList);
+
+			foreach my $claim (@$claims)
+			{
+				# $valMgr->validateClaim('Claim_Payer', DEFAULT_VFLAGS, $claim);
+			}
+
+			if($valMgr->haveErrors() == 0)
+			{
+				my $st = $claimList->getStatistics;
+
+				my @outArray = ();
+				if ($valMgr->haveErrors() == 0)
+				{
+					my $outResult = $output->processClaims(
+						destination => NSFDEST_FILE, 
+						outArray => \@outArray, 
+						outFile => $NSF_FILE_NAME, 
+						claimList => $claimList, 
+						validationMgr => $valMgr, 
+						nsfType => NSF_HALLEY,
+						FLAG_STRIPDASH => '1',
+					);
+
+					print " Total Claims Processed = $st->{count} \n ";
+
+				}
+				else
+				{
+					printErrors($valMgr,"NSF ERRORS");
+				}
+			}
+			else
+			{
+				printErrors($valMgr,"CLAIM VALIDATORS");
+			}
+		}
+		else
+		{
+			printErrors($valMgr,"DBI ERRORS");
+		}
+	}
+	else
+	{
+		printErrors($valMgr,"CLAIM POPULATION");
+	}
+}
+
+main(@ARGV);
+
+
+sub printErrors
+{
+	my ($valMgr, $header) = @_;
+	my $errors = $valMgr->getErrors();
+
+	open(CLAIMFILE,">./claimerror.txt");
+	print CLAIMFILE "$header","\n\n\n";
+	foreach my $error (@$errors)
+	{
+		print CLAIMFILE @$error,"\n";
+
+	}
+	close(CLAIMFILE);
+}
+
---
+++ HealthSuite/Scripts/Per-Se/update_transmitted.pl
@@ -0,0 +1,56 @@
+use strict;
+use DBI;
+
+my $batchSize = 100;
+
+my $cs = shift;
+die "Usage Example:  $0 sde_prime/sde\@sdedbs02 file.nsf" unless $cs;
+my $nsfFileName = shift;
+
+open (NSF, "$nsfFileName") || die "Unable to open '$nsfFileName': $!\n";
+
+$cs =~ /(.*?)\/(.*?)\@(.*)/;
+my ($userName, $password, $twoTask) =  ($1, $2, $3);
+
+my $dbh = DBI->connect('dbi:Oracle:' . $twoTask, $userName, $password, 
+	{ RaiseError => 1, AutoCommit => 0 }) || die "Unable To Connect to Database: $!";
+
+my @transmittedClaims = ();
+while(<NSF>)
+{
+	chomp;
+	next unless /^FA001(.*?)\s/;
+	
+	my $claimNo = $1;
+	push(@transmittedClaims, $claimNo);
+}
+
+my @sortedClaims = sort @transmittedClaims;
+print "@sortedClaims\n";
+
+my @inStrings = ();
+for(my $i=0; $i< scalar @sortedClaims; $i += $batchSize)
+{
+	my $string = qq{@{[ join(',', @sortedClaims[$i..$i+$batchSize-1]) ]}};
+	$string =~ s/,+$//;
+	push(@inStrings, "($string)");
+}
+
+my @sqlStmts = ();
+for (@inStrings)
+{
+	push(@sqlStmts, "update Invoice set invoice_status = 5 where invoice_id in " . $_);
+}
+
+for my $sqlStmt (@sqlStmts)
+{
+	my $sth = $dbh->prepare($sqlStmt);
+	my $result = $sth->execute();
+	
+	print "$sqlStmt\n";
+	print "$result\n";
+	
+	$sth->finish();
+}
+
+$dbh->disconnect();
