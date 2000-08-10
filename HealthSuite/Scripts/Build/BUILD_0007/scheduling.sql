bdf243bfb4aa292739e56de657de253f51cd24d8
thai_nguyen <thai_nguyen>
2000-08-10 23:52:11 +0000

    the sql script for scheduling for BUILD_0007

--- HealthSuite/Scripts/Build/BUILD_0007/scheduling.sql
+++ HealthSuite/Scripts/Build/BUILD_0007/scheduling.sql
@@ -1,2 +1,14 @@
-select count(*) from Event;
-select count(*) from Event_Attribute;
+update Event_Attribute
+	set value_textB = (select value_text from Event_Attribute ea
+		where ea.parent_id = Event_Attribute.parent_id
+			and ea.item_name = 'Appointment/Attendee/Physician')
+where item_name = 'Appointment/Attendee/Patient';
+
+delete from Event_Attribute where item_name = 'Appointment/Attendee/Physician';
+
+update Event_Attribute set item_name = 'Appointment';
+
+insert into Attribute_Value_Type (id, caption, dialog_params, group_name) 
+	values (333, 'Appointment', 'type="text"', 'Code');
+	
+update Event_Attribute set value_type = 333;
