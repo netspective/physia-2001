cbb65cf4da751370589ae19940c699829037fa95
thai_nguyen <thai_nguyen>
2000-08-17 21:30:54 +0000

    Added load_pre and load_post at the end

--- HealthSuite/Scripts/Build/BUILD_0008/BUILD_0008_scheduling.sql
+++ HealthSuite/Scripts/Build/BUILD_0008/BUILD_0008_scheduling.sql
@@ -6,3 +6,6 @@ insert into Session_Action_Type (id, caption) values (8, 'Verify');
 
 alter table Sch_Template_R_Ids modify (member_name VARCHAR2(64));
 alter table Appt_Type_R_Ids modify (member_name VARCHAR2(64));
+
+@load_pre.sql
+@load_post.sql
