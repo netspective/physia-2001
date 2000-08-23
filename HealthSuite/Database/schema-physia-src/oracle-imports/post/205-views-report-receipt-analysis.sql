dc2f07de305bd7cb50517451542c542024134e04
radha_kotagiri <radha_kotagiri>
2000-08-23 21:29:04 +0000

    [B682] Changed the views

--- HealthSuite/Database/schema-physia-src/oracle-imports/post/205-views-report-receipt-analysis.sql
+++ HealthSuite/Database/schema-physia-src/oracle-imports/post/205-views-report-receipt-analysis.sql
@@ -1,5 +1,6 @@
 create or replace view yearToDateReceiptAnalysis as
-select 	t.provider_id as ProviderId,
+select 	t.provider_id as ProviderId,	
+	iia.pay_date as PayDate,	
 	p.short_sortable_name as Name,
 	decode(iia.payer_type,0,'Personal Receipts', 'Insurance Receipts') as Category_Name, 
 	decode(iia.payer_type,0,pm.caption,'PAYMENT') as Transaction_Type,
@@ -13,7 +14,6 @@ where 	t.trans_id = i.main_transaction
 and 	i.invoice_id = ii.parent_id
 and 	ii.item_id = iia.parent_id
 and 	poc.person_id = t.provider_id
-and 	poc.org_internal_id = t.service_facility_id
 and	i.invoice_id = ib.invoice_id
 and 	ib.bill_ins_id = ins.ins_internal_id(+) 
 and 	poc.category = 'Physician'
@@ -21,15 +21,17 @@ and 	poc.person_id = p.person_id
 and	iia.pay_method = pm.id
 and	trunc(iia.pay_date) >= ( select to_date('01/'||'01/'||to_char(sysdate, 'yyyy'), 'mm/dd/yyyy') from dual)
 and	trunc(iia.pay_date) <= sysdate
-group by t.provider_id,short_sortable_name,decode(iia.payer_type,0,'Personal Receipts', 'Insurance Receipts'),decode(iia.payer_type,0,pm.caption,'PAYMENT'),ins.plan_name
+and 	ib.bill_sequence = 1
+and 	ib.invoice_item_id is NULL
+group by t.provider_id,iia.pay_date,short_sortable_name,decode(iia.payer_type,0,'Personal Receipts', 'Insurance Receipts'),decode(iia.payer_type,0,pm.caption,'PAYMENT'),ins.plan_name
 order by Category_Name desc;
 
 
 
 
 create or replace view monthToDateReceiptAnalysis as
-select 	iia.pay_date as PayDate,
-	t.provider_id as ProviderId,
+select 	iia.pay_date as PayDate,	
+	t.provider_id as ProviderId,	
 	p.short_sortable_name as Name,
 	decode(iia.payer_type,0,'Personal Receipts', 'Insurance Receipts') as Category_Name, 
 	decode(iia.payer_type,0,pm.caption,'PAYMENT') as Transaction_Type,
@@ -43,7 +45,6 @@ where 	t.trans_id = i.main_transaction
 and 	i.invoice_id = ii.parent_id
 and 	ii.item_id = iia.parent_id
 and 	poc.person_id = t.provider_id
-and 	poc.org_internal_id = t.service_facility_id
 and	i.invoice_id = ib.invoice_id
 and 	ib.bill_ins_id = ins.ins_internal_id(+) 
 and 	poc.category = 'Physician'
@@ -51,6 +52,8 @@ and 	poc.person_id = p.person_id
 and	iia.pay_method = pm.id
 and	trunc(iia.pay_date) >= ( select to_date(to_char(sysdate, 'mm')||'/01/'||to_char(sysdate, 'yyyy'), 'mm/dd/yyyy') from dual)
 and	trunc(iia.pay_date) <= sysdate
+and 	ib.bill_sequence = 1
+and 	ib.invoice_item_id is NULL
 group by iia.pay_date,t.provider_id,short_sortable_name,decode(iia.payer_type,0,'Personal Receipts', 'Insurance Receipts'),decode(iia.payer_type,0,pm.caption,'PAYMENT'),ins.plan_name
 order by Category_Name desc;
 
