5adec2cb473f69bea529ba84a3a7fcb236bdb5c3
radha_kotagiri <radha_kotagiri>
2000-08-29 17:33:03 +0000

    [B701] Added a new transaction to showup the Phonemessage in the User's Ph message component

--- HealthSuite/Lib/perl/App/Dialog/Transaction/PhoneMessage.pm
+++ HealthSuite/Lib/perl/App/Dialog/Transaction/PhoneMessage.pm
@@ -31,11 +31,13 @@ sub new
 	croak 'schema parameter required' unless $schema;
 
 	$self->addContent(
+		new CGI::Dialog::Field(type => 'hidden', name => 'patient_phone_message'),
 		new App::Dialog::Field::Person::ID(name => 'person_called', caption =>'Call From', options => FLDFLAG_REQUIRED, readOnlyWhen => CGI::Dialog::DLGFLAG_UPDORREMOVE),
 		new App::Dialog::Field::Person::ID(name => 'provider', caption =>'Call For', options => FLDFLAG_REQUIRED, readOnlyWhen => CGI::Dialog::DLGFLAG_UPDORREMOVE),
-		new CGI::Dialog::Field(name => 'datecalled', caption => 'Date', type => 'date'),
-		new CGI::Dialog::Field(name => 'phonemessage', caption => 'Phone Message', type => 'memo', options => FLDFLAG_REQUIRED, readOnlyWhen => CGI::Dialog::DLGFLAG_UPDORREMOVE),
-		new CGI::Dialog::Field(name => 'responsemessage', caption => 'Comments', type => 'memo',  readOnlyWhen => CGI::Dialog::DLGFLAG_UPDORREMOVE),
+		#new CGI::Dialog::Field(name => 'datecalled', caption => 'Date', type => 'date'),
+		new CGI::Dialog::Field( caption => 'Date and Time of Calling', name => 'time', readOnlyWhen => CGI::Dialog::DLGFLAG_UPDORREMOVE, options => FLDFLAG_REQUIRED),
+		new CGI::Dialog::Field(name => 'phone_message', caption => 'Phone Message', type => 'memo', options => FLDFLAG_REQUIRED, readOnlyWhen => CGI::Dialog::DLGFLAG_UPDORREMOVE),
+		new CGI::Dialog::Field(name => 'responsemessage', caption => 'Comments', type => 'memo'),
 		new CGI::Dialog::Field(type => 'select',
 				style => 'radio',
 				selOptions => 'Not Read;Read',
@@ -68,8 +70,10 @@ sub makeStateChanges
 
 	$self->SUPER::makeStateChanges($page, $command, $dlgFlags);
 
-	$self->setFieldFlags('datecalled', FLDFLAG_READONLY);
 	$self->updateFieldFlags('status', FLDFLAG_INVISIBLE, 1) if $command eq 'add';
+	my $startTime = $page->getTimeStamp();
+	$page->field('time', $startTime);
+	$self->setFieldFlags('time', FLDFLAG_READONLY);
 
 }
 
@@ -97,26 +101,30 @@ sub populateData
         if($phoneInfo->{data_num_a} eq '')
         {
 
-		$page->field('phonemessage', $phoneInfo->{data_text_b});
-		$page->field('datecalled', $phoneInfo->{trans_begin_stamp});
+		$page->field('phone_message', $phoneInfo->{data_text_b});
+		$page->field('time', $phoneInfo->{data_text_c});
+		#$page->field('datecalled', $phoneInfo->{trans_begin_stamp});
 		$page->field('provider', $phoneInfo->{provider_id});
 		$page->field('responsemessage', $phoneInfo->{data_text_a});
 		$page->field('status', $phoneStatus);
 		$page->field('data_num_b', $phoneInfo->{data_num_b});
 		$page->field('person_called', $phoneInfo->{consult_id});
 	}
+
 	elsif($phoneInfo->{data_num_a} ne '')
         {
-
-                $page->field('phonemessage', $phoneInfo->{data_text_a});
-                $page->field('datecalled', $phoneInfo->{trans_begin_stamp});
-                $page->field('provider', $phoneInfo->{trans_owner_id});
+                $page->field('phone_message', $phoneInfo->{data_text_a});
+                #$page->field('datecalled', $phoneInfo->{trans_begin_stamp});
+                $page->field('provider', $phoneInfo->{provider_id});
                 $page->field('responsemessage', $phoneInfo->{data_text_b});
+                $page->field('time', $phoneInfo->{data_text_c});
                 $page->field('status', $phoneStatus);
 		$page->field('data_num_b', $phoneInfo->{data_num_b});
 		$page->field('person_called', $phoneInfo->{consult_id});
-
         }
+
+        my $populateMessage = $page->field('phone_message');
+        $page->field('patient_phone_message', $populateMessage);
 }
 
 sub execute
@@ -126,7 +134,7 @@ sub execute
 	$page->field('status', 'Not Read')if $command eq 'add';
 	my $phoneStatus = $page->field('status') eq 'Not Read' ? 5 : 4;
 	my $status =  $page->field('status', $phoneStatus);
-
+	my $userId = $page->session('user_id');
         if($command eq 'add')
 	{
         	my $trans_id = $page->schemaAction(
@@ -138,9 +146,10 @@ sub execute
                         caption =>'Phone Message',
                         trans_type => 1000,
                         trans_status => $phoneStatus,
-                        trans_begin_stamp => $page->field('datecalled'),
+                     #   trans_begin_stamp => $page->field('datecalled'),
                         data_text_a => $page->field('responsemessage') || undef,
-                        data_text_b => $page->field('phonemessage')  || undef,
+                        data_text_b => $page->field('phone_message')  || undef,
+                        data_text_c => $page->field('time')  || undef,
                         data_num_b   => $page->field('data_num_b')  || undef,
                         consult_id  => $page->field('person_called') || undef,
                         _debug => 0
@@ -151,24 +160,49 @@ sub execute
                         trans_id => $page->param('trans_id') || undef,
                         trans_owner_id => $page->field('provider') || undef,
                         trans_owner_type => 0,
-                        provider_id => $page->param('person_id') || undef,
+                        provider_id => $page->field('provider') || undef,
                         caption =>'Phone Message',
                         trans_type => 1000,
                         trans_status => $phoneStatus,
-                        trans_begin_stamp => $page->field('datecalled'),
-                        data_text_a => $page->field('phonemessage') || undef,
+                        #trans_begin_stamp => $page->field('datecalled'),
+                        data_text_a => $page->field('phone_message') || undef,
                         data_text_b => $page->field('responsemessage')  || undef,
+                        data_text_c => $page->field('time')  || undef,
 			data_num_a => $trans_id,
 		        data_num_b   => $page->field('data_num_b')  || undef,
 		        consult_id  => $page->field('person_called') || undef,
                         _debug => 0
                 );
 
+                $page->schemaAction(
+			'Transaction', $command,
+			trans_id => $page->param('trans_id') || undef,
+			trans_owner_id => $userId || undef,
+			trans_owner_type => 0,
+			provider_id => $page->field('provider') || undef,
+			caption =>'Phone Message',
+			trans_type => 1000,
+			trans_status => $phoneStatus,
+			#trans_begin_stamp => $page->field('datecalled'),
+			data_text_a => $page->field('phone_message') || undef,
+			data_text_b => $page->field('responsemessage')  || undef,
+			data_text_c => $page->field('time')  || undef,
+			data_num_b   => $page->field('data_num_b')  || undef,
+			data_num_a => $trans_id,
+			consult_id  => $page->field('person_called') || undef,
+			_debug => 0
+                );
+
+
+
 	}
         elsif($command eq 'update' || $command eq 'remove')
         {
                 my $transId = $page->param('trans_id');
+                my $hiddenMessage = $page->field('patient_phone_message');
+		my $phone =  $page->field('phone_message', $hiddenMessage);
                 my $phoneDataInfo = $STMTMGR_TRANSACTION->getRowAsHash($page, STMTMGRFLAG_NONE, 'selTransactionById', $transId);
+
                 if($phoneDataInfo->{data_num_a} eq '')
                 {
 	                $page->schemaAction(
@@ -177,9 +211,10 @@ sub execute
                         	trans_owner_id => $page->param('person_id') || undef,
                         	provider_id => $page->field('provider') || undef,
                         	trans_status => $phoneStatus,
-                        	trans_begin_stamp => $page->field('datecalled'),
+                        	#trans_begin_stamp => $page->field('datecalled'),
                         	data_text_a => $page->field('responsemessage') || undef,
-                        	data_text_b => $page->field('phonemessage')  || undef,
+                        	data_text_b => $page->field('phone_message')  || undef,
+                        	data_text_c => $page->field('time')  || undef,
                         	data_num_b   => $page->field('data_num_b')  || undef,
                         	consult_id  => $page->field('person_called') || undef,
                         	_debug => 0
@@ -189,43 +224,82 @@ sub execute
        		                'Transaction', $command,
                		        trans_id => $physicianData->{trans_id}|| undef,
                        		trans_owner_id => $page->field('provider') || undef,
-                        	provider_id => $page->param('person_id') || undef,
+                        	provider_id => $page->field('provider') || undef,
                         	trans_status => $phoneStatus,
-                        	trans_begin_stamp => $page->field('datecalled'),
-                        	data_text_a => $page->field('phonemessage') || undef,
+                        	#trans_begin_stamp => $page->field('datecalled'),
+                        	data_text_a => $page->field('phone_message') || undef,
                         	data_text_b => $page->field('responsemessage')  || undef,
+                        	data_text_c => $page->field('time')  || undef,
                         	data_num_b   => $page->field('data_num_b')  || undef,
+                        	data_num_a   => $transId,
          	               consult_id  => $page->field('person_called') || undef,
                         	_debug => 0
                 	);
 
+                        my $userData = $STMTMGR_TRANSACTION->getRowAsHash($page, STMTMGRFLAG_NONE, 'selTransactionByUserAndData_num_a', $transId, $userId);
+
+			$page->schemaAction(
+				'Transaction', $command,
+				trans_id => $userData->{trans_id}|| undef,
+				trans_owner_id => $userId || undef,
+				provider_id => $page->field('provider') || undef,
+				caption =>'Phone Message',
+				trans_status => $phoneStatus,
+				data_text_a => $page->field('phone_message') || undef,
+				data_text_b => $page->field('responsemessage')  || undef,
+				data_text_c => $page->field('time')  || undef,
+				data_num_b   => $page->field('data_num_b')  || undef,
+				consult_id  => $page->field('person_called') || undef,
+				_debug => 0
+			);
+
                 }
                 elsif($phoneDataInfo->{data_num_a} ne '')
                 {
-                        $page->schemaAction(
-                                'Transaction', $command,
-                                trans_id => $page->param('trans_id') || undef,
-                                trans_owner_id => $page->field('provider') || undef,
-                                trans_status => $phoneStatus,
-                                trans_begin_stamp => $page->field('datecalled'),
-                                data_text_a => $page->field('phonemessage') || undef,
-                                data_text_b => $page->field('responsemessage')  || undef,
-                                data_num_b   => $page->field('data_num_b')  || undef,
-	                        consult_id  => $page->field('person_called') || undef,
-                                _debug => 0
-                        );
+			$page->schemaAction(
+				'Transaction', $command,
+				trans_id => $page->param('trans_id') || undef,
+				trans_owner_id => $page->param('person_id') || undef,
+				provider_id => $page->field('provider') || undef,
+				trans_status => $phoneStatus,
+				data_text_a => $page->field('phone_message') || undef,
+				data_text_b => $page->field('responsemessage')  || undef,
+				data_text_c => $page->field('time')  || undef,
+				data_num_b   => $page->field('data_num_b')  || undef,
+				data_num_a  => $phoneDataInfo->{'data_num_a'},
+				consult_id  => $page->field('person_called') || undef,
+				_debug => 0
+			);
+			my $user = $page->param('person_id') ne $userId ? $userId : $page->field('provider');
+			my $userData = $STMTMGR_TRANSACTION->getRowAsHash($page, STMTMGRFLAG_NONE, 'selTransactionByUserAndData_num_a', "$phoneDataInfo->{data_num_a}", $user);
+
+			$page->schemaAction(
+				'Transaction', $command,
+				trans_id => $userData->{trans_id}|| undef,
+				trans_owner_id => $user || undef,
+				provider_id => $page->field('provider') || undef,
+				caption =>'Phone Message',
+				trans_status => $phoneStatus,
+				data_text_a => $page->field('phone_message') || undef,
+				data_text_b => $page->field('responsemessage')  || undef,
+				data_text_c => $page->field('time')  || undef,
+				data_num_b   => $page->field('data_num_b')  || undef,
+				consult_id  => $page->field('person_called') || undef,
+				_debug => 0
+			);
+
                         my $parentItemId = $phoneDataInfo->{data_num_a};
                         my $personData = $STMTMGR_TRANSACTION->getRowAsHash($page, STMTMGRFLAG_NONE, 'selTransactionById', $parentItemId);
-
 
                         $page->schemaAction(
                                 'Transaction', $command,
                                 trans_id => $personData->{trans_id} || undef,
+                                trans_owner_id => $page->field('person_called') || undef,
                                 provider_id => $page->field('provider') || undef,
                                 trans_status => $phoneStatus,
-                                trans_begin_stamp => $page->field('datecalled'),
                                 data_text_a => $page->field('responsemessage') || undef,
-                                data_text_b => $page->field('phonemessage')  || undef,
+                                data_text_b => $page->field('phone_message')  || undef,
+                                data_text_c => $page->field('time')  || undef,
                                 data_num_b   => $page->field('data_num_b')  || undef,
  	                       consult_id  => $page->field('person_called') || undef,
                                 _debug => 0
@@ -234,7 +308,6 @@ sub execute
 
         }
 	$self->handlePostExecute($page, $command, $flags);
-
 }
 
 1;
