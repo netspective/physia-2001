d0769452422b9b5dc5c2872f8eaab92bc7f7822f
snshah <snshah>
2000-08-30 21:14:46 +0000

    [B470] Added new "facesheet" view

--- HealthSuite/Lib/perl/App/Page/Person.pm
+++ HealthSuite/Lib/perl/App/Page/Person.pm
@@ -35,6 +35,7 @@ use vars qw(@ISA %RESOURCE_MAP);
 			{caption => 'Account', name => 'account',},
 			{caption => 'Activity', name => 'activity',},
 			{caption => 'Home', name => 'home',},
+			{caption => 'Face Sheet', name => 'facesheet',},
 			],
 		},
 	);
@@ -144,6 +145,7 @@ sub prepare_page_content_header
 			['Chart', "$urlPrefix/chart", 'chart'],
 			['Account', "$urlPrefix/account", 'account'],
 			['Activity', "$urlPrefix/activity", 'activity'],
+			['Face Sheet', "javascript:doActionPopup(\"/person-p/$personId/facesheet\")", 'facesheet'],
 			#['Add Appointment', "$urlPrefix/appointment", 'appointment'],
 		], ' | ');
 
@@ -460,6 +462,71 @@ sub prepare_view_session
 		);
 
 	return 1;
+}
+
+sub prepare_view_facesheet
+{
+	my ($self) = @_;
+
+	my $personId = $self->param('person_id');	
+	my $accountInfo = $STMTMGR_WORKLIST_COLLECTION->recordExists($self, STMTMGRFLAG_NONE, 'selInColl', $personId) ? '<font color=red>(Account in Collection)</font>' : '';
+	my $content = qq{
+		<center><h2>Patient Profile Summary</h2></center>
+		
+		<p align=right>
+			@{[ UnixDate('today', '%g') ]}<br>
+			#session.user_id#
+		</p>
+		<table width=100%>
+			<tr valign=top>
+				<td>
+					<b>#property.person_simple_name#</b> $accountInfo<br>
+					Responsible Party: @{[ $self->property('person_responsible') || 'Self' ]}
+				</td>
+				<td>
+					ID: <b>$personId</b><br>
+					SSN: #property.person_ssn#
+				</td>
+				<td align=right>
+					DOB: #property.person_date_of_birth#<br>
+					Gender: #property.person_gender_caption#
+				</td>
+			</tr>
+		</table>
+		<p>
+		<TABLE CELLSPACING=0 BORDER=0 CELLPADDING=0>
+			<TR VALIGN=TOP>
+				<TD>
+					<font size=1 face=arial>
+					#component.stpt-person.officeLocation#
+					<p>
+					#component.stpt-person.contactMethodsAndAddresses#
+					<p>
+					#component.stpt-person.insurance#
+					<p>
+					#component.stpt-person.careProviders#
+					</font>
+				</TD>
+				<TD WIDTH=10><FONT SIZE=1>&nbsp;</FONT></TD>
+				<TD>
+					#component.stpt-person.employmentAssociations#
+					<p>
+					#component.stpt-person.emergencyAssociations#
+					<p>
+					#component.stpt-person.familyAssociations#
+					<p>
+					#component.stpt-person.accountPanel#
+				</TD>
+			</TR>
+		</TABLE>
+	};
+	
+	$self->replaceVars(\$content);
+	
+	# strip all of the images because we don't want them linked
+	#
+	$content =~ s!<A.*?><IMG.*action-(add|edit).*?></A>!!g;
+	$self->addContent($content);
 }
 
 sub prepare_view_profile
