26d7eb93eabfddc0703ba6b1499430aae07c2fac
robert_jenks <robert_jenks>
2000-08-31 14:22:06 +0000

    Made some changes which should reduce server memory user, or rather increase the amount of shared vs. real memory.  I also added config option to limit the number of requests each child can handle before being replaced by a new child.  This should also help keep any children from using more and more memory over time.

--- HealthSuite/Conf/apache/startup.pl
+++ HealthSuite/Conf/apache/startup.pl
@@ -6,9 +6,15 @@ BEGIN {
 
 print "Entering startup.pl\n";
 use strict;
-use CGI;
-use CGI::Page;
+use CGI ();
+use CGI::Page ();
+use DBI ();
 use Apache::HealthSuite::PracticeManagement;
+use Apache::Status;
+
+DBI->install_driver('Oracle');
+CGI->compile(':all');
+
 print "Leaving startup.pl\n";
 
 1;
