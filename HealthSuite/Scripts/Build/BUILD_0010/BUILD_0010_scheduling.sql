cb071cd92ccbe8c39b5a7c633ad3af358515da70
thai_nguyen <thai_nguyen>
2000-08-31 01:30:57 +0000

    BUILD_0010 DB change

---
+++ HealthSuite/Scripts/Build/BUILD_0010/BUILD_0010_scheduling.sql
@@ -0,0 +1,5 @@
+insert into Transaction_Type (id, caption, group_name, icon_img_summ, remarks) 
+	values (8025, 'Patient Accounting Alert', 'Alert', 'alert_btn.gif', 
+	'Alert requiring action on a patient account');
+	
+create index TRANS_OWNER_ID on Transaction (trans_owner_id) TABLESPACE TS_INDEXES;	
---
+++ HealthSuite/Scripts/Build/BUILD_0010/Upgrade.pl
@@ -0,0 +1,79 @@
+#!/usr/bin/perl -I.
+
+use strict;
+use Schema::API;
+use App::Data::MDL::Module;
+use App::Universal;
+use App::Configuration;
+
+use DBI::StatementManager;
+use App::Statements::Scheduling;
+
+use vars qw($page $sqlPlusKey);
+
+&connectDB();
+
+# You are now connected to the database and have
+# access to a minimal $page object.
+#
+######## BEGIN UPGRADE SCRIPT #########
+
+update_for_scheduling();
+
+
+######## END UPGRADE SCRIPT #########
+
+exit;
+
+# Subroutines
+
+sub update_for_scheduling
+{
+	my $count = $STMTMGR_SCHEDULING->getRowCount($page, STMTMGRFLAG_DYNAMICSQL,
+		q{select count(*) from Transaction_Type where id = 8025}
+	);
+
+	unless ($count)
+	{
+		my $buildDir = `pwd`;
+		chomp $buildDir;
+
+		my $sqlFile = $buildDir . '/BUILD_0010_scheduling.sql';
+		my $logFile = $buildDir . '/BUILD_0010_scheduling.log';
+	
+		die "Missing required '$sqlFile'.  Aborted.\n" unless (-f $sqlFile);
+		
+		system(qq{
+			echo "---------------------------------------" >> $logFile
+			date >> $logFile
+			echo "---------------------------------------" >> $logFile
+			$ENV{ORACLE_HOME}/bin/sqlplus -s $sqlPlusKey < $sqlFile >> $logFile 2>&1
+		});
+	}
+}
+
+sub connectDB
+{
+	$page = new App::Data::MDL::Module();
+	$page->{schema} = undef;
+	$page->{schemaFlags} = SCHEMAAPIFLAG_LOGSQL | SCHEMAAPIFLAG_EXECSQL;
+	if($CONFDATA_SERVER->db_ConnectKey() && $CONFDATA_SERVER->file_SchemaDefn())
+	{
+		my $schemaFile = $CONFDATA_SERVER->file_SchemaDefn();
+		print STDOUT "Loading schema from $schemaFile\n";
+		$page->{schema} = new Schema::API(xmlFile => $schemaFile);
+
+		my $connectKey = $CONFDATA_SERVER->db_ConnectKey();
+		print STDOUT "Connecting to $connectKey\n";
+
+		$page->{schema}->connectDB($connectKey);
+		$page->{db} = $page->{schema}->{dbh};
+		
+		$sqlPlusKey = $connectKey;
+		$sqlPlusKey =~ s/dbi:Oracle://;
+	}
+	else
+	{
+		die "DB Schema File and Connect Key are required!";
+	}
+}
