38e318fddab4299f112684a3c377f0a690d0f081
alex_hillman <alex_hillman>
2000-09-05 23:29:51 +0000

    Added drop index before creating

--- HealthSuite/Scripts/Build/BUILD_0010/BUILD_0010_scheduling.sql
+++ HealthSuite/Scripts/Build/BUILD_0010/BUILD_0010_scheduling.sql
@@ -2,4 +2,6 @@ insert into Transaction_Type (id, caption, group_name, icon_img_summ, remarks)
 	values (8025, 'Patient Accounting Alert', 'Alert', 'alert_btn.gif', 
 	'Alert requiring action on a patient account');
 	
+drop index TRANS_OWNER_ID;
+	
 create index TRANS_OWNER_ID on Transaction (trans_owner_id) TABLESPACE TS_INDEXES;	
