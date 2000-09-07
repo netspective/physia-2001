f9809ab26a965d7f5480c11c043a0f36dc06eeb9
alex_hillman <alex_hillman>
2000-09-07 23:52:51 +0000

    Initial implementation

---
+++ HealthSuite/Scripts/Build/BUILD_0012/BUILD_0012_add_attribute_value_types.sql
@@ -0,0 +1,5 @@
+insert into Attribute_Value_Type (id, caption, dialog_params, group_name, remarks)
+values (550, 'Insurance Company Provider Id', 'type="text"', 'Certificate', 'This is the id code assigned by an insurance company to a provider code which authorizes the provider to bill the insurance company for service rendered');
+insert into Attribute_Value_Type (id, caption, dialog_params, group_name, remarks)
+values (570, 'Tax Id', 'type="text"', 'Certificate', 'Code assigned by a government to a person or a corporation for tax purposes');
+commit;
