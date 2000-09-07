d2a9a52a2f8a69b03c726d0399112e1db8fbfd57
radha_kotagiri <radha_kotagiri>
2000-09-07 17:36:18 +0000

    [B874] Deleted the validate condition to add the same License name more than once

--- HealthSuite/Lib/perl/App/Dialog/Attribute/Certificate/License.pm
+++ HealthSuite/Lib/perl/App/Dialog/Attribute/Certificate/License.pm
@@ -10,6 +10,7 @@ use CGI::Dialog;
 use CGI::Validator::Field;
 use App::Dialog::Field::Attribute;
 use App::Dialog::Attribute::Certificate;
+use App::Statements::Org;
 use App::Universal;
 use Date::Manip;
 use vars qw(@ISA %RESOURCE_MAP);
@@ -33,20 +34,25 @@ sub initialize
 	$self->heading('$Command License');
 
 	$self->addContent(
-		new App::Dialog::Field::Attribute::Name(
+		new CGI::Dialog::Field(
 			type => 'select',
 			selOptions => 'DEA;DPS;Medicaid;Medicare;UPIN;Tax ID;IRS;Board Certification;BCBS;Railroad Medicare;Champus;WC#;National Provider Identification;Nursing/License;Memorial Sisters Charity;EPSDT',
 			caption => 'License',
 			name => 'value_textb',
 			options => FLDFLAG_REQUIRED,
-			readOnlyWhen => CGI::Dialog::DLGFLAG_UPDORREMOVE,
-			attrNameFmt => "#field.value_textb#",
-			fKeyStmtMgr => $STMTMGR_PERSON,
-			valueType => $self->{valueType},
-			selAttrNameStmtName => 'selAttributeByItemNameAndValueTypeAndParent'),
-
+			readOnlyWhen => CGI::Dialog::DLGFLAG_UPDORREMOVE
+		),
 		new CGI::Dialog::Field(caption => 'Number', name => 'value_text', options => FLDFLAG_REQUIRED),
 		new CGI::Dialog::Field(type => 'date', caption => 'Expiration Date', name => 'value_dateend', futureOnly => 1, defaultValue => ''),
+		new CGI::Dialog::Field(	name => 'name_sort',
+					caption => 'Facility ID',
+					fKeyStmtMgr => $STMTMGR_ORG,
+					fKeyStmt => 'selChildFacilityOrgs',
+					fKeyDisplayCol => 0,
+					fKeyValueCol => 0,
+					type => 'select',
+					fKeyStmtBindSession => ['org_internal_id'],
+					options => FLDFLAG_PREPENDBLANK),
 		new CGI::Dialog::Field(type => 'bool', name => 'value_int', caption => 'License Required',	style => 'check'),
 	);
 
