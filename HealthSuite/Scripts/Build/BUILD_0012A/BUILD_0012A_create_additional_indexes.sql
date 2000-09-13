b55b48dd0fb09c04d5f56c3979870ecc406425db
alex_hillman <alex_hillman>
2000-09-13 21:39:25 +0000

    Changed index name to trans_trans_invoice_id

--- HealthSuite/Scripts/Build/BUILD_0012A/BUILD_0012A_create_additional_indexes.sql
+++ HealthSuite/Scripts/Build/BUILD_0012A/BUILD_0012A_create_additional_indexes.sql
@@ -3,11 +3,11 @@ drop index invitem_parent_id;
 drop index invitemadj_parent_id;
 drop index invoice_client_id;
 drop index invoice_client_id_type;
-drop index trans_invoice_id;
+drop index trans_trans_invoice_id;
 
 create index trans_owner_id_trans_type on transaction (trans_owner_id, trans_type) tablespace ts_indexes;
 create index invoice_client_id_type on invoice (client_id, client_type) tablespace ts_indexes;
-create index trans_invoice_id on transaction (trans_invoice_id) tablespace ts_indexes;
+create index trans_trans_invoice_id on transaction (trans_invoice_id) tablespace ts_indexes;
 create index invitem_parent_id on invoice_item (parent_id) tablespace ts_indexes;
 create index invitemadj_parent_id on invoice_item_adjust (parent_id) tablespace ts_indexes;
 
