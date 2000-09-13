d7cc9ab6706448a8280256ef96e2f97f1f8e4b46
alex_hillman <alex_hillman>
2000-09-13 20:13:33 +0000

    Changed column invoice_id to trans_invoice_id

--- HealthSuite/Scripts/Build/BUILD_0012A/BUILD_0012A_create_additional_indexes.sql
+++ HealthSuite/Scripts/Build/BUILD_0012A/BUILD_0012A_create_additional_indexes.sql
@@ -7,7 +7,7 @@ drop index trans_invoice_id;
 
 create index trans_owner_id_trans_type on transaction (trans_owner_id, trans_type) tablespace ts_indexes;
 create index invoice_client_id_type on invoice (client_id, client_type) tablespace ts_indexes;
-create index trans_invoice_id on transaction (invoice_id) tablespace ts_indexes;
+create index trans_invoice_id on transaction (trans_invoice_id) tablespace ts_indexes;
 create index invitem_parent_id on invoice_item (parent_id) tablespace ts_indexes;
 create index invitemadj_parent_id on invoice_item_adjust (parent_id) tablespace ts_indexes;
 
