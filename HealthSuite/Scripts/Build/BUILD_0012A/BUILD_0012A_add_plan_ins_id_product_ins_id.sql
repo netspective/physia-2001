dab2ed5835fe99a620bfc52a619d9aa2dede7f39
alex_hillman <alex_hillman>
2000-09-17 17:59:30 +0000

    Initial implementation

---
+++ HealthSuite/Scripts/Build/BUILD_0012A/BUILD_0012A_add_plan_ins_id_product_ins_id.sql
@@ -0,0 +1,21 @@
+alter table insurance add (plan_ins_id number(16), product_ins_id number(16));
+alter table insurance_aud add (plan_ins_id number(16), product_ins_id number(16));
+
+
+update insurance a
+ set plan_ins_id=(select ins_internal_id from insurance b where b.ins_internal_id=a.parent_ins_id and
+                    b.record_type=2) where record_type=3;
+
+
+update insurance a
+ set product_ins_id=(select ins_internal_id from insurance b where b.ins_internal_id=a.parent_ins_id and
+                    b.record_type=1) where record_type=3 and plan_ins_id is NULL;
+
+update insurance a
+ set product_ins_id=
+  (select ins_internal_id from insurance b where b.ins_internal_id=
+    (select parent_ins_id from insurance c where c.ins_internal_id=b.plan_ins_id)
+  )
+    where plan_ins_id is not NULL and record_type=3;
+
+commit;
