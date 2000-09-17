7fc782bdef5efc344473f087d5ab7f4710f0044e
alex_hillman <alex_hillman>
2000-09-17 17:43:39 +0000

    Added trans_invoice_id to transaction_aud

--- HealthSuite/Scripts/Build/BUILD_0012A/BUILD_0012A_add_invoice_id_to_transaction.sql
+++ HealthSuite/Scripts/Build/BUILD_0012A/BUILD_0012A_add_invoice_id_to_transaction.sql
@@ -1,5 +1,10 @@
+
+
 alter table transaction add (trans_invoice_id number(16));
+
+alter table transaction_aud add (trans_invoice_id number(16));
 
 update transaction set trans_invoice_id=data_num_a where data_num_a is not null and data_num_a>0;
 
 commit;
+
