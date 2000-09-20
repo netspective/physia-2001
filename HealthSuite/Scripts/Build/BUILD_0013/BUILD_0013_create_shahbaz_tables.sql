a2100df293ce1fc2c37c45e6e2672e1f0bb6f3aa
alex_hillman <alex_hillman>
2000-09-20 21:39:52 +0000

    Initial implementation

---
+++ HealthSuite/Scripts/Build/BUILD_0013/BUILD_0013_create_shahbaz_tables.sql
@@ -0,0 +1,15 @@
+start tables/Document_Type
+start tables/Device_Type
+start tables/Device_Specification
+start tables/Device_Association
+start tables/Device_Attribute
+
+start tables-code/Device_Specification
+start tables-code/Device_Association
+
+
+start data/Document_Type
+start data/Device_Type
+start data/Device_Specification
+start data/Device_Association
+
