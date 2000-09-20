e7784128643a4f9f8dd761758a8d4e9a4cc48de6
alex_hillman <alex_hillman>
2000-09-20 23:59:22 +0000

    Added check for disabled constraints

--- HealthSuite/Scripts/Build/BUILD_0013/BUILD_0013_load_pre_post_code.sql
+++ HealthSuite/Scripts/Build/BUILD_0013/BUILD_0013_load_pre_post_code.sql
@@ -3,3 +3,5 @@
 @load_tables-code
 
 select object_type, object_name from user_objects where status='INVALID' order by object_type;
+
+select constraint_name, constraint_type from user_constraints where status='DISABLED' order by constraint_name;
