d1a124e58d840a5b1923e957bb950b8632d42432
alex_hillman <alex_hillman>
2000-09-21 22:38:55 +0000

    Added blank line

--- HealthSuite/Database/schema-physia-src/oracle-imports/post/500-index-trans-inv.sql
+++ HealthSuite/Database/schema-physia-src/oracle-imports/post/500-index-trans-inv.sql
@@ -8,5 +8,6 @@ BEGIN
 END;
 /
 
+
 create index trans_owner_id_trans_type on transaction (trans_owner_id, trans_type) tablespace ts_indexes;
 create index invoice_client_id_type on invoice (client_id, client_type) tablespace ts_indexes;
