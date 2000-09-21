1521efe420300eb1c45fe20776b1e2874252994b
shahbaz_javeed <shahbaz_javeed>
2000-09-21 16:45:50 +0000

    * B275: This prints out superbills on preprinted forms

---
+++ HealthSuite/Lib/perl/App/Dialog/Report/Org/General/Billing/Superbill.pm
@@ -0,0 +1,139 @@
+##############################################################################
+package App::Dialog::Report::Org::General::Billing::Superbill;
+##############################################################################
+
+use strict;
+use Carp;
+use App::Dialog::Report;
+use App::Dialog::Field::Person;
+use App::Universal;
+
+use CGI::Dialog;
+use CGI::Validator::Field;
+
+use DBI::StatementManager;
+use App::Statements::Report::Accounting;
+use App::Statements::Org;
+use App::Statements::Device;
+use Data::Publish;
+use App::Configuration;
+use App::Device;
+
+use vars qw(@ISA $INSTANCE);
+
+@ISA = qw(App::Dialog::Report);
+
+sub new
+{
+	my $self = App::Dialog::Report::new(@_, id => 'test_sj', heading => 'Superbills');
+
+	$self->addContent(
+	
+		new CGI::Dialog::Field(
+			name => 'startDate',
+			type => 'date',
+			caption => 'Date'
+		),
+
+		new CGI::Dialog::Field(
+			name => 'printReport',
+			type => 'bool',
+			style => 'check',
+			caption => 'Print report',
+			defaultValue => 0
+		),
+
+		new CGI::Dialog::Field(
+			caption =>'Printer',
+			name => 'printerQueue',
+			options => FLDFLAG_PREPENDBLANK,
+			fKeyStmtMgr => $STMTMGR_DEVICE,
+			fKeyStmt => 'sel_org_devices',
+			fKeyDisplayCol => 0
+		),
+	);
+
+	$self->addFooter(new CGI::Dialog::Buttons);
+	$self;
+}
+
+sub populateData
+{
+	my ($self, $page, $command, $activeExecMode, $flags) = @_;
+
+	my $startDate = $page->getDate();
+	$page->field('date', $startDate);
+}
+
+sub execute
+{
+	my ($self, $page, $command, $flags) = @_;
+
+	my $startDate = $page->field('startDate');
+	my $hardCopy = $page->field('printReport');
+
+	# Data placeholders...
+	my $superbillData;
+	my $textOutput;
+
+	# Get a printer device handle...
+	my $printerAvailable = 1;
+	my $printerDevice;
+	$printerDevice = ($page->field('printerQueue') ne '') ? $page->field('printerQueue') : App::Device::getPrinter ($page, 0);
+	my $printHandle = App::Device::openPrintHandle ($printerDevice);
+	
+	$printerAvailable = 0 if (ref $printHandle eq 'SCALAR');
+
+	# Create a filename and the corresponding file...
+	my $tempFileOpened = 1;
+	my $tempDir = $CONFDATA_SERVER->path_temp();
+	my $theFilename .= "/" . $page->session ('org_id') . $page->session ('user_id') . time() . ".txt";
+	
+	open (TMPFILEHANDLE, ">$tempDir$theFilename") or $tempFileOpened = 0;
+	
+	$superbillData = $STMTMGR_REPORT_ACCOUNTING->getRowsAsArray($page, STMTMGRFLAG_NONE, 'sel_patient_superbill_info', $startDate);
+
+	foreach my $superbillPatientInfo (@{$superbillData}) {
+		my $patientID = $superbillPatientInfo->[8];
+
+		my $superbillAcctInfo = $STMTMGR_REPORT_ACCOUNTING->getRowAsArray($page, STMTMGRFLAG_NONE, 'sel_aged_patient', $patientID, $page->session ('org_internal_id'));
+
+		if ($#$superbillAcctInfo >= 0) {
+			push @{$superbillPatientInfo},
+				($superbillAcctInfo->[4] + $superbillAcctInfo->[5] + $superbillAcctInfo->[6] + $superbillAcctInfo->[7]),
+				$superbillAcctInfo->[3], $superbillAcctInfo->[8], $superbillAcctInfo->[2], $superbillAcctInfo->[9];
+		} else {
+			push @{$superbillPatientInfo}, 0, 0, 0, 0, 0;
+		}
+
+		my $superbillInsInfo = $STMTMGR_REPORT_ACCOUNTING->getRowAsArray($page, STMTMGRFLAG_NONE, 'sel_patient_superbill_ins_info', $patientID);
+
+		if ($#$superbillInsInfo >= 0) {
+			push @{$superbillPatientInfo},
+				$superbillInsInfo->[1], $superbillInsInfo->[2], $superbillInsInfo->[3];
+		} else {
+			push @{$superbillPatientInfo}, " ", " ", 9;
+		}
+	}
+
+	$textOutput = createTextFromData($page, STMTMGRFLAG_NONE, $superbillData, $STMTMGR_REPORT_ACCOUNTING->{"_dpd_sel_patient_superbill_info"});
+	if ($tempFileOpened) {
+		print TMPFILEHANDLE $textOutput;
+		close TMPFILEHANDLE
+	}
+
+	if ($hardCopy == 1 and $printerAvailable) {
+		print $printHandle $textOutput;
+		$printHandle->close;
+	}
+
+	my $html = createHtmlFromData ($page, STMTMGRFLAG_NONE, $superbillData, $STMTMGR_REPORT_ACCOUNTING->{"_dpd_sel_patient_superbill_info"});
+
+#	return ($tempFileOpened ? qq{<a href="/temp$theFilename">Printable version</a> <br>} : "" ) . qq{<br><b>Printer Device: </b> oki<br><b>printerAvailable: </b> $printerAvailable<br>} . $html;
+	return ($tempFileOpened ? qq{<a href="/temp$theFilename">Printable version</a> <br>} : "" ) . $html;
+}
+
+# create a new instance which will automatically add it to the directory of
+# reports
+#
+$INSTANCE = new __PACKAGE__;
