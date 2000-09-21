8c6e8e06561f8eb403015b6f1576cf0a9cce3253
alex_hillman <alex_hillman>
2000-09-21 20:20:08 +0000

    Added dropping of indexes before creation

--- HealthSuite/Database/schema-physia-src/oracle-imports/post/500-index-trans-inv.sql
+++ HealthSuite/Database/schema-physia-src/oracle-imports/post/500-index-trans-inv.sql
@@ -1,2 +1,12 @@
+BEGIN
+	execArbitrarySql('drop index trans_owner_id_trans_type');
+END;
+/
+
+BEGIN
+	execArbitrarySql('drop index invoice_client_id_type');
+END;
+/
+
 create index trans_owner_id_trans_type on transaction (trans_owner_id, trans_type) tablespace ts_indexes;
 create index invoice_client_id_type on invoice (client_id, client_type) tablespace ts_indexes;
--- HealthSuite/Database/schema-physia-src/oracle-imports/post/510-index-uppercase.sql
+++ HealthSuite/Database/schema-physia-src/oracle-imports/post/510-index-uppercase.sql
@@ -1,5 +1,14 @@
-drop index PER_NAME_LAST_UPP_IND;
+BEGIN
+	execArbitrarySql('drop index PER_NAME_LAST_UPP_IND');
+END;
+/
+
+BEGIN
+	execArbitrarySql('drop index PER_ATRR_VALUE_TEXT_UPP_IND;');
+END;
+/
+
+
 create index PER_NAME_LAST_UPP_IND on person (upper(name_last));
 
-drop index PER_ATRR_VALUE_TEXT_UPP_IND;
 create index PER_ATRR_VALUE_TEXT_UPP_IND on person_attribute (upper(value_text));
