c2d389421d16885d8025a7d14bed03f1a9451346
radha_kotagiri <radha_kotagiri>
2000-09-21 22:26:12 +0000

    [B461] CHanged the url to have upper R and D in 'Referring Physician'

--- HealthSuite/Lib/perl/App/Page/Search/Person.pm
+++ HealthSuite/Lib/perl/App/Page/Search/Person.pm
@@ -17,7 +17,7 @@ use vars qw(@ISA %RESOURCE_MAP);
 	'search/staff' => {},
 	'search/nurse' => {},
 	'search/associate' => {},
-	'search/referring-Doctor' => {},
+	'search/Referring-Doctor' => {},
 	);
 
 sub getForm
