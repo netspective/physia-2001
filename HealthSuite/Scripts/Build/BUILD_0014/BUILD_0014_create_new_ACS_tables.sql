60a330e03df7ce1973d698745df1902b05d4a39f
alex_hillman <alex_hillman>
2000-10-05 15:59:07 +0000

    nit. implm.

---
+++ HealthSuite/Scripts/Build/BUILD_0014/BUILD_0014_create_new_ACS_tables.sql
@@ -0,0 +1,18 @@
+start tables/Referral_Source_Type
+start tables/Referral_Result
+start tables/Referral_Followup_Status
+start tables/Referral_Unit_Type
+start tables/Referral_Service_Descr
+start tables/Intake_Detail
+start tables/Intake_Client
+
+
+start data/Referral_Source_Type
+start data/Referral_Result
+start data/Referral_Followup_Status
+start data/Referral_Unit_Type
+start data/Referral_Service_Descr
+start data/Intake_Detail
+start data/Intake_Client
+
+commit;
