6b6707396e0526e23389a3b2a7ac6164d2d0d386
alex_hillman <alex_hillman>
2000-10-05 15:56:54 +0000

    Initial implm.

--- HealthSuite/Scripts/Build/BUILD_0014/BUILD_0014_add_indexes.sql
+++ HealthSuite/Scripts/Build/BUILD_0014/BUILD_0014_add_indexes.sql
@@ -1,3 +1,8 @@
+exec execarbitrarysql('drop index TRANS_TRANS_TYPE');
+exec execarbitrarysql('drop index EVENT_START_TIME');
+exec execarbitrarysql('drop index SESACT_PERSON_ID');
+exec execarbitrarysql('drop index SESACT_ACTIVITY_STAMP');
+
 create index TRANS_TRANS_TYPE on Transaction (trans_type) TABLESPACE TS_INDEXES;
 create index EVENT_START_TIME on Event (start_time) TABLESPACE TS_INDEXES;
 create index SESACT_PERSON_ID on PerSess_Activity (person_id) TABLESPACE TS_INDEXES;
