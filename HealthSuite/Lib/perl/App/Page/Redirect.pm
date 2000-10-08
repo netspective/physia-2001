4a2197b709544219c0a92d7f02d7e6ea8d4ae7ee
robert_jenks <robert_jenks>
2000-10-08 21:55:14 +0000

    [B1166] Added code to switch between login orgs (resets cookies and permissions)

--- HealthSuite/Lib/perl/App/Page/Redirect.pm
+++ HealthSuite/Lib/perl/App/Page/Redirect.pm
@@ -5,6 +5,9 @@ package App::Page::Redirect;
 use strict;
 use App::Page;
 use App::Universal;
+use DBI::StatementManager;
+use App::Statements::Org;
+use App::Statements::Person;
 
 use vars qw(@ISA %RESOURCE_MAP);
 @ISA = qw(App::Page);
@@ -20,6 +23,26 @@ sub prepare
 {
 	my ($self) = @_;
 	my $rsrc = $self->param('arl_resource');
+
+	if (my $newOrgId = $self->param('_switchTo'))
+	{
+		if (my $newOrgIntId = $STMTMGR_ORG->getSingleValue($self, STMTMGRFLAG_CACHE, 'selOwnerOrgId', $newOrgId))
+		{
+			if (my $categories = $STMTMGR_PERSON->getSingleValueList($self, STMTMGRFLAG_CACHE, 'selCategory', $self->session('person_id'), $newOrgIntId))
+			{
+				$self->session('org_id', $newOrgId);
+				$self->session('org_internal_id', $newOrgIntId);
+				$self->session('categories', $categories);
+				$self->addCookie(-name => 'defaultOrg', -value => $newOrgId, -expires => '+1y');
+
+				# Reset their permissions
+				$self->session('aclPermissions', undef);
+				$self->session('aclRoleNames', undef);
+				$self->setupACL();
+			}
+		}
+
+	}
 
 	# when coming from a login, redirect will already be set, so check for it
 	unless($self->{page_redirect})
