d8582ebb9fd90429bdfbe0a07336e06e38c3f4ff
munir_faridi <munir_faridi>
2000-10-10 15:43:56 +0000

    [B1287] Fixed bad sql statement.

--- HealthSuite/Lib/perl/App/Statements/Search/Session.pm
+++ HealthSuite/Lib/perl/App/Statements/Search/Session.pm
@@ -16,19 +16,19 @@ my $LIMIT = App::Universal::SEARCH_RESULTS_LIMIT;
 $STMTFMT_SEL_SESSION = qq{
 	SELECT
 		p.person_id,
-		short_sortable_name,
-		TO_CHAR(first_access, '$SQLSTMT_DEFAULTSTAMPFORMAT') first_access,
-		TO_CHAR(last_access, '$SQLSTMT_DEFAULTSTAMPFORMAT') last_access,
-		remote_host,
-		remote_addr
+		p.short_sortable_name,
+		TO_CHAR(ps.first_access, '$SQLSTMT_DEFAULTSTAMPFORMAT') as first_access,
+		TO_CHAR(ps.last_access, '$SQLSTMT_DEFAULTSTAMPFORMAT') as last_access,
+		ps.remote_host,
+		ps.remote_addr
 	FROM
 		person_session ps,
 		person p
 	WHERE
-		(status between ? AND ?)
+		(ps.status between ? AND ?)
 		AND p.person_id = ps.person_id
 		AND	%whereCond%
-		AND org_id = ?
+		AND ps.org_internal_id = ?
 		AND rownum <= $LIMIT
 };
 
