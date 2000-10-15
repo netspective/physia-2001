26830ca618c5493be6809988f6ded5e0c9e3ed07
shahbaz_javeed <shahbaz_javeed>
2000-10-15 22:36:27 +0000

    B758: Added an ASCII version of the report

--- HealthSuite/Lib/perl/App/Dialog/Report/Org/General/Accounting/AppointmentCharges.pm
+++ HealthSuite/Lib/perl/App/Dialog/Report/Org/General/Accounting/AppointmentCharges.pm
@@ -11,6 +11,11 @@ use CGI::Dialog;
 use CGI::Validator::Field;
 
 use App::Statements::Component::Invoice;
+use Data::Publish;
+use Data::TextPublish;
+use App::Configuration;
+use App::Device;
+use App::Statements::Device;
 
 use vars qw(@ISA $INSTANCE);
 
@@ -29,6 +34,22 @@ sub new
 				readOnlyWhen => CGI::Dialog::DLGFLAG_UPDORREMOVE,
 				invisibleWhen => CGI::Dialog::DLGFLAG_ADD
 				),
+			new CGI::Dialog::Field(
+				name => 'printReport',
+				type => 'bool',
+				style => 'check',
+				caption => 'Print report',
+				defaultValue => 0
+			),
+
+			new CGI::Dialog::Field(
+				caption =>'Printer',
+				name => 'printerQueue',
+				options => FLDFLAG_PREPENDBLANK,
+				fKeyStmtMgr => $STMTMGR_DEVICE,
+				fKeyStmt => 'sel_org_devices',
+				fKeyDisplayCol => 0
+			),
 			);
 	$self->addFooter(new CGI::Dialog::Buttons);
 
@@ -53,9 +74,62 @@ sub execute
 	my $reportBeginDate = $page->field('daily_begin_date');
 	my $reportEndDate = $page->field('daily_end_date');
 
-	return $STMTMGR_COMPONENT_INVOICE->createHtml($page, 0, 'invoice.appointmentCharges', 
-		[$reportBeginDate, $reportEndDate, $page->session('org_internal_id'), $page->session('GMT_DAYOFFSET')]);
+	my $hardCopy = $page->field('printReport');
+	my $html;
+	my $textOutputFilename;
 
+	# Get a printer device handle...
+	my $printerAvailable = 1;
+	my $printerDevice;
+	$printerDevice = ($page->field('printerQueue') ne '') ? $page->field('printerQueue') : App::Device::getPrinter ($page, 0);
+	my $printHandle = App::Device::openPrintHandle ($printerDevice, "-o cpi=17 -o lpi=6");
+	
+	$printerAvailable = 0 if (ref $printHandle eq 'SCALAR');
+
+	my $data = $STMTMGR_COMPONENT_INVOICE->getRowsAsArray($page, 0, 'invoice.appointmentCharges', 
+		$reportBeginDate, $reportEndDate, $page->session('org_internal_id'), $page->session('GMT_DAYOFFSET'));
+
+#	return $STMTMGR_COMPONENT_INVOICE->createHtml($page, 0, 'invoice.appointmentCharges', 
+#		[$reportBeginDate, $reportEndDate, $page->session('org_internal_id'), $page->session('GMT_DAYOFFSET')]);
+
+	my $pub = {
+		columnDefn => [
+			{ colIdx => 11, head => 'Patient ID', dataFmt => '#11#',},
+			{ colIdx => 0, head => 'Receptionist', dataFmt => '#0#' },
+			{ colIdx => 1, head => 'Date', dataFmt => '#1#' },
+			{ colIdx => 2, head => 'Start Time', dataFmt => '#2#' },
+			{ colIdx => 3, head => 'End Time', dataFmt => '#3#' },
+			{ colIdx => 4, head => 'Org', dataFmt => '#4#' },
+			{ colIdx => 5, head => 'Reason', dataFmt => '#5#' },
+			#{ colIdx => 6, head => 'Visit Type', dataFmt => '#6#' },
+			{ colIdx => 7, head => 'Visit Type', dataFmt => '#7#' },
+			{ colIdx => 8, head => 'Provider Name', dataFmt => '#8#' },
+			{ colIdx => 9, head => 'Billed To', dataFmt => '#9#' },
+			{ colIdx => 10, head => 'Charges', dataFmt => '#10#', dformat => 'currency'},
+		],
+	};
+
+	$html .= createHtmlFromData($page, 0, $data, $pub);
+	$textOutputFilename = createTextRowsFromData($page, 0, $data, $pub);
+
+#	if ($tempFileOpened) {
+#		print TMPFILEHANDLE $textOutput;
+#		close TMPFILEHANDLE
+#	}
+
+	if ($hardCopy == 1 and $printerAvailable) {
+		my $reportOpened = 1;
+		my $tempDir = $CONFDATA_SERVER->path_temp();
+		open (ASCIIREPORT, $tempDir.$textOutputFilename) or $reportOpened = 0;
+		if ($reportOpened) {
+			while (my $reportLine = <ASCIIREPORT>) {
+				print $printHandle $reportLine;
+			}
+		}
+		close ASCIIREPORT;
+	}
+
+	return ($textOutputFilename ? qq{<a href="/temp$textOutputFilename">Printable version</a> <br>} : "" ) . $html;
 }
 
 
