227c8a52acf5d75f9b239c1de833f0c8526c49bf
frank_major <frank_major>
2000-10-19 23:56:26 +0000

    [B1142] Rolled Year to date data to fiscal year of org

--- HealthSuite/Lib/perl/App/Dialog/Report/Org/General/Accounting/ProcedureAnalysis.pm
+++ HealthSuite/Lib/perl/App/Dialog/Report/Org/General/Accounting/ProcedureAnalysis.pm
@@ -6,7 +6,8 @@ use strict;
 use Carp;
 use App::Dialog::Report;
 use App::Universal;
-
+use Date::Calc qw(:all);
+use Date::Manip;
 use CGI::Dialog;
 use CGI::Validator::Field;
 use DBI::StatementManager;
@@ -60,124 +61,111 @@ sub execute
 {
 	my ($self, $page, $command, $flags) = @_;
 
-	my $personId = $page->field('person_id');
-#	my $batchDate = $page->field('batch_date');
+	my $personId = $page->field('person_id');;
 	my $batch_from = $page->field('batch_begin_date');
 	my $batch_to = $page->field('batch_end_date');	
-	#my $orgId = $page->field('org_id');
 	my $cptFrom = $page->field('cpt_from');
 	my $cptTo = $page->field('cpt_to');
-	my $orgIntId=$page->field('org_id');		
-	my $allPub =
+	my $orgIntId=$page->field('org_id');	
+	my $inc_date = 0;
+	if($batch_from eq $batch_to)
 	{
+		$inc_date = 1;
+	}	
+	my $allPub;
+	if (!$inc_date)
+	{
+	 $allPub =
+		{
 		columnDefn =>
 			[
-			{ colIdx => 0, head => 'Physician Name', groupBy => '#0#', dAlign => 'LEFT' },			
-			{ colIdx => 1, head => 'Visit Type' ,groupBy => '#1#'},
-			{ colIdx => 2, head => 'Code' },
-			{ colIdx => 3, head => 'Name'  },
-			{ colIdx => 4, head => 'Batch Date' },	
-			{ colIdx => 5, head => 'Batch Units' ,summarize => 'sum',  },						
-			{ colIdx => 6, head => 'Batch Cost' ,summarize => 'sum', dformat => 'currency' },
-			{ colIdx => 7, head => 'Mth Units', hHints=>'Month To Date Units'  },
-			{ colIdx => 8, head => 'Mth Unit Cost', hHints=>'Month To Date Unit Cost',dformat => 'currency' },
-			{ colIdx => 9, head => 'Yr Units', hHints=>'Year To Date Units'},
-			{ colIdx => 10, head => 'Yr Unit Cost', hHints=>'Year To Date Unit Cost' ,dformat => 'currency'},
+			{ colIdx => 0, ,hAlign=>'left',head => 'Physician Name', groupBy => '#0#', dAlign => 'LEFT' },			
+			{ colIdx => 1,,hAlign=>'left', head => 'Visit Type' ,groupBy => '#1#'},
+			{ colIdx => 2,,hAlign=>'left', head => 'Code' },
+			{ colIdx => 3,,hAlign=>'left', head => 'Name'  },
+			{ colIdx => 4, head => 'Mth Units', summarize => 'sum',hHints=>'Month To Date Units'  },
+			{ colIdx => 5, head => 'Mth Unit Cost', summarize => 'sum',hHints=>'Month To Date Unit Cost',dformat => 'currency' },
+			{ colIdx => 6, head => 'Yr Units', summarize => 'sum',hHints=>'Year To Date Units'},
+			{ colIdx => 7, head => 'Yr Unit Cost',summarize => 'sum', hHints=>'Year To Date Unit Cost' ,dformat => 'currency'},
 		],
-	};	
+		};	
+	}
+	else
+	{
+	 $allPub =
+		{
+		columnDefn =>
+			[
+			{ colIdx => 0, ,hAlign=>'left',head => 'Physician Name', groupBy => '#0#', dAlign => 'LEFT' },			
+			{ colIdx => 1, ,hAlign=>'left',head => 'Visit Type' ,groupBy => '#1#'},
+			{ colIdx => 2, ,hAlign=>'left',head => 'Code' },
+			{ colIdx => 3, ,hAlign=>'left',head => 'Name'  },
+			{ colIdx => 8, ,hAlign=>'left',head => 'Batch Date' },	
+			{ colIdx => 9, ,hAlign=>'left',head => 'Batch Units' ,summarize => 'sum',  },						
+			{ colIdx => 10, head => 'Batch Cost' ,summarize => 'sum', dformat => 'currency' },
+			{ colIdx => 4, head => 'Mth Units',summarize => 'sum', hHints=>'Month To Date Units'  },
+			{ colIdx => 5, head => 'Mth Unit Cost', summarize => 'sum',hHints=>'Month To Date Unit Cost',dformat => 'currency' },
+			{ colIdx => 6, head => 'Yr Units', summarize => 'sum',hHints=>'Year To Date Units'},
+			{ colIdx => 7, head => 'Yr Unit Cost',summarize => 'sum', hHints=>'Year To Date Unit Cost' ,dformat => 'currency'},
+		],
+		};	
+	
+	}
+
+
+	my $orgInternalId = $page->field('org_id') || $page->session('org_internal_id');
+	
+	#Get Fiscal year for Main Org
+	my $fiscal = $STMTMGR_ORG->getRowAsHash($page,STMTMGRFLAG_NONE,'selAttribute',$orgInternalId,'Fiscal Month');	
+	
+	#month is 1 less so add a 1
+	my $month = $fiscal->{value_int}+1;	
+	
+	#Check if end date is less then fiscal month if so go back one year
+	my @start_Date = Decode_Date_US($batch_from);
+	if ($start_Date[1] < $month)
+	{
+		#Go back one year
+		$start_Date[0]--;
+	}
+	#Use Fiscal month
+	$start_Date[1] = $month;
+	
+	#First of fiscal month
+	$start_Date[2] = 1;
+	
+	my $startDate = sprintf("%02d/%02d/%04d", $start_Date[1],$start_Date[2],$start_Date[0]);
+
+	#Convert to first day of fiscal month
+	$batch_from =$startDate;
 
 	
 	#$STMTMGR_ORG->getSingleValue($page, STMTMGRFLAG_NONE, 'selOrgId', $page->session('org_internal_id'), $orgId) if $orgId;
 	my $rcpt = $STMTMGR_REPORT_ACCOUNTING->getRowsAsHashList($page, STMTMGRFLAG_NONE, 'procAnalysis', $personId,$batch_from,$batch_to,	
 		$orgIntId,$cptFrom,$cptTo,$page->session('org_internal_id'));
 	my @data = ();
-	my $month_cost=0;
-	my $year_cost=0;
-	my $month_units=0;
-	my $year_units=0;	
-	my $track_doc=undef;	
-	my $track_type=undef;
-	my $track_code=undef;
-	my $track_proc=undef;
-	my $track_year=undef;
-	my $track_month=undef;	
 	foreach (@$rcpt)
 	{
-		#Track informatiom so we can sum it up for month and year info
-		if(! defined $track_doc)
-		{
-			$track_doc=$_->{short_sortable_name};
-			$track_type=$_->{visit_type};
-			$track_code=$_->{code};
-			$track_proc=$_->{proc};
-			$track_year=$_->{year_date};
-			$track_month=$_->{month_date};
-			$month_units = $_->{units};
-			$year_units = $_->{units};			
-			$month_cost = $_->{unit_cost};
-			$year_cost = $_->{unit_cost};			
-			
-		}		
-		elsif ($track_doc ne $_->{short_sortable_name} || $track_type ne $_->{visit_type} )
-		{
-			
-			$track_doc=$_->{short_sortable_name};
-			$track_type=$_->{visit_type};
-			$track_code=$_->{code};
-			$track_proc=$_->{proc};
-			$track_year=$_->{year_date};
-			$track_month=$_->{month_date};
-			$month_units = $_->{units};
-			$year_units = $_->{units};			
-			$month_cost = $_->{unit_cost};
-			$year_cost = $_->{unit_cost};				
-		}
-		elsif ($track_year ne $_->{year_date} )
-		{
-			$track_doc=$_->{short_sortable_name};
-			$track_type=$_->{visit_type};
-			$track_code=$_->{code};
-			$track_proc=$_->{proc};
-			$track_year=$_->{year_date};
-			$track_month=$_->{month_date};
-			$month_units = $_->{units};
-			$year_units = $_->{units};			
-			$month_cost = $_->{unit_cost};
-			$year_cost = $_->{unit_cost};			
-		}
-		elsif ($track_month ne $_->{month_date})
-		{
-			$track_month=$_->{month_date};
-			$month_units = $_->{units};
-			$month_cost = $_->{unit_cost};			
-			$year_units += $_->{units};			
-			$year_cost += $_->{unit_cost};	
-		}
-		else
-		{
-			$month_units += $_->{units};
-			$month_cost += $_->{unit_cost};			
-			$year_units += $_->{units};				
-			$year_cost += $_->{unit_cost};	
-		};		
-			
 		my @rowData =
 		(
-			$_->{short_sortable_name},
+			$_->{simple_name},
 			$_->{visit_type},
 			$_->{code},
 			$_->{proc},
-			$_->{invoice_date},
-			$_->{units},			
-			$_->{unit_cost},			
-			$month_units,
-			$month_cost,
-			$year_units,
-			$year_cost
+			$_->{month_units},
+			$_->{month_cost},
+			$_->{year_units},
+			$_->{year_cost},
+			$batch_to,
+			$_->{batch_units},
+			$_->{batch_cost},
+			
 		);
 		push(@data, \@rowData);		
 	}
-	return createHtmlFromData($page, 0, \@data,$allPub);		
+	my $html = '<br> <b style="font-family:Helvetica; font-size:12pt">(Fiscal Range '. $batch_from .' - ' . $batch_to . ' ) </b><br>';
+	$html .= createHtmlFromData($page, 0, \@data,$allPub);
+	return $html;
 }
 
 
--- HealthSuite/Lib/perl/App/Dialog/Report/Org/General/Accounting/ReceiptAnalysis.pm
+++ HealthSuite/Lib/perl/App/Dialog/Report/Org/General/Accounting/ReceiptAnalysis.pm
@@ -4,11 +4,14 @@ package App::Dialog::Report::Org::General::Accounting::ReceiptAnalysis;
 
 use strict;
 use Carp;
+use Date::Calc qw(:all);
+use Date::Manip;
 use App::Dialog::Report;
 use App::Universal;
 use App::Configuration;
 use App::Device;
 use App::Statements::Device;
+use App::Statements::Org;
 use Data::Publish;
 use Data::TextPublish;
 use CGI::Dialog;
@@ -26,7 +29,7 @@ sub new
 	my $self = App::Dialog::Report::new(@_, id => 'rpt-acct-receipt-analysis', heading => 'Receipt Analysis');
 
 	$self->addContent(
-			new App::Dialog::Field::Person::ID(caption =>'Provider ID', name => 'person_id', invisibleWhen => CGI::Dialog::DLGFLAG_UPDORREMOVE),
+			new App::Dialog::Field::Person::ID(caption =>'Provider ID',types => ['Physician'], name => 'person_id', invisibleWhen => CGI::Dialog::DLGFLAG_UPDORREMOVE),
 			new CGI::Dialog::Field(caption =>'Payment Type',
 					name => 'transaction_type',
 					options => FLDFLAG_PREPENDBLANK,
@@ -37,11 +40,12 @@ sub new
 
 			new CGI::Dialog::Field(caption => 'Batch ID', size => 12,name=>'batch_id'),					
 			new CGI::Dialog::Field::Duration(
+				options=>FLDFLAG_REQUIRED,
 				name => 'batch',
 				caption => 'Batch Report Date',
 				begin_caption => 'Report Begin Date',
 				end_caption => 'Report End Date',
-				),
+				),		
 			new CGI::Dialog::Field(
 				name => 'printReport',
 				type => 'bool',
@@ -57,6 +61,7 @@ sub new
 				fKeyStmt => 'sel_org_devices',
 				fKeyDisplayCol => 0
 			),
+			
 			);
 	$self->addFooter(new CGI::Dialog::Buttons);
 
@@ -71,9 +76,9 @@ sub execute
 	my $provider = $page->field('person_id');
 	my $receipt = $page->field('transaction_type');
 	my $batch_id = $page->field('batch_id');
-	my $reportBeginDate = $page->field('batch_begin_date')||'01/01/1800';
-	my $reportEndDate = $page->field('batch_end_date')||'01/01/9999';
-
+	my $reportBeginDate = $page->field('batch_begin_date');
+	my $reportEndDate = $page->field('batch_end_date');
+	my $inc_date=0;
 	my $hardCopy = $page->field('printReport');
 	# Get a printer device handle...
 	my $printerAvailable = 1;
@@ -84,97 +89,110 @@ sub execute
 	$printerAvailable = 0 if (ref $printHandle eq 'SCALAR');
 
 
-	my $allPub =
+	if($reportEndDate eq $reportBeginDate)
 	{
+		$inc_date = 1;
+	}
+	my $allPub;
+	if (!$inc_date)
+	{ $allPub =
+		{
 		reportTitle => $self->heading(),
 		columnDefn =>
 			[
-			{ colIdx => 0, head => 'Physician Name', groupBy => '#0#', dAlign => 'LEFT' },			
-			{ colIdx => 1, head => 'Category' ,groupBy => '#1#'},
-			{ colIdx => 2, head => 'Tranaction Type',groupBy => '#2#' },
-			{ colIdx => 3, head => 'Payer Name',groupBy => 'Sub-Total'  },
-			{ colIdx => 4, head => 'Batch Date' },			
-			{ colIdx => 5, head => 'Batch Date Rcpt', summarize => 'sum', dformat => 'currency' },
-			{ colIdx => 6, head => 'Month Rcpt',  dformat => 'currency' },
-			{ colIdx => 7, head => 'Year Rcpt', dformat => 'currency' },
+			{ colIdx => 0,hAlign=>'left', head => 'Physician Name', groupBy => '#0#', dAlign => 'LEFT' },			
+			{ colIdx => 1, ,hAlign=>'left',head => 'Category' ,groupBy => '#1#'},
+			{ colIdx => 2, ,hAlign=>'left',head => 'Tranaction Type',groupBy => '#2#' },
+			{ colIdx => 3,,hAlign=>'left', head => 'Payer Name',groupBy => 'Sub-Total'  },
+			{ colIdx => 4, head => 'Month Rcpt',  summarize => 'sum',dformat => 'currency' },
+			{ colIdx => 5, head => 'Year Rcpt',summarize => 'sum',dformat => 'currency' },
 		],
-	};	
+		};
+	}
+	else
+	{
+	   $allPub =
+		{
+			reportTitle => $self->heading(),
+			columnDefn =>
+				[
+			{ colIdx => 0,hAlign=>'left', head => 'Physician Name', groupBy => '#0#', dAlign => 'LEFT' },			
+			{ colIdx => 1, ,hAlign=>'left',head => 'Category' ,groupBy => '#1#'},
+			{ colIdx => 2, ,hAlign=>'left',head => 'Tranaction Type',groupBy => '#2#' },
+			{ colIdx => 3,,hAlign=>'left', head => 'Payer Name',groupBy => 'Sub-Total'  },
+			{ colIdx => 6, head => 'Batch Date' },			
+			{ colIdx => 7, head => 'Batch Date Rcpt', summarize => 'sum', dformat => 'currency' },			
+			{ colIdx => 4, head => 'Month Rcpt',  summarize => 'sum',dformat => 'currency' },
+			{ colIdx => 5, head => 'Year Rcpt',summarize => 'sum',dformat => 'currency' },
+			],
+		};	
+	}
 	my $orgInternalId = $page->session('org_internal_id');
-	my $rcpt  = $STMTMGR_REPORT_ACCOUNTING->getRowsAsHashList($page, STMTMGRFLAG_NONE, 'sel_providerreceipt',
-		$provider,$receipt,$batch_id,$reportBeginDate,$reportEndDate,$orgInternalId);
+	
+	#Get Fiscal year for Main Org
+	my $fiscal = $STMTMGR_ORG->getRowAsHash($page,STMTMGRFLAG_NONE,'selAttribute',$page->session('org_internal_id')||undef,'Fiscal Month');	
+	
+	#month is 1 less so add a 1
+	my $month = $fiscal->{value_int}+1;	
+	
+	#Check if end date if less then fiscal month if so go back one year
+	my @start_Date = Decode_Date_US($reportBeginDate);
+	if ($start_Date[1] < $month)
+	{
+		#Go back one year
+		$start_Date[0]--;
+	}
+	#Use Fiscal month
+	$start_Date[1] = $month;
+	
+	#First of first month
+	$start_Date[2] = 1;
+	
+	my $startDate = sprintf("%02d/%02d/%04d", $start_Date[1],$start_Date[2],$start_Date[0]);
+
+	#Convert to first day of fiscal month
+	$reportBeginDate =$startDate;
+
+	#Get Report Data 
+	my $rcpt  =  $STMTMGR_REPORT_ACCOUNTING->getRowsAsHashList($page, STMTMGRFLAG_NONE, 'sel_providerreceipt',
+		$provider,$receipt,$batch_id,$reportBeginDate,$reportEndDate,$orgInternalId) ;
 	my @data = ();
-	my $month_rcpt=0;
-	my $year_rcpt=0;
-	my $track_doc=undef;
-	my $track_cat=undef;
-	my $track_trans=undef;
-	my $track_payer=undef;
-	my $track_year=undef;
-	my $track_month=undef;	
 	foreach (@$rcpt)
 	{
-		#Track informatiom so we can sum it up for month and year info
-		if(! defined $track_doc)
+		#set batch date to report end date 
+		#if end date a begin date are the same then output batch date
+ 		$_->{batch_date} = $reportEndDate;
+		if($_->{payer_type} == 1)
 		{
-			$track_doc=$_->{provider};
-			$track_cat=$_->{category};
-			$track_trans=$_->{pay_type};
-			$track_payer=$_->{payer_name};
-			$track_year=$_->{year_date};
-			$track_month=$_->{month_date};
-			$month_rcpt = $_->{rcpt};
-			$year_rcpt = $_->{rcpt};			
+			my $getName = $STMTMGR_ORG->getRowAsHash($page,STMTMGRFLAG_NONE,'selId',$_->{payer_id});	
+			$_->{payer_type} = 'Insurance Receipts';
+			$_->{payer_id} = $getName->{org_id};
 		}
-		elsif ($track_doc ne $_->{provider} || $track_cat ne $_->{category} ||$track_trans ne $_->{pay_type} )
+		elsif($_->{payer_type}==-1)
 		{
-			$track_doc=$_->{provider};
-			$track_cat=$_->{category};
-			$track_trans=$_->{pay_type};
-			$track_payer=$_->{payer_name};
-			$track_year=$_->{year_date};
-			$track_month=$_->{month_date};
-			$month_rcpt = $_->{rcpt};
-			$year_rcpt = $_->{rcpt};				
-			
-		}
-		elsif ($track_year ne $_->{year_date} )
-		{
-			$month_rcpt = $_->{rcpt};
-			$year_rcpt = $_->{rcpt};			
-			$track_year=$_->{year_date};
-			$track_month=$_->{month_date};			
-		}
-		elsif ($track_month ne $_->{month_date})
-		{
-			$month_rcpt = $_->{rcpt};
-			$year_rcpt  += $_->{rcpt};			
-			$track_year=$_->{year_date};
-			$track_month=$_->{month_date};			
-			
+			$_->{payer_type} = 'Cap Insurance Receipts'; 				
 		}
 		else
 		{
-			$month_rcpt += $_->{rcpt};
-			$year_rcpt  += $_->{rcpt}
-			
-		};
-		
+			$_->{payer_type} = 'Personal Receipts';
+		}
+
 		my @rowData =
 		(
 			$_->{provider},
-			$_->{category},
+			$_->{payer_type},			
 			$_->{pay_type},
-			$_->{payer_name},
-			$_->{invoice_date},
-			$_->{rcpt},
-			$month_rcpt,
-			$year_rcpt,
-			$_->{invoice_id},
+			$_->{payer_id},									
+			$_->{month_rcpt},			
+			$_->{year_rcpt},
+			$_->{batch_date},
+			$_->{batch_rcpt},
 		);
 		push(@data, \@rowData);			
 	}
 	
-	my $html = createHtmlFromData($page, 0, \@data,$allPub);
+	my $html = '<br> <b style="font-family:Helvetica; font-size:12pt">(Fiscal Range '. $reportBeginDate .' - ' . $reportEndDate . ' ) </b><br>';
+	$html .= createHtmlFromData($page, 0, \@data,$allPub);
 	my $textOutputFilename = createTextRowsFromData ($page, 0, \@data, $allPub);
 	
 	if ($hardCopy == 1 and $printerAvailable) {
