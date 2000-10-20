7f19f61c1b87bee9d34fb48a693bb278b305e6c5
munir_faridi <munir_faridi>
2000-10-20 21:04:21 +0000

    [B1206] Added account notes component to patient account.

--- HealthSuite/Lib/perl/App/Page/Person.pm
+++ HealthSuite/Lib/perl/App/Page/Person.pm
@@ -737,12 +737,22 @@ sub prepare_view_account
 	my $formatter = new Number::Format('INT_CURR_SYMBOL' => '$');
 	my $queryStmt = $self->param('viewall') ? 'selAllInvoiceTypeForClient' : 'selNonVoidInvoiceTypeForClient';
 
+
 	$self->addContent(
-		'<CENTER>',
+		"<CENTER>
+		<TABLE CELLSPACING=0 BORDER=0 CELLPADDING=0>
+			<TR VALIGN=TOP>
+				<TD>
+					<font size=1 face=arial>
+					#component.stpt-person.account-notes#<BR>
+					</font>
+				</TD>
+			</TR>
+		</TABLE>",
 		$STMTMGR_INVOICE->createHtml($self, STMTMGRFLAG_NONE, $queryStmt,
 			[$personId, $self->session('org_internal_id')],
 		),
-		'</CENTER>'
+		"</CENTER>"
 	);
 }
 
