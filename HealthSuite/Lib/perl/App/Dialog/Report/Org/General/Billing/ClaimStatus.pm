ddbf43141faf02d6859500936c2f114be6bffff1
yousuf_sayed <yousuf_sayed>
2000-10-23 13:27:50 +0000

    [B1144] Fixed, now showing patients complete name in the billing/claim status report

--- HealthSuite/Lib/perl/App/Dialog/Report/Org/General/Billing/ClaimStatus.pm
+++ HealthSuite/Lib/perl/App/Dialog/Report/Org/General/Billing/ClaimStatus.pm
@@ -31,7 +31,7 @@ sub new
 {
 	my $self = App::Dialog::Report::new(@_, id => 'claimStatus');
 	$self->{heading} = 'Claims Status';
-	
+
 	$self->addContent(
 		new CGI::Dialog::Field::Duration(caption => 'Invoice Dates',
 			name => 'report',
@@ -41,7 +41,7 @@ sub new
 		new App::Dialog::Field::Insurance::Plan(caption => 'Claim Number(s)',
 			name => 'claim_numbers',
 			findPopup => '/lookup/claim',
-			findPopupAppendValue => ',',			
+			findPopupAppendValue => ',',
 		),
 
 		new CGI::Dialog::Field(caption => 'Payer Type',
@@ -50,10 +50,10 @@ sub new
 			fKeyStmtMgr => $STMTMGR_RPT_CLAIM_STATUS,
 			fKeyStmt => 'sel_payer_type',
 			fKeyDisplayCol => 0,
-			fKeyValueCol => 1,			
+			fKeyValueCol => 1,
 		),
 
-		new CGI::Dialog::Field(caption => 'Insurance Company ID', 
+		new CGI::Dialog::Field(caption => 'Insurance Company ID',
 			name => 'ins_org_id',
 			type => 'select',
 			fKeyStmtMgr => $STMTMGR_RPT_CLAIM_STATUS,
@@ -64,14 +64,14 @@ sub new
 		),
 
 		new App::Dialog::Field::Insurance::Product(caption => 'Insurance Product',
-			name => 'product_name', 
-			findPopup => '/lookup/insproduct/insorgid/itemValue', 
+			name => 'product_name',
+			findPopup => '/lookup/insproduct/insorgid/itemValue',
 			findPopupControlField => '_f_ins_org_id'
 		),
-		
-		new App::Dialog::Field::Insurance::Plan(caption => 'Insurance Plan', 
-			name => 'plan_name', 
-			findPopup => '/lookup/insplan/product/itemValue', 
+
+		new App::Dialog::Field::Insurance::Plan(caption => 'Insurance Plan',
+			name => 'plan_name',
+			findPopup => '/lookup/insplan/product/itemValue',
 			findPopupControlField => '_f_product_name'
 		),
 
@@ -120,11 +120,11 @@ sub populateData
 sub buildSqlStmt
 {
 	my ($self, $page, $flags) = @_;
-	
+
 	my $facilityId  = $page->field('facility_id');
-	my $internalFacilityId = $STMTMGR_ORG->getSingleValue($page, STMTMGRFLAG_NONE, 'selOrgId', 
+	my $internalFacilityId = $STMTMGR_ORG->getSingleValue($page, STMTMGRFLAG_NONE, 'selOrgId',
 		$page->session('org_internal_id'), $facilityId);
-	
+
 	my $startDate   = $page->field('report_begin_date');
 	my $endDate     = $page->field('report_end_date');
 
@@ -135,15 +135,15 @@ sub buildSqlStmt
 	my $providerCond;
 	my $facilityCond;
 	my $insId = $page->param('_f_ins_org_id');
-	my $insOrgId = $STMTMGR_ORG->getSingleValue($page, STMTMGRFLAG_NONE, 'selOrgId', 
+	my $insOrgId = $STMTMGR_ORG->getSingleValue($page, STMTMGRFLAG_NONE, 'selOrgId',
 		$page->session('org_internal_id'), $insId);
-	
+
 	#my $insOrgId = $page->param('_f_ins_org_id');
 	my $productName = $page->param('_f_product_name');
 	my $planName = $page->param('_f_plan_name');
 	my $providerId = $page->param('_f_provider_id');
 	my $claimNumbers = $page->param('_f_claim_numbers');
-	
+
 	$claimNumberCond = qq{and Invoice.invoice_id in ($claimNumbers)} if $claimNumbers;
 
 	$insuranceNameCond = qq{and Insurance.ins_org_id = $insOrgId} if $insOrgId;
@@ -155,7 +155,7 @@ sub buildSqlStmt
 	{
 		$transTable = qq{Transaction, };
 	}
-	
+
 	$providerCond = qq{
 		and Transaction.trans_id = Invoice.main_transaction
 		and upper(Transaction.provider_id) = upper('$providerId')
@@ -177,19 +177,19 @@ sub buildSqlStmt
 			};
 		}
 	}
-	
+
 	my $payerTypeCond;
 	my $payerType = $page->param('_f_payer_type');
-	$payerTypeCond = qq{and Invoice_Billing.bill_party_type = $payerType} 
+	$payerTypeCond = qq{and Invoice_Billing.bill_party_type = $payerType}
 		if $payerType != -1;
-	
+
 	my ($columns, $groupBy, $invoiceStatusCond);
 	if ($flags & FLAG_GETCOUNTS)
 	{
-		$columns = qq{Invoice_Status.caption as caption, count(Invoice_Status.caption) as cnt, 
+		$columns = qq{Invoice_Status.caption as caption, count(Invoice_Status.caption) as cnt,
 			Invoice.invoice_status
 		};
-			
+
 		$groupBy = qq{group by Invoice_Status.caption, Invoice.invoice_status};
 	}
 	else
@@ -197,7 +197,7 @@ sub buildSqlStmt
 		my $invoiceStatus = $page->param('invoice_status');
 		$invoiceStatusCond = qq{and Invoice.invoice_status = $invoiceStatus};
 
-		my $submitDate; 
+		my $submitDate;
 		if ($invoiceStatus == 4)
 		{
 			$submitDate = qq{to_char(nvl(submit_date, invoice_date), '$SQLSTMT_DEFAULTDATEFORMAT') as submit_date,};
@@ -206,31 +206,29 @@ sub buildSqlStmt
 		{
 			$submitDate = qq{to_char(submit_date, '$SQLSTMT_DEFAULTDATEFORMAT') as submit_date,};
 		}
-		
+
 		$columns = qq{to_char(Invoice.invoice_date, '$SQLSTMT_DEFAULTDATEFORMAT') as invoice_date,
-			$submitDate	
-			decode(Invoice_Billing.bill_party_type,0,Invoice_Billing.bill_to_id,1,Invoice_Billing.bill_to_id,(select org_id FROM ORG WHERE org_internal_id = Invoice_Billing.bill_to_id)) as  bill_to_id,			
-			product_name as insurance_product,	
-			plan_name as insurance_plan, 
-			total_cost as total_charge, 
-			total_adjust, 
-			Invoice.invoice_id, 
+			$submitDate
+			decode(Invoice_Billing.bill_party_type,0,Invoice_Billing.bill_to_id,1,Invoice_Billing.bill_to_id,(select org_id FROM ORG WHERE org_internal_id = Invoice_Billing.bill_to_id)) as  bill_to_id,
+			product_name as insurance_product,
+			plan_name as insurance_plan,
+			total_cost as total_charge,
+			total_adjust,
+			Invoice.invoice_id,
 			Invoice.client_id,
-			(select ia.value_text 
+			(select ia.value_text
 			 FROM insurance_attribute  ia
 			 WHERE ia.parent_id = Insurance.parent_ins_id
-			 AND ia.item_name = 'Contact Method/Telephone/Primary' ) as phone_number
-			 
-			
-			
+			 AND ia.item_name = 'Contact Method/Telephone/Primary' ) as phone_number,
+			 Person.complete_name
 		};
-		
+
 		$groupBy = qq{order by 3,1};
 	}
 
 	my $html = qq{
 		select $columns
-			from $transTable Insurance, Invoice_Billing, Invoice_Status, Invoice 
+			from $transTable Insurance, Invoice_Billing, Invoice_Status, Invoice, Person
 			where Invoice.owner_id = ?
 				and Invoice.owner_type = $typeOrg
 				and Invoice.invoice_date between to_date(? || ' 12:00 AM', '$SQLSTMT_DEFAULTSTAMPFORMAT')
@@ -248,6 +246,7 @@ sub buildSqlStmt
 				$insurancePlanCond
 				$providerCond
 				$facilityCond
+				and Invoice.client_id = Person.person_id
 			$groupBy
 	};
 
@@ -267,7 +266,7 @@ sub execute
 	my $printerDevice;
 	$printerDevice = ($page->field('printerQueue') ne '') ? $page->field('printerQueue') : App::Device::getPrinter ($page, 0);
 	my $printHandle = App::Device::openRawPrintHandle ($printerDevice);
-	
+
 	$printerAvailable = 0 if (ref $printHandle eq 'SCALAR');
 
 	my $orgId = $page->session('org_id');
@@ -277,23 +276,23 @@ sub execute
 		reportTitle => $self->heading (),
 		columnDefn =>
 		[
-			{	head => 'Claims', 
+			{	head => 'Claims',
 				url => q{javascript:doActionPopup('#hrefSelfPopup#&detail=status&invoice_status=#2#&status_caption=#0#', null, 'width=800,height=600,scrollbars,resizable')},
-				hint => 'View Details' 
+				hint => 'View Details'
 			},
 			{head => 'Count', dAlign => 'right'},
 		],
 	};
-	
+
 	my $sqlStmt = $self->buildSqlStmt($page, $flags | FLAG_GETCOUNTS);
 	my $data = $STMTMGR_RPT_CLAIM_STATUS->getRowsAsArray($page, STMTMGRFLAG_DYNAMICSQL, $sqlStmt, $orgIntId, $startDate, $endDate);
 	my $textOutputFilename = createTextRowsFromData($page, STMTMGRFLAG_NONE, $data, $publishDefn);
-	
+
 	my $html = qq{
 	<table cellpadding=10>
 		<tr align=center valign=top>
 		<td>
-			@{[ $STMTMGR_RPT_CLAIM_STATUS->createHtml($page, STMTMGRFLAG_DYNAMICSQL, 
+			@{[ $STMTMGR_RPT_CLAIM_STATUS->createHtml($page, STMTMGRFLAG_DYNAMICSQL,
 				$sqlStmt,
 				[$orgIntId, $startDate, $endDate], undef, undef, $publishDefn) ]}
 		</td>
@@ -324,7 +323,7 @@ sub getDrillDownHandlers
 sub prepare_detail_status
 {
 	my ($self, $page) = @_;
-	
+
 	my $startDate   = $page->field('report_begin_date');
 	my $endDate     = $page->field('report_end_date');
 
@@ -338,7 +337,7 @@ sub prepare_detail_status
 				url => q{javascript:chooseItemForParent('/invoice/#7#/summary') },
 				hint => 'View Invoice Summary',
 			},
-			{head => 'Patient ID', colIdx => 8, dAlign => 'center',
+			{head => 'Patient', colIdx => 8, dAlign => 'center', dataFmt => '#11#',
 				url => q{javascript:chooseItemForParent('/person/#8#/account')},
 				hint => 'View #8# Account',
 			},
@@ -346,18 +345,18 @@ sub prepare_detail_status
 			{head => 'Submit Date', colIdx => 1,},
 			{head => 'Insurance Product', colIdx => 3, dAlign => 'center'},
 			{head => 'Insurance Plan', colIdx => 4, dAlign => 'center'},
-			{head => 'Total Charge', colIdx => 5, 
+			{head => 'Total Charge', colIdx => 5,
 				dformat => 'currency', tAlign => 'RIGHT', summarize=>'sum',
-				tDataFmt => '&{avg_currency:&{?}}<BR>&{sum_currency:&{?}}' 
+				tDataFmt => '&{avg_currency:&{?}}<BR>&{sum_currency:&{?}}'
 			},
-			{head => 'Total Adjust', colIdx => 6, 
-				dformat => 'currency', tAlign => 'RIGHT', summarize=>'sum', 
+			{head => 'Total Adjust', colIdx => 6,
+				dformat => 'currency', tAlign => 'RIGHT', summarize=>'sum',
 			},
 			{head =>'Contact Number' , colIdx =>9},
-			{head =>'Notes' , colIdx =>10},			
+			{head =>'Notes' , colIdx =>10},
 		],
 	};
-	
+
 	my @data = ();
 	my $claimStatus = $STMTMGR_RPT_CLAIM_STATUS->getRowsAsHashList($page,STMTMGRFLAG_DYNAMICSQL,$sqlStmt,$page->session('org_internal_id'), $startDate, $endDate);
 	foreach (@$claimStatus)
@@ -369,9 +368,9 @@ sub prepare_detail_status
 		foreach my $value (@$getEDINotes)
 		{
 			$notes .= "<b>Note :</b> $value->{value_text} </br>";
-		}	
+		}
 		my @rowData = (
-		$_->{invoice_date},		
+		$_->{invoice_date},
 		$_->{submit_date},
 		$_->{bill_to_id},
 		$_->{insurance_product},
@@ -381,13 +380,14 @@ sub prepare_detail_status
 		$_->{invoice_id},
 		$_->{client_id},
 		$_->{phone_number},
-		$notes);
+		$notes,
+		$_->{complete_name});
 		push(@data, \@rowData);
 	};
-	
+
 	my $caption =$page->param('status_caption');
 	my $html =qq{<b style="font-family:Helvetica; font-size:12pt">('$caption' Claims) </b><br><br>};
-	$html .= createHtmlFromData($page, 0, \@data,$pub);		
+	$html .= createHtmlFromData($page, 0, \@data,$pub);
 	#$page->addContent('<b style="font-family:Helvetica; font-size:12pt">('. $page->param('status_caption') . ' Claims) </b><br><br>',
 	#	@{[ $STMTMGR_RPT_CLAIM_STATUS->createHtml($page, STMTMGRFLAG_DYNAMICSQL, #| STMTMGRFLAG_DEBUG,
 	#	$sqlStmt,	[$page->session('org_internal_id'), $startDate, $endDate], undef, undef, $publishDefn) ]}
