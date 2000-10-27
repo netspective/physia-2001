a6f8fa133a3e85972800328f1c28ecc9251b785e
yousuf_sayed <yousuf_sayed>
2000-10-27 17:23:35 +0000

    [B1298] Fixed

--- HealthSuite/Lib/perl/App/Dialog/Report/Org/General/Accounting/AccountCollection.pm
+++ HealthSuite/Lib/perl/App/Dialog/Report/Org/General/Accounting/AccountCollection.pm
@@ -44,13 +44,48 @@ sub populateData
 	$page->field('report_end_date', $startDate);
 }
 
+sub getDrillDownHandlers
+{
+	return ('prepare_detail_$detail$');
+}
+
+sub prepare_detail_notes
+{
+	my ($self, $page) = @_;
+	my $person_id = $page->param('person_id');
+
+	$page->addContent($STMTMGR_REPORT_ACCOUNT_COLLECTION->createHtml($page, STMTMGRFLAG_NONE, 'selCollectorsNotes',
+			[$person_id]));
+}
 
 sub execute
 {
 	my ($self, $page, $command, $flags) = @_;
 	my $orgInternalID = $page->session('org_internal_id');
+	my @data = ();
 
-	my $html = $STMTMGR_REPORT_ACCOUNT_COLLECTION->createHtml($page, STMTMGRFLAG_NONE, 'selCollectors', [$page->field('report_begin_date'), $page->field('report_end_date'), $orgInternalID]);
+	my $pub = $PUBLISH_DEFN;
+
+	my $rows = $STMTMGR_REPORT_ACCOUNT_COLLECTION->getRowsAsHashList($page,STMTMGRFLAG_NONE,'selCollectors', $page->field('report_begin_date'), $page->field('report_end_date'), $page->session('org_internal_id'));
+	foreach my $row (@$rows)
+	{
+		my ($patientName, $patientId, $balance, $age) =  ($row->{patient_name}, $row->{patient_id}, $row->{balance}, $row->{age});
+		my $notesCount = $STMTMGR_REPORT_ACCOUNT_COLLECTION->getSingleValue($page,STMTMGRFLAG_NONE,'selCollectorsNotesCount', $row->{patient_id});
+		if($notesCount == 0)
+		{
+			$notesCount = undef;
+		}
+
+		my $rows1 = $STMTMGR_REPORT_ACCOUNT_COLLECTION->getRowsAsHashList($page,STMTMGRFLAG_NONE,'selCollectorsOwners', $row->{patient_id});
+		foreach my $row1 (@$rows1)
+		{
+			my @rowData = ($patientName, $patientId, $balance, $age, $row1->{provider_name}, $row1->{provider_id}, $notesCount);
+			push(@data, \@rowData);
+			($patientName, $patientId, $balance, $age, $notesCount) =(undef,undef,undef,undef,undef);
+		}
+	}
+
+	my $html = createHtmlFromData($page, 0, \@data, $pub);
 	return $html;
 }
 
--- HealthSuite/Lib/perl/App/Statements/Report/AccountCollection.pm
+++ HealthSuite/Lib/perl/App/Statements/Report/AccountCollection.pm
@@ -7,14 +7,61 @@ use Exporter;
 use DBI::StatementManager;
 use App::Universal;
 
-use vars qw(@ISA @EXPORT $STMTMGR_REPORT_ACCOUNT_COLLECTION);
+use vars qw(@ISA @EXPORT $STMTMGR_REPORT_ACCOUNT_COLLECTION $PUBLISH_DEFN);
 
 @ISA    = qw(Exporter DBI::StatementManager);
-@EXPORT = qw($STMTMGR_REPORT_ACCOUNT_COLLECTION );
+@EXPORT = qw($STMTMGR_REPORT_ACCOUNT_COLLECTION $PUBLISH_DEFN);
 
 my $ACTIVE = App::Universal::TRANSSTATUS_ACTIVE;
 my $OWNER = App::Universal::TRANSTYPE_ACCOUNT_OWNER;
 my $NOTES = App::Universal::TRANSTYPE_ACCOUNTNOTES;
+
+
+$PUBLISH_DEFN =
+{
+	columnDefn =>
+	[
+		{
+			colIdx => 0,
+			head => 'Patient',
+			hAlign => 'center',
+			dAlign => 'left',
+			dataFmt => '#0# <A HREF = "/person/#1#/profile">#1#</A>',
+		},
+		{
+			colIdx => 2,
+			head => 'Balance',
+			hAlign => 'center',
+			dAlign => 'right',
+			dformat => 'currency',
+			summarize => 'sum',
+			dataFmt => '#2#',
+		},
+		{
+			colIdx => 3,
+			head => 'Age',
+			hAlign => 'center',
+			dAlign => 'right',
+			dataFmt => '#3#',
+		},
+		{
+			colIdx => 4,
+			head => 'Collector',
+			hAlign => 'center',
+			dAlign => 'left',
+			dataFmt => '#4# <A HREF = "/person/#5#/profile">#5#</A>',
+		},
+		{
+			colIdx => 5,
+			head => 'Notes',
+			hAlign => 'center',
+			dAlign => 'right',
+			dataFmt => '#6#',
+			url => q{javascript:doActionPopup('#hrefSelfPopup#&detail=notes&person_id=#1#')}
+		},
+	],
+};
+
 
 $STMTMGR_REPORT_ACCOUNT_COLLECTION = new App::Statements::Report::AccountCollection
 (
@@ -26,29 +73,19 @@ $STMTMGR_REPORT_ACCOUNT_COLLECTION = new App::Statements::Report::AccountCollect
 				p1.simple_name patient_name,
 				t1.trans_owner_id patient_id,
 				i.balance,
-				trunc(sysdate - i.invoice_date) age,
-				p2.simple_name provider_name,
-				t1.provider_id provider_id,
-				t2.detail notes
+				trunc(sysdate - i.invoice_date) age
 			from
 				invoice i,
 				person p1,
-				person p2,
 				transaction t1,
-				person_org_category poc,
-				(
-					select * from	transaction
-					where trans_type = $NOTES
-				) t2
+				person_org_category poc
 			where i.invoice_date between to_date(:1, 'MM/DD/YYYY') and to_date(:2, 'MM/DD/YYYY')
 			and i.balance > 0
 			and i.invoice_status <> 16
 			and i.invoice_id = t1.trans_invoice_id
 			and t1.trans_owner_id = p1.person_id
-			and t1.trans_owner_id = t2.trans_owner_id (+)
 			and p1.person_id = poc.person_id
 			and poc.org_internal_id = :3
-			and t1.provider_id = p2.person_id
 			and t1.trans_status = $ACTIVE
 			and t1.trans_type = $OWNER
 			and t1.trans_subtype = 'Owner'
@@ -57,52 +94,65 @@ $STMTMGR_REPORT_ACCOUNT_COLLECTION = new App::Statements::Report::AccountCollect
 
 		sqlStmtBindParamDescr => ['Date Range and Org Internal Id'],
 
+	},
+
+
+	'selCollectorsOwners' =>
+	{
+		sqlStmt => qq
+		{
+			select distinct
+				p2.simple_name provider_name,
+				t1.provider_id provider_id
+			from
+				person p2,
+				transaction t1
+			where t1.trans_owner_id = :1
+			and t1.provider_id = p2.person_id
+			and t1.trans_status = $ACTIVE
+			and t1.trans_type = $OWNER
+			and t1.trans_subtype = 'Owner'
+			order by 2
+		},
+	},
+
+
+	'selCollectorsNotesCount' =>
+	{
+		sqlStmt => qq
+		{
+			select count(*)
+			from transaction
+			where trans_type = $NOTES
+			and trans_owner_id = :1
+		},
+
+	},
+
+	'selCollectorsNotes' =>
+	{
+		sqlStmt => qq
+		{
+			select detail
+			from transaction
+			where trans_type = $NOTES
+			and trans_owner_id = :1
+		},
+
 		publishDefn =>
 		{
 			columnDefn =>
 			[
 				{
 					colIdx => 0,
-					head => 'Patient',
-					hAlign => 'center',
-					dAlign => 'left',
-					dataFmt => '#0# <A HREF = "/person/#1#/profile">#1#</A>',
-				},
-				{
-					colIdx => 2,
-					head => 'Balance',
-					hAlign => 'center',
-					dAlign => 'right',
-					dformat => 'currency',
-					summarize => 'sum',
-					dataFmt => '#2#',
-				},
-				{
-					colIdx => 3,
-					head => 'Age',
-					hAlign => 'center',
-					dAlign => 'right',
-					dataFmt => '#3#',
-				},
-				{
-					colIdx => 4,
-					head => 'Collector',
-					hAlign => 'center',
-					dAlign => 'left',
-					dataFmt => '#4# <A HREF = "/person/#5#/profile">#5#</A>',
-				},
-				{
-					colIdx => 6,
 					head => 'Notes',
 					hAlign => 'center',
 					dAlign => 'left',
-					dataFmt => '#6#',
+					dataFmt => '#0#',
 				},
 			],
 		},
 	},
 );
 
-
 1;
-
