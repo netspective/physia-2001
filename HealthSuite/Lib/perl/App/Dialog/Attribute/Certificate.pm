2aeb0342b07827451998068e1508ea3a3acaa9e4
radha_kotagiri <radha_kotagiri>
2000-10-30 20:25:34 +0000

    [B1025] Added validation for Specialty Sequence

--- HealthSuite/Lib/perl/App/Dialog/Attribute/Certificate.pm
+++ HealthSuite/Lib/perl/App/Dialog/Attribute/Certificate.pm
@@ -67,6 +67,51 @@ sub customValidate
 		$pId->invalidate($page, "This 'Specialty' already exists for $personId");
 	}
 
+	#my $sequenceDoesnotExist = $STMTMGR_PERSON->getRowAsHash($page,STMTMGRFLAG_NONE, 'selSpecialtySequence', $personId, $sequence-1) if $sequence ne "&SEQUENCE_SPECIALTY_UNKNOWN";
+
+	#if ($command eq 'add' && $valueType eq $specialtyValType  && ($sequenceDoesnotExist->{'value_int'} eq '' || $sequence < $sequenceDoesnotExist->{'value_int'})  && $sequence > SEQUENCE_SPECIALTY_PRIMARY && $sequence <=SEQUENCE_SPECIALTY_QUATERNARY && $sequence ne "&SEQUENCE_SPECIALTY_UNKNOWN")
+	#{
+	#	$sName->invalidate($page, "This 'Specialty Sequence' cannot be added until the previous sequence is added");
+	#}
+
+	#if ($command ne 'add' && $valueType eq $specialtyValType  && ($sequenceDoesnotExist->{'value_int'} eq '' || $sequence < $sequenceDoesnotExist->{'value_int'})  && ($itemId ne $sequenceExists->{'item_id'}) && $sequence > SEQUENCE_SPECIALTY_PRIMARY && $sequence <=SEQUENCE_SPECIALTY_QUATERNARY && $sequence ne "&SEQUENCE_SPECIALTY_UNKNOWN")
+	#{
+	#	$sName->invalidate($page, "This 'Specialty Sequence' cannot be added until the previous sequence is added");
+	#}
+	my $sequenceDoesnotExist = $STMTMGR_PERSON->getRowAsHash($page,STMTMGRFLAG_NONE, 'selAttributeById', $itemId);
+
+	for (my $x = SEQUENCE_SPECIALTY_PRIMARY; $x < $sequence; $x++)
+	{
+		my $seqCap = '';
+
+		if ($x == 1)
+		{
+			$seqCap = 'Primary';
+		}
+		elsif ($x == 2)
+		{
+			$seqCap = 'Secondary';
+		}
+		elsif ($x == 3)
+		{
+			$seqCap = 'Tertiary';
+		}
+		else
+		{
+			$seqCap = 'Unknown';
+		}
+
+		if ($sequence ne SEQUENCE_SPECIALTY_UNKNOWN)
+		{
+			$sName->invalidate($page, "This 'Specialty Sequence' cannot be added until the sequence '$seqCap' is added")unless $STMTMGR_PERSON->recordExists($page,STMTMGRFLAG_NONE, 'selSpecialtySequence', $personId, $x);
+		}
+		if ($command eq 'update' && $sequence > $sequenceDoesnotExist->{'value_int'} && $sequence ne SEQUENCE_SPECIALTY_UNKNOWN)
+		{
+			$sName->invalidate($page, "This 'Specialty Sequence' cannot be added until the previous sequence is added");
+			last;
+		}
+	}
+
 	if($page->field('value_textb') eq 'Nursing/License' && ($page->field('value_int') eq '' || $page->field('value_dateend') eq ''))
 	{
 		$licenseNum->invalidate($page, "'Expiration Date' and 'License Required' should be entered when the license is 'Nursing/License' ");
