19372503c94fd342aabfd07ef36da621d4b6a7ac
alex_hillman <alex_hillman>
2000-11-08 15:19:02 +0000

    Moved views creation and copy of REF_CPT to separate script

--- HealthSuite/Scripts/Build/BUILD_0016/BUILD_0016_main_script.sql
+++ HealthSuite/Scripts/Build/BUILD_0016/BUILD_0016_main_script.sql
@@ -47,13 +47,6 @@ start tables-code/Invoice_Item
 start tables/Ethnicity
 start data/Ethnicity 
 
-start post/views-report-aged-patient-receiv
-
-set long 32000
-
-truncate table ref_cpt;
-
-copy from sde01/sde@sdedbs04 to pro_test/pro@sdedbs04 insert ref_cpt using select * from ref_cpt;
 
 
 
