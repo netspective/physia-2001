0309f0a83d04cda2d6708a5cd1c2ca5c0f7d851e
frank_major <frank_major>
2000-11-10 16:37:38 +0000

    [B1463] Added Fiscal year to report , and corrected A/R calc

--- HealthSuite/Lib/perl/App/Dialog/Report/Org/General/Accounting/FinancialAnalysisReport.pm
+++ HealthSuite/Lib/perl/App/Dialog/Report/Org/General/Accounting/FinancialAnalysisReport.pm
@@ -13,6 +13,8 @@ use DBI::StatementManager;
 use Data::Publish;
 use App::Statements::Report::Accounting;
 use App::Statements::Org;
+use Date::Calc qw(:all);
+use Date::Manip;
 
 use vars qw(@ISA $INSTANCE);
 
@@ -28,29 +30,77 @@ sub new
 					caption => 'Report Date',
 					begin_caption => 'Report Begin Date',
 					end_caption => 'Report End Date',
+					options=>FLDFLAG_REQUIRED,
 					),
 				new App::Dialog::Field::Organization::ID(caption =>'Site Organization ID', name => 'org_id'),
 				new App::Dialog::Field::Person::ID(caption =>'Physican ID', name => 'person_id'),
 			
 			);
 	$self->addFooter(new CGI::Dialog::Buttons);
+	
 
 	$self;
 }
 
-#sub populateData
-#{
+sub populateData
+{
 #	my ($self, $page, $command, $activeExecMode, $flags) = @_;
 #
 #	$page->field('person_id', $page->session('person_id'));
-#}
+}
+
+
+sub customValidate
+{
+	my ($self, $page) = @_;
+	
+	my $reportBeginDate = $page->field('batch_begin_date');
+	my $reportEndDate = $page->field('batch_end_date');
+	#Check Dates
+	#And Fiscal Month
+	my $orgInternalId = $page->session('org_internal_id');
+	
+	#Get Fiscal year for Main Org
+	my $fiscal = $STMTMGR_ORG->getRowAsHash($page,STMTMGRFLAG_NONE,'selAttribute',$orgInternalId,'Fiscal Month');	
+	
+	#month is 1 less so add a 1
+	my $month = $fiscal->{value_int}+1;	
+	
+	
+	my @start_Date = Decode_Date_US($reportBeginDate);
+	my @end_Date = Decode_Date_US($reportEndDate);
+	my $field = $self->getField('batch');	
+	#Check if month in begin date is less then fiscal month
+	#$page->addError("[$start_Date[0]] [$end_Date[0]]"); 
+	if ($start_Date[1] < $month && $end_Date[1] >= $month)
+	{
+		#Go back one year
+		#Error message
+		#my $contractID = $page->field('contract_id');
+		#my $code = $page->field('code');
+		$field->invalidate($page, qq{Requested dates [$reportBeginDate - $reportEndDate]  cross fiscal year [check month]});
+			
+	}	
+	elsif($start_Date[0] != $end_Date[0])
+	{
+	 	#unless ($start_Date[0]+1 == $end_Date[0] && end_Date[1]  < $month)
+		$field->invalidate($page, qq{Requested dates [$reportBeginDate - $reportEndDate]  cross fiscal year. Check Year})unless ($start_Date[0] + 1 == $end_Date[0] && $end_Date[1]  < $month);
+	}
+	
+	
+	
+	
+	
+}
+
+
 
 
 sub execute
 {
 	my ($self, $page, $command, $flags) = @_;
-	my $reportBeginDate = $page->field('batch_begin_date')||'01/01/1800';
-	my $reportEndDate = $page->field('batch_end_date')||'01/01/9999';
+	my $reportBeginDate = $page->field('batch_begin_date');
+	my $reportEndDate = $page->field('batch_end_date');
 	my $orgId = $page->field('org_id');
 	my $person_id = $page->field('person_id')||undef;
 	my $batch_from = $page->field('batch_id_from')||undef;
@@ -60,7 +110,7 @@ sub execute
 	my $pub ={
 		columnDefn =>
 		[
-		{colIdx => 12, head =>'Year',groupBy=>'#12#', dAlign => 'left',},
+		{colIdx => 12, head =>'Year', dAlign => 'left',},
 		{colIdx => 0, head => 'Month', dAlign => 'left',},
 		{colIdx => 1, head => 'Chrgs', dAlign => 'left',summarize => 'sum',dformat => 'currency' },
 		{colIdx => 2, head => 'Misc Chrgs',dAlign =>'left' , hAlign =>'left',summarize => 'sum',dformat => 'currency' },
@@ -73,19 +123,42 @@ sub execute
 		{colIdx => 9, head => 'Rcpt Adj', dAlign => 'center',summarize => 'sum',dformat => 'currency'},
 		{colIdx => 10,head => 'Net Rcpts', summarize => 'sum',  dformat => 'currency' },		
 		{colIdx => 13,head => 'Monthly A/R', summarize => 'sum', dformat => 'currency' },		
-		{colIdx => 11,head => 'A/R', tDataFmt => '&{sum_currency:13}',sDataFmt => '&{sum_currency:13}',  dformat => 'currency' },
+		{colIdx => 11,head => 'A/R', 	tDataFmt => '&{sum_currency:14}',dformat => 'currency' },
 		],
 		};
+#'&{count:0} Contracts'		
 		
 	
 	$orgIntId = $STMTMGR_ORG->getSingleValue($page, STMTMGRFLAG_NONE, 'selOrgId', $page->session('org_internal_id'), $orgId) if $orgId;
 
 	my $far = $STMTMGR_REPORT_ACCOUNTING->getRowsAsHashList($page, STMTMGRFLAG_NONE , 'sel_financial_monthly',$reportBeginDate,
 	$reportEndDate,$orgIntId,$person_id,$page->session('org_internal_id'));
-	my $total_ar = 0;
+
+
+	my $orgInternalId = $page->session('org_internal_id');
+	
+	#Get Fiscal year for Main Org
+	my $fiscal = $STMTMGR_ORG->getRowAsHash($page,STMTMGRFLAG_NONE,'selAttribute',$orgInternalId,'Fiscal Month');	
+	
+	#month is 1 less so add a 1
+	my $month = $fiscal->{value_int}+1;	
+	
+	#Check if end date is less then fiscal month if so go back one year
+	my @start_Date = Decode_Date_US($reportBeginDate);
+	if ($start_Date[1] < $month)
+	{
+		#Go back one year
+		$start_Date[0]--;
+	}	
+	my $startFiscal = sprintf("%02d/%02d/%04d", $month,$start_Date[2],$start_Date[0]);
+		
+	my $total_ar=$STMTMGR_REPORT_ACCOUNTING->getSingleValue($page,STMTMGRFLAG_NONE,'sel_a_r_before',$startFiscal,$reportBeginDate,$page->session('org_internal_id'));
+	#$page->addError("Value : $total_ar");
+	my $track_ar = $total_ar;
 	foreach (@$far)	
 	{
 		$total_ar +=$_->{a_r};
+		$track_ar +=$_->{a_r};
 		my @rowData =
 		(
 		$_->{invoice_month},
@@ -102,9 +175,12 @@ sub execute
 		$total_ar,
 		$_->{invoice_year},
 		$_->{a_r},
+		$track_ar
 		);
+		$track_ar = 0;
 		push(@data, \@rowData);				
 	}
+	#$page->param('total_ar',$total_ar);
 	return createHtmlFromData($page, 0, \@data,$pub);
 }
 
