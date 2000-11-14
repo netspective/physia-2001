85b79127342f45b1e1329b2f0344d2f50b5bb34e
alex_hillman <alex_hillman>
2000-11-14 21:58:48 +0000

    Commented copying of ref_cpt

--- HealthSuite/Scripts/Build/BUILD_0016/BUILD_0016_views_and_ref_cpt.sql
+++ HealthSuite/Scripts/Build/BUILD_0016/BUILD_0016_views_and_ref_cpt.sql
@@ -1,14 +1,16 @@
 start post/views-report-aged-patient-receiv
 
+/*
 set long 32000
 
 alter table ref_cpt_usage disable constraint CPTUSG_PARENT_ID_FK;
 
 truncate table ref_cpt;
 
-copy from sde01/sde@sdedbs04 to pro_test/pro@sdedbs04 insert ref_cpt using select * from ref_cpt;
+copy from sde01/sde@sdedbs04 to demo02/demo@sdedbs02 insert ref_cpt using select * from ref_cpt;
 
 alter table ref_cpt_usage enable constraint CPTUSG_PARENT_ID_FK;
+*/
 
 
 
