0d6b2d88d91951396e59913b55d10de7c6231164
alex_hillman <alex_hillman>
2000-11-15 00:59:50 +0000

    Commented new index creation

--- HealthSuite/Scripts/Build/BUILD_0016/Upgrade.pl
+++ HealthSuite/Scripts/Build/BUILD_0016/Upgrade.pl
@@ -33,7 +33,7 @@ runSQL('BUILD_0016_drop_duplicate_triggers.sql');
 runSQL('BUILD_0016_only_in_production.sql');
 runSQL('BUILD_0016_views_and_ref_cpt.sql');
 runSQL('BUILD_0016_alter_table_monitoring.sql');
-runSQL('BUILD_0016_create_new_indexes.sql');
+#runSQL('BUILD_0016_create_new_indexes.sql');
 runSQL('BUILD_0016_compile_invalid_and_disabled.sql');
 
 ######## END UPGRADE SCRIPT #########
