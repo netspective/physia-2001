6852cdda1aee532536b4009dd98ff5584f4a457b
radha_kotagiri <radha_kotagiri>
2000-11-17 12:22:13 +0000

    [B252] Added 'Go to Worklist' in the next action list

--- HealthSuite/Lib/perl/App/Dialog/Catalog.pm
+++ HealthSuite/Lib/perl/App/Dialog/Catalog.pm
@@ -62,38 +62,38 @@ sub new
 
 	croak 'schema parameter required' unless $schema;
 	my $phy_field = new App::Dialog::Field::MultiPerson::ID(caption => 'Physician ID',
-			name => 'physician_id',			
+			name => 'physician_id',
 			types => ['Physician'],
 	);
-	my $org_field =new App::Dialog::Field::MultiOrg::ID(caption => ' Site Organization ID ',	
+	my $org_field =new App::Dialog::Field::MultiOrg::ID(caption => ' Site Organization ID ',
 			name => 'org_id',
-			),		
-	
-	$phy_field->clearFlag(FLDFLAG_IDENTIFIER);				
+			),
+
+	$phy_field->clearFlag(FLDFLAG_IDENTIFIER);
 
 	$self->addContent(
-	
+
 		new CGI::Dialog::Field(type=>'hidden',
 					name=>'org_item_id',
 					),
 		new CGI::Dialog::Field(type=>'hidden',
 					name=>'phy_item_id',
-					),	
+					),
 		new CGI::Dialog::Field(type=>'hidden',
 					name=>'phy_item_id',
-					),	
+					),
 		new CGI::Dialog::Field(caption => 'Internal Catalog ID',
 			name => 'parent_catalog_id',
 			type=>'hidden'
 		),
-		
+
 		new CGI::Dialog::Field(caption => 'Internal Catalog ID',
 			name => 'internal_catalog_id',
 			type=>'hidden'
 		),
 
 		new App::Dialog::Field::Catalog::ID::New(caption => 'Fee Schedule Name',
-			name => 'catalog_id', 
+			name => 'catalog_id',
 			size => 20,
 			options => FLDFLAG_REQUIRED,
 			postHtml => "&nbsp; <a href=\"javascript:doActionPopup('/lookup/catalog');\">Lookup Fee Schedules</a>",
@@ -102,11 +102,11 @@ sub new
 			name => 'catalog_type',
 			type => 'hidden',
 			column => 'offering_catalog_type.id',
-			schema => $schema, 
+			schema => $schema,
 			value => 0
 		),
-		new CGI::Dialog::Field(caption => 'Fee Schedule Caption', 
-			name => 'caption', 
+		new CGI::Dialog::Field(caption => 'Fee Schedule Caption',
+			name => 'caption',
 			options => FLDFLAG_REQUIRED,
 			size => 45,
 		),
@@ -114,18 +114,18 @@ sub new
 			name => 'description',
 			type => 'memo',
 		),
-		
+
                 new CGI::Dialog::Field::Duration(
                         name => 'effective',
                         caption => 'Effective Date Range',
-                ),		
+                ),
 		new CGI::Dialog::Field(caption => 'RVRBS Multiplier',
 			name => 'rvrbs_multiplier',
-			type => 'float', 
+			type => 'float',
 			minValue => 0,
-		),	
+		),
 		$phy_field,
-		$org_field,		
+		$org_field,
 		new App::Dialog::Field::Catalog::ID(caption => 'Parent Fee Schedule ID',
 	      		name => 'parent_catalog_name',
 	      		#type => 'integer',
@@ -139,10 +139,10 @@ sub new
 			defaultValue => 0,
 		),
 		new CGI::Dialog::Field(type => 'hidden', name => 'add_mode'),
-		
-		
+
+
 	);
-	
+
 	$self->{activityLog} =
 	{
 		scope =>'offering_catalog',
@@ -154,8 +154,9 @@ sub new
 			['Add Another Fee Schedule', '/org/#session.org_id#/dlg-add-catalog', 1],
 			['Add RVRBS Fee Schedule Entries', '/org/#session.org_id#/dlg-add-feescheduledataentry/%field.catalog_id%'],
 			#['Show Current Fee Schedule', '/org/#session.org_id#/catalog/%field.internal_catalog_id%/%field.catalog_id%'],
-			['Show Current Fee Schedule', '/org/#session.org_id#/catalog?catalog=fee_schedule_detail&fee_schedule_detail=%field.internal_catalog_id%'],			
-			['Show List of Fee Schedules', '/org/#session.org_id#/catalog?catalog=fee_schedule']
+			['Show Current Fee Schedule', '/org/#session.org_id#/catalog?catalog=fee_schedule_detail&fee_schedule_detail=%field.internal_catalog_id%'],
+			['Show List of Fee Schedules', '/org/#session.org_id#/catalog?catalog=fee_schedule'],
+			['Go to Work List', "/worklist"],
 			],
 		cancelUrl => $self->{cancelUrl} || undef));
 
@@ -172,7 +173,7 @@ sub populateData_add
 	{
 		my $parentCatalog = $STMTMGR_CATALOG->getRowAsHash($page,STMTMGRFLAG_NONE,'selCatalogById',$page->field('parent_catalog_id')) ;
 		$page->field('parent_catalog_name',$parentCatalog->{catalog_id});
-	};	
+	};
 	$page->field('add_mode', 1);
 }
 
@@ -197,11 +198,11 @@ sub populateData_update
 		$decimal =~ s/^\./0./;
 		$page->field('rvrbs_multiplier', $decimal)
 	}
-	
+
 	my $recExist = $STMTMGR_CATALOG->getRowAsHash($page, STMTMGRFLAG_NONE, 'sel_Catalog_Attribute',
 		$page->field('internal_catalog_id'), App::Universal::ATTRTYPE_BOOLEAN,
 		'Capitated Contract'
-	);	
+	);
 	my $data_range = $STMTMGR_CATALOG->getRowAsHash($page,STMTMGRFLAG_NONE,'sel_Catalog_Attribute',$page->field('internal_catalog_id'),
 	App::Universal::ATTRTYPE_DATE,'Effective/Date/Range');
 	my $orgFS = $STMTMGR_CATALOG->getRowsAsHashList($page,STMTMGRFLAG_NONE,'selOrgIdLinkedFS',$page->field('internal_catalog_id'));
@@ -210,13 +211,13 @@ sub populateData_update
 	{
 		$orgList .=$orgList ? ",$org_fs_data->{'org_id'}" :  $org_fs_data->{'org_id'};
 	}
-	
+
 	my $perFS = $STMTMGR_CATALOG->getRowsAsHashList($page,STMTMGRFLAG_NONE,'selPersonIdLinkedFS',$page->field('internal_catalog_id'));
 	my $perList=undef;
 	foreach my $phy_fs_data (@$perFS)
 	{
 		$perList .=$perList ? ",$phy_fs_data->{'person_id'}" :  $phy_fs_data->{'person_id'};
-	}	
+	}
 	$page->field('capitated_contract', 1) if $recExist->{value_int};
 	$page->field('add_mode', 0);
 	$page->field('org_id',$orgList);
@@ -231,14 +232,14 @@ sub populateData_remove
 sub checkDupName
 {
 	my ($self, $page) = @_;
-	
+
 	my $catalogExists = $STMTMGR_CATALOG->recordExists($page, STMTMGRFLAG_NONE,
 		'sel_catalog_by_id_orgId', $page->field('catalog_id'), $page->session('org_internal_id'));
 
 	my $field = $self->getField('catalog_id');
 	my $fieldValue = $page->field('catalog_id');
-	
-	$field->invalidate($page, qq{Fee Schedule Name '$fieldValue' already exists for this Org.}) 
+
+	$field->invalidate($page, qq{Fee Schedule Name '$fieldValue' already exists for this Org.})
 		if $catalogExists;
 }
 
@@ -258,9 +259,9 @@ sub execute
 	my $status = $page->field('status');
 	my $internalCatalogId = $page->field('internal_catalog_id');
 	my $phy_Id = $page->field('physician_id');
-	my $org_Id = $page->field('org_id');		
+	my $org_Id = $page->field('org_id');
 	my $parentCatalog = $STMTMGR_CATALOG->getRowAsHash($page,STMTMGRFLAG_NONE,'selInternalCatalogIdByIdType',$page->session('org_internal_id'),$page->field('parent_catalog_name'),$FS_CATALOG_TYPE);
-	
+
 	my $newId = $page->schemaAction(
 		'Offering_Catalog', $command,
 		internal_catalog_id => $command eq 'add' ? undef : $internalCatalogId,
@@ -272,13 +273,13 @@ sub execute
 		rvrbs_multiplier => $page->field('rvrbs_multiplier') || undef,
 		parent_catalog_id => $parentCatalog->{internal_catalog_id} || undef,
 		effective_begin_date  =>$page->field('effective_begin_date')||undef,
-		effective_end_date => $page->field('effective_end_date') ||undef, 
+		effective_end_date => $page->field('effective_end_date') ||undef,
 		_debug => 0
 	);
 
 	$page->field('internal_catalog_id', $newId) if $command eq 'add';
-	
-	saveAttribute($page, 'OfCatalog_Attribute', $internalCatalogId || $newId , 'Capitated Contract', 
+
+	saveAttribute($page, 'OfCatalog_Attribute', $internalCatalogId || $newId , 'Capitated Contract',
 		App::Universal::ATTRTYPE_BOOLEAN, $STMTMGR_CATALOG, 'sel_Catalog_Attribute',
 		value_int => defined $page->field('capitated_contract') ? 1 : 0,
 	) if $command ne 'remove';
@@ -309,13 +310,13 @@ sub savePerOrgAttr
 					item_name => $FS_TEXT,
 					item_type => 0,
 					value_type => $FS_ATTRR_TYPE,
-				value_int => $fsId,			
+				value_int => $fsId,
 			)if $_;
 		}
-	
+
 		foreach (@orgList)
 		{
-			my $orgParentId =  $STMTMGR_ORG->getSingleValue($page, STMTMGRFLAG_NONE, 'selOrgId', $page->session('org_internal_id'), $_);		
+			my $orgParentId =  $STMTMGR_ORG->getSingleValue($page, STMTMGRFLAG_NONE, 'selOrgId', $page->session('org_internal_id'), $_);
 			$page->schemaAction(
 					'Org_Attribute', 'add',
 					item_id => $page->field('org_item_id') || undef,
@@ -323,7 +324,7 @@ sub savePerOrgAttr
 					item_name => $FS_TEXT,
 					item_type => 0,
 					value_type => $FS_ATTRR_TYPE,
-					value_int => $fsId,		
+					value_int => $fsId,
 			) if $orgParentId;
 		}
 	}
@@ -333,13 +334,13 @@ sub savePerOrgAttr
 sub saveAttribute
 {
 	my ($page, $table, $parentId, $itemName, $valueType, $stmtMgr, $stmt, %data) = @_;
-	
-	my $recExist = $stmtMgr->getRowAsHash($page, STMTMGRFLAG_NONE, $stmt, 
+
+	my $recExist = $stmtMgr->getRowAsHash($page, STMTMGRFLAG_NONE, $stmt,
 		$parentId, $valueType, $itemName);
-	
+
 	my $itemId = $recExist->{item_id};
 	my $command = $itemId ? 'update' : 'add';
-	
+
 	my $newItemId = $page->schemaAction(
 		$table, $command,
 		item_id    => $command eq 'add' ? undef : $itemId,
@@ -347,7 +348,7 @@ sub saveAttribute
 		item_name  => $itemName,
 		value_type => $valueType,
 		%data
-	);	
+	);
 
 	return $newItemId;
 }
@@ -391,13 +392,13 @@ sub new
 			#hints => 'Numeric Fee Schedule ID',
 		),
 		new CGI::Dialog::Subhead(heading => '', name => ''),
-		
+
 		new App::Dialog::Field::Catalog::ID::New(caption => 'New Fee Schedule Name',
 			name => 'new_catalog_id',
 			size => 25,
 			options => FLDFLAG_REQUIRED,
 		),
-		new CGI::Dialog::Field(caption => 'New Fee Schedule Caption', 
+		new CGI::Dialog::Field(caption => 'New Fee Schedule Caption',
 			name => 'caption',
 			size => 45,
 			hints => 'Same as Source if not specified',
@@ -407,13 +408,13 @@ sub new
 			type => 'memo',
 		),
 		new CGI::Dialog::Field(caption => 'Make all entries capitated',
-			type => 'select', 
-			style => 'radio', 
+			type => 'select',
+			style => 'radio',
 			selOptions => 'Capitated;FFS;Copy As Is',
 			name => 'copy_entries_as'
 		),
 	);
-	
+
 	$self->addFooter(new CGI::Dialog::Buttons);
 
 	return $self;
@@ -434,7 +435,7 @@ sub checkDupName
 		'selInternalCatalogIdByIdType', $page->session('org_internal_id'),$page->field('new_catalog_id'),$FS_CATALOG_TYPE);
 
 	my $field = $self->getField('new_catalog_id');
-	$field->invalidate($page, qq{Fee Schedule Name already exists for this Org.}) 
+	$field->invalidate($page, qq{Fee Schedule Name already exists for this Org.})
 		if $catalogExists;
 }
 
@@ -443,7 +444,7 @@ sub customValidate
 	my ($self, $page) = @_;
 	$self->checkDupName($page);
 
-	my $catalog = $STMTMGR_CATALOG->getRowAsHash($page, STMTMGRFLAG_NONE,'selInternalCatalogIdByIdType', 
+	my $catalog = $STMTMGR_CATALOG->getRowAsHash($page, STMTMGRFLAG_NONE,'selInternalCatalogIdByIdType',
 			$page->session('org_internal_id'),$page->field('catalog_id'),$FS_CATALOG_TYPE);
 	$page->field('internal_catalog_id',$catalog->{internal_catalog_id});
 	my $attribute = $STMTMGR_CATALOG->getRowAsHash($page, STMTMGRFLAG_NONE,
@@ -454,7 +455,7 @@ sub customValidate
 	{
 		$self->updateFieldFlags('copy_entries_as', FLDFLAG_INVISIBLE, 0);
 		$page->field('copy_entries_as', 'Copy As Is');
-		
+
 		my $getCapCheckboxField = $self->getField('copy_entries_as');
 		$getCapCheckboxField->invalidate($page, 'Existing Fee Schedule ID is a capitated contract. Make all items capitated?');
 
@@ -465,16 +466,16 @@ sub customValidate
 sub execute
 {
 	my ($self, $page, $command, $flags) = @_;
-	
+
 	my $orgId = $page->session('org_internal_id');
 	my $orgName = $page->session('org_id');
 	my $existingCatalog = $STMTMGR_CATALOG->getRowAsHash($page, STMTMGRFLAG_NONE,
 		'selCatalogById', $page->field('internal_catalog_id'));
-	
+
 	my $newInternalCatalogId = $page->schemaAction(
 		'Offering_Catalog', 'add',
 		internal_catalog_id => undef,
-		catalog_id => $page->field('new_catalog_id') || undef,		
+		catalog_id => $page->field('new_catalog_id') || undef,
 		catalog_type => 0,
 		caption => $page->field('caption') || $existingCatalog->{caption} || undef,
 		description => $page->field('description') || $existingCatalog->{description} || undef,
@@ -488,11 +489,11 @@ sub execute
 		'sel_Catalog_Attribute', $page->field('internal_catalog_id'), App::Universal::ATTRTYPE_BOOLEAN,
 		'Capitated Contract');
 
-	App::Dialog::Catalog::saveAttribute($page, 'OfCatalog_Attribute', $newInternalCatalogId, 
-		'Capitated Contract', App::Universal::ATTRTYPE_BOOLEAN, $STMTMGR_CATALOG, 
-		'sel_Catalog_Attribute', value_int => defined $attribute->{value_int} ? 
+	App::Dialog::Catalog::saveAttribute($page, 'OfCatalog_Attribute', $newInternalCatalogId,
+		'Capitated Contract', App::Universal::ATTRTYPE_BOOLEAN, $STMTMGR_CATALOG,
+		'sel_Catalog_Attribute', value_int => defined $attribute->{value_int} ?
 		$attribute->{value_int} : 0,
-	);	
+	);
 
 	my $sessionId = $page->session('_session_id');
 	my $userId = $page->session('user_id');
@@ -507,7 +508,7 @@ sub execute
 			insert into Offering_Catalog_Entry (cr_session_id, cr_stamp, cr_user_id, cr_org_internal_id,
 				catalog_id, parent_entry_id, entry_type, flags, status, code, modifier, name, default_units,
 				cost_type, unit_cost, description, units_avail,data_text)
-			(select '$sessionId', sysdate, '$userId', '$orgId', $newInternalCatalogId, parent_entry_id, 
+			(select '$sessionId', sysdate, '$userId', '$orgId', $newInternalCatalogId, parent_entry_id,
 				entry_type, $flags, status, code, modifier, name, default_units, cost_type, unit_cost,
 				description, units_avail ,data_text
 			from Offering_Catalog_Entry where catalog_id = $internalCatalogId)
@@ -519,7 +520,7 @@ sub execute
 			insert into Offering_Catalog_Entry (cr_session_id, cr_stamp, cr_user_id, cr_org_internal_id,
 				catalog_id, parent_entry_id, entry_type, flags, status, code, modifier, name, default_units,
 				cost_type, unit_cost, description, units_avail,data_text)
-			(select '$sessionId', sysdate, '$userId', '$orgId', $newInternalCatalogId, parent_entry_id, 
+			(select '$sessionId', sysdate, '$userId', '$orgId', $newInternalCatalogId, parent_entry_id,
 				entry_type, flags, status, code, modifier, name, default_units, cost_type, unit_cost,
 				description, units_avail ,data_text
 			from Offering_Catalog_Entry where catalog_id = $internalCatalogId)
