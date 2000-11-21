9ee98fb8f81cb345423d1e011d8f9c6d9ca99104
alex_hillman <alex_hillman>
2000-11-21 19:45:26 +0000

    Init. implm.

---
+++ HealthSuite/Scripts/Build/BUILD_0017/BUILD_0017_creating_new_frank_tables.sql
@@ -0,0 +1,18 @@
+start tables/Invoice_Worklist
+start tables-code/Invoice_Worklist
+
+start tables/Statement
+start tables-code/Statement
+
+start tables/Statement_Invoice_IDS
+start tables-code/Statement_Invoice_IDS
+
+
+start tables/Org_Association
+start tables-code/Org_Association
+
+start tables/Org_Association_Status 
+start data/Org_Association_Status 
+
+start tables/Org_Association_Type
+start data/Org_Association_Type
