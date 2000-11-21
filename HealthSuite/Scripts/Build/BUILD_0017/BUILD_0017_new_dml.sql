dc7f6c7d077689dc6b3e045f154824de01fd0ee6
alex_hillman <alex_hillman>
2000-11-21 19:51:27 +0000

    Init. Implm

---
+++ HealthSuite/Scripts/Build/BUILD_0017/BUILD_0017_new_dml.sql
@@ -0,0 +1,52 @@
+whenever sqlerror exit sql.sqlcode rollback
+
+prompt Inserting into HCFA1500_Modifier_Code
+
+insert into HCFA1500_Modifier_Code (id, caption, abbrev, result) values (116, 'Item or service prov. as routine care in an apprv. clin. trial', 'QV', 2);
+insert into HCFA1500_Modifier_Code (id, caption, abbrev, result) values (117, 'Waived test', 'QW', 2);
+
+prompt Inserting into Payment_Type
+
+insert into payment_type (id, caption, group_name) values (8, 'Copay', 'personal');
+
+prompt Populating Invoice_History
+
+delete from invoice_history;
+
+insert into Invoice_History (
+	cr_stamp,
+	cr_user_id,
+	cr_org_internal_id,
+	cr_session_id,
+	parent_id,
+	value_text,
+	value_textb,
+	value_int,
+	value_intb,
+	value_float,
+	value_floatb,
+	value_date,
+	value_dateend,
+	value_datea,
+	value_dateb
+)
+select
+	cr_stamp,
+	cr_user_id,
+	cr_org_internal_id,
+	cr_session_id,
+	parent_id,
+	value_text,
+	value_textb,
+	value_int,
+	value_intb,
+	value_float,
+	value_floatb,
+	value_date,
+	value_dateend,
+	value_datea,
+	value_dateb
+from Invoice_Attribute where item_name = 'Invoice/History/Item';
+
+
+commit;
