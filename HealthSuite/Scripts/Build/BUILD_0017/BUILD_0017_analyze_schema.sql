00cc78958c537379369b9026b476f8d39d5385f4
alex_hillman <alex_hillman>
2000-11-27 21:43:09 +0000

    Changed schema name

--- HealthSuite/Scripts/Build/BUILD_0017/BUILD_0017_analyze_schema.sql
+++ HealthSuite/Scripts/Build/BUILD_0017/BUILD_0017_analyze_schema.sql
@@ -1,2 +1,2 @@
 alter session set sort_area_size=200000000;
-exec dbms_utility.analyze_schema('PRO_TEST', 'COMPUTE', 0, 0, 'FOR TABLE FOR ALL INDEXES FOR ALL COLUMNS');
+exec dbms_utility.analyze_schema('PRO_TEST_NEW', 'COMPUTE', 0, 0, 'FOR TABLE FOR ALL INDEXES FOR ALL COLUMNS');
