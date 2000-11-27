e4228133388d087022d994e350fbc07eb576bbef
munir_faridi <munir_faridi>
2000-11-27 22:49:41 +0000

    [B1620] Fixed prepare method not overridden error.

--- HealthSuite/Lib/perl/App/Page/Person.pm
+++ HealthSuite/Lib/perl/App/Page/Person.pm
@@ -199,7 +199,7 @@ sub prepare_page_content_header
 		qq{<SELECT onchange="if(this.selectedIndex > 0) window.location.href = this.options[this.selectedIndex].value">
 			<OPTION>Choose Action</OPTION>
 			<OPTION value="/person/$personId/dlg-add-referral?home=$homeArl">Add Service Request</OPTION>
-			<OPTION value="/person/$personId/dlg-add-appointment?_dialogreturnurl=/person/$personId">Schedule Appointment</OPTION>
+			<OPTION value="/person/$personId/dlg-add-appointment?_dialogreturnurl=/person/$personId/profile">Schedule Appointment</OPTION>
 			<OPTION value="/person/$personId/dlg-add-claim?home=$homeArl">Add Claim</OPTION>
 			<OPTION value="/person/$personId/dlg-add-claim?home=$homeArl&isHosp=1">Add Hospital Claim</OPTION>
 			<OPTION value="/person/$personId/dlg-add-invoice?home=$homeArl">Add Invoice</OPTION>
