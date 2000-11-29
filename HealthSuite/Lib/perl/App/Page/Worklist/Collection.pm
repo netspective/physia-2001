bbbb57383027488b2a8a1e28494d01ad9d9f17c3
frank_major <frank_major>
2000-11-29 14:55:17 +0000

    [B1645] Used yesterday instead of today so would not have to use >= in SQL statement

--- HealthSuite/Lib/perl/App/Page/Worklist/Collection.pm
+++ HealthSuite/Lib/perl/App/Page/Worklist/Collection.pm
@@ -98,14 +98,14 @@ $COLLECTIONARL='/worklist/collection';
 sub collectionQuery
 {
 	my $self = shift;
-	my $date=  UnixDate('yesterday','%d-%b-%y');
+	my $date=  UnixDate('tomorrow','%d-%b-%y');
 	my $sqlGen = new SQL::GenerateQuery(file => $QDL);
 
 	my $cond1 = $sqlGen->WHERE('owner_id', 'is', $self->session('person_id'));
 	my $cond2 = $sqlGen->WHERE('worklist_status', 'is','Account In Collection' );
 	my $cond3 = $sqlGen->WHERE('worklist_type', 'is','Collection' );
 	my $cond4 = $sqlGen->WHERE('responsible_id', 'is',$self->session('person_id') );
-	my $cond5 = $sqlGen->WHERE('reck_date', 'lessthan',$date);
+	my $cond5 = $sqlGen->WHERE('reck_date','lessthan',$date);
 	my $cond6 = $sqlGen->WHERE('reck_date', 'isnotdefined');
 	my $cond7 = $sqlGen->OR($cond5,$cond6);
 	my $query = $sqlGen->AND($cond1,$cond2,$cond3,$cond4,$cond7);
