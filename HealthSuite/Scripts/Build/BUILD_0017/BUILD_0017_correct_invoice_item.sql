7258243c71335aba4847913c3fbc3633d554c875
alex_hillman <alex_hillman>
2000-12-04 17:28:22 +0000

    Init. implm.

---
+++ HealthSuite/Scripts/Build/BUILD_0017/BUILD_0017_correct_invoice_item.sql
@@ -0,0 +1,16 @@
+update invoice_item iic set data_num_a = (select data_num_a from invoice_item 
+iip where iip.item_id = iic.parent_item_id) where iic.item_type = 7 and 
+iic.data_num_a is NULL and exists (select data_num_a from invoice_item iip 
+where iip.item_id = iic.parent_item_id);
+
+update invoice_item iic set hcfa_service_place = (select hcfa_service_place 
+from invoice_item iip where iip.item_id = iic.parent_item_id) where 
+iic.item_type = 7 and iic.hcfa_service_place is NULL and exists (select 
+hcfa_service_place from invoice_item iip where iip.item_id = iic.parent_item_id);
+
+update invoice_item iic set hcfa_service_type = (select hcfa_service_type from 
+invoice_item iip where iip.item_id = iic.parent_item_id) where iic.item_type = 
+7 and iic.hcfa_service_type is NULL and exists (select hcfa_service_type from 
+invoice_item iip where iip.item_id = iic.parent_item_id);
+
+commit;
