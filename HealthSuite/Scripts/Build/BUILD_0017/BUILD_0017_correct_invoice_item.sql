81148914561ef65aa0e012ffa233f91b0c3f7cc8
alex_hillman <alex_hillman>
2000-12-05 21:15:09 +0000

    Added exit with rollback if sql error

--- HealthSuite/Scripts/Build/BUILD_0017/BUILD_0017_correct_invoice_item.sql
+++ HealthSuite/Scripts/Build/BUILD_0017/BUILD_0017_correct_invoice_item.sql
@@ -1,3 +1,5 @@
+whenever sqlerror exit sql.sqlcode rollback
+
 update invoice_item iic set data_num_a = (select data_num_a from invoice_item 
 iip where iip.item_id = iic.parent_item_id) where iic.item_type = 7 and 
 iic.data_num_a is NULL and exists (select data_num_a from invoice_item iip 
