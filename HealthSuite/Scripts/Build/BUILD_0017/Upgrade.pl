b420f2507387ea035e45e5bfe5a9e3c234c1ac4f
thai_nguyen <thai_nguyen>
2000-12-08 01:08:05 +0000

    parameterize analyze_schema.sql

--- HealthSuite/Scripts/Build/BUILD_0017/Upgrade.pl
+++ HealthSuite/Scripts/Build/BUILD_0017/Upgrade.pl
@@ -24,11 +24,14 @@ system(qq{
 	./GenerateSchema.pl
 });
 
+my $schemaName = uc($sqlPlusKey);
+$schemaName =~ s/\/.*//;
+
 runSQL('BUILD_0017_alter_schema.sql');
 runSQL('BUILD_0017_creating_new_frank_tables.sql');
 runSQL('BUILD_0017_new_dml.sql');
 runSQL('BUILD_0017_correct_invoice_item.sql');
-runSQL('BUILD_0017_analyze_schema.sql');
+runSQL('BUILD_0017_analyze_schema.sql', $schemaName);
 runSQL('BUILD_0017_compile_invalid_and_disabled.sql');
 
 
