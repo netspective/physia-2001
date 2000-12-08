a0bfcdf88e74553ff7aad770ae2588f7f5f27e38
radha_kotagiri <radha_kotagiri>
2000-12-08 18:58:38 +0000

    [B1740] Updated the dialog to make the fields other than the lastName and Firstname as not required

--- HealthSuite/Lib/perl/App/Dialog/ShortFormPerson.pm
+++ HealthSuite/Lib/perl/App/Dialog/ShortFormPerson.pm
@@ -21,8 +21,8 @@ use vars qw(@ISA %RESOURCE_MAP);
 
 %RESOURCE_MAP = (
 	'shortformPerson' => {
-		heading => '$Command Person',
-		_arl => ['person_id'], 
+		heading => '$Command Patient',
+		_arl => ['person_id'],
 		_idSynonym => 'shortPerson'
 	},
 );
@@ -30,7 +30,7 @@ use vars qw(@ISA %RESOURCE_MAP);
 sub new
 {
 	my ($self, $command) = CGI::Dialog::new(@_, id => 'shortformPerson');
-	
+
 	my $schema = $self->{schema};
 	delete $self->{schema};  # make sure we don't store this!
 
@@ -41,61 +41,57 @@ sub new
 		new CGI::Dialog::Field(type => 'hidden', name => 'work_phone_item_id'),
 		new CGI::Dialog::Field(type => 'hidden', name => 'driver_license_item_id'),
 		new CGI::Dialog::Field(type => 'hidden', name => 'addr_item_id'),
-		
-		new App::Dialog::Field::Person::ID::New(caption => 'Person / Patient ID', 
+
+		new App::Dialog::Field::Person::ID::New(caption => 'Person / Patient ID',
 			name => 'person_id',
-			readOnlyWhen => CGI::Dialog::DLGFLAG_UPDORREMOVE, 
+			readOnlyWhen => CGI::Dialog::DLGFLAG_UPDORREMOVE,
 		#	options => FLDFLAG_REQUIRED,
 		#	hints => '',
 			postHtml => qq{&nbsp; <a href="javascript:doActionPopup('/lookup/person');">Lookup Persons</a>},
 		),
-		
+
 		new App::Dialog::Field::Person::Name(),
-		
+
 		new CGI::Dialog::Field(caption => 'Gender',
 			name => 'gender',
 			type=> 'select',
 			selOptions => 'Male:1;Female:2',
 			style => 'radio',
-			options => FLDFLAG_REQUIRED,
 		),
 		new CGI::Dialog::Field(caption => 'Date of Birth',
 			name => 'date_of_birth',
 			type => 'date',
 			futureOnly => 0,
 			defaultValue => '',
-			options => FLDFLAG_REQUIRED,
 		),
 		new App::Dialog::Field::Address(caption=>'Home Address',
 			name => 'address',
-			options => FLDFLAG_REQUIRED,
 		),
 		new CGI::Dialog::MultiField(
-			fields => 
+			fields =>
 			[
-				new CGI::Dialog::Field(type => 'phone', caption => 'Home ', 
-					name => 'home_phone', 
-					options => FLDFLAG_REQUIRED, 
+				new CGI::Dialog::Field(type => 'phone', caption => 'Home ',
+					name => 'home_phone',
 				),
-				new CGI::Dialog::Field(type => 'phone', caption => 'Work Phone', 
-					name => 'work_phone', 
+				new CGI::Dialog::Field(type => 'phone', caption => 'Work Phone',
+					name => 'work_phone',
 				),
 			]
 		),
-		
+
 		new CGI::Dialog::Field(caption => 'Social Security Number',
-			name => 'ssn', 
+			name => 'ssn',
 			type=> 'ssn'
 		),
 		new CGI::Dialog::MultiField(
-			fields => 
+			fields =>
 			[
 				new CGI::Dialog::Field(caption => 'Drivers License Number', name => 'license_number'),
 				new CGI::Dialog::Field(caption => 'State', name => 'license_state', size => 2, maxLength => 2,)
 			]
 		),
 	);
-	
+
 	$self->addFooter(new CGI::Dialog::Buttons);
 
 	return $self;
@@ -113,30 +109,30 @@ sub populateData
 
 	my $personId = $page->field('person_id', $page->param('person_id'));
 	return unless $flags & CGI::Dialog::DLGFLAG_UPDORREMOVE_DATAENTRY_INITIAL;
-	
+
 	$STMTMGR_PERSON->createFieldsFromSingleRow($page, STMTMGRFLAG_NONE, 'selPersonData', $personId);
-	
+
 	my $itemName = 'Driver/License';
-	my $attribute =  $STMTMGR_PERSON->getRowAsHash($page, STMTMGRFLAG_NONE, 'selAttribute', 
+	my $attribute =  $STMTMGR_PERSON->getRowAsHash($page, STMTMGRFLAG_NONE, 'selAttribute',
 		$personId, $itemName);
 	$page->field('driver_license_item_id', $attribute->{'item_id'});
 	$page->field('license_number', $attribute->{'value_text'});
 	$page->field('license_state', $attribute->{'value_textb'});
-	
+
 	$itemName = 'Home';
-	$attribute =  $STMTMGR_PERSON->getRowAsHash($page, STMTMGRFLAG_NONE, 
-		'selAttributeByItemNameAndValueTypeAndParent', $personId, $itemName, 
+	$attribute =  $STMTMGR_PERSON->getRowAsHash($page, STMTMGRFLAG_NONE,
+		'selAttributeByItemNameAndValueTypeAndParent', $personId, $itemName,
 		App::Universal::ATTRTYPE_PHONE);
 	$page->field('home_phone_item_id', $attribute->{'item_id'});
 	$page->field('home_phone', $attribute->{'value_text'});
 
 	$itemName = 'Work';
-	$attribute =  $STMTMGR_PERSON->getRowAsHash($page, STMTMGRFLAG_NONE, 
-		'selAttributeByItemNameAndValueTypeAndParent', $personId, $itemName, 
+	$attribute =  $STMTMGR_PERSON->getRowAsHash($page, STMTMGRFLAG_NONE,
+		'selAttributeByItemNameAndValueTypeAndParent', $personId, $itemName,
 		App::Universal::ATTRTYPE_PHONE);
 	$page->field('home_phone_item_id', $attribute->{'item_id'});
 	$page->field('work_phone', $attribute->{'value_text'});
-	
+
 	my $address = $STMTMGR_PERSON->getRowAsHash($page, STMTMGRFLAG_NONE, 'selHomeAddress', $personId);
 	$page->field('addr_item_id', $address->{item_id});
 	$page->field('addr_line1', $address->{line1});
@@ -185,7 +181,7 @@ sub execute
 		value_text => $page->field('work_phone'),
 		_debug => 0
 	) if $page->field('work_phone');
-	
+
 	$page->schemaAction(
 		'Person_Address', $page->field('addr_item_id') ? 'update' : 'add',
 		item_id => $page->field('addr_item_id') || undef,
