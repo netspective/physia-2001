5b23390d94d09abdb3a38876bf07432352bd4c0b
shahbaz_javeed <shahbaz_javeed>
2000-12-12 18:18:49 +0000

    Clinical Notes: Allows the user to choose from a list of xml files to use generate the forms from.

--- HealthSuite/Lib/perl/App/Page/Person/ClinicalNote.pm
+++ HealthSuite/Lib/perl/App/Page/Person/ClinicalNote.pm
@@ -20,22 +20,55 @@ sub prepare_view
 {
 	my ($self) = @_;
 
-	$self->addContent(qq{
-		<script src="/lib/dynamic-page.js" language="JavaScript1.2"></script>
-		<link rel="stylesheet" type="text/css" href="/lib/dynamic-page.css">
+	my $personId = $self->param ('person_id');
+	my $templateFile = $self->param ('template');
+	my $html;
+	
+	if ($templateFile) {
+		$html = qq{
+			<script src="/lib/dynamic-page.js" language="JavaScript1.2"></script>
+			<link rel="stylesheet" type="text/css" href="/lib/dynamic-page.css">
+	
+			<script>
+				if(loadSource('/resources/data/$templateFile'))
+				{
+					var html = createTemplateHtml(activeTemplate);
+					document.write(html);
+				}
+				else
+				{
+					document.write("Unable to load source file.");
+				}
+			</script>
+		};
+	} else {
+		$html = $self->findResources (qq{/person/$personId/cnote?template=});
+	}
 
-		<script>
-			if(loadSource('/resources/data/clinical-dictionary.xml'))
-			{
-				var html = createTemplateHtml(activeTemplate);
-				document.write(html);
-			}
-			else
-			{
-				document.write("Unable to load source file.");
-			}
-		</script>
+	$self->addContent(qq{
+		$html
 	});
+
+}
+
+sub findResources {
+	my ($self) = shift;
+	my ($url) = shift;
+	my $html = qq{Please choose from the follow xml templates:<br>};
+	
+	my $dirOpened = opendir DIRHANDLE, '/export/home/sjaveed/projects/HealthSuite/WebSite/resources/data';
+	
+	if ($dirOpened) {
+		my @xmlDatafiles = grep /\.xml$/i, readdir DIRHANDLE;
+		
+		foreach my $xmlFile (@xmlDatafiles) {
+			$html .= qq{<a href="$url$xmlFile">$xmlFile</a><br>};
+		}
+	} else {
+		$html .= '<b>Couldnt open directory /export/home/sjaveed/projects/HealthSuite/WebSite/resources/data<br>';
+	}
+	
+	return $html;
 }
 
 1;
