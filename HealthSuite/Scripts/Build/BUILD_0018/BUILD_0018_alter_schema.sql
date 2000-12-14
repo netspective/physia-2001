1aa04459665b800746e8de00644ee3e335b7726b
thai_nguyen <thai_nguyen>
2000-12-14 16:39:04 +0000

    [B713] added Appointment Alert Trans Type

---
+++ HealthSuite/Scripts/Build/BUILD_0018/BUILD_0018_alter_schema.sql
@@ -0,0 +1,3 @@
+insert into Transaction_Type (id, caption, group_name, icon_img_summ, remarks) 
+	values (8026, 'Patient Appointment Alert', 'Alert', 'alert_btn.gif', 
+	'Alert requiring action on a patient appointments');
