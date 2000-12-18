cfe68ffa2e19e77042ddf0c0534dd560c912fb4e
alex_hillman <alex_hillman>
2000-12-18 17:19:47 +0000

    Made changes to use bind variables in insert and delete statements

--- HealthSuite/Database/schema-physia-src/oracle-imports/pre/011-pkg-set.sql
+++ HealthSuite/Database/schema-physia-src/oracle-imports/pre/011-pkg-set.sql
@@ -24,30 +24,50 @@ create or replace package body Pkg_Set as
 		v_remainingMembers varchar2(512) := p_membersList;	
 	begin
 		begin
+		        execute immediate 'delete from '||p_tableName||' where parent_id = '''||:parent_id||'''' using p_parentID; 
+/*	        
 			v_cursor := DBMS_SQL.OPEN_CURSOR;
 			DBMS_SQL.PARSE(v_cursor, 'delete from ' || p_tableName || ' where parent_id = ''' || p_parentId || '''', DBMS_SQL.NATIVE);
 			v_NumRows := DBMS_SQL.EXECUTE(v_cursor);
 			DBMS_SQL.CLOSE_CURSOR(v_cursor);
+
+*/
 		exception
 			when others then null;
 		end;
 
+/*
+
 		v_cursor := DBMS_SQL.OPEN_CURSOR;
+		
+		
 		DBMS_SQL.PARSE(v_cursor, 'insert into ' || p_tableName || ' (parent_id, member_name) values (''' || p_parentId || ''', :member_name)', DBMS_SQL.NATIVE);
+*/
+
 
 		if v_delim_comma > 0 then
 			while v_delim_comma > 0 
 			loop
 				v_activeMember := substr(v_remainingMembers, 1, v_delim_comma-1);
-				DBMS_SQL.BIND_VARIABLE(v_cursor, 'member_name', v_activeMember);
+/*				
+				DBMS_SQL.BIND_VARIABLE(v_cursor, 'member_name', v_activeMember);   */
 				v_NumRows := DBMS_SQL.EXECUTE(v_cursor);
+*/
+				execute immediate 'insert into '||p_tableName||' (parent_id, member_name) values('':parent_id'', '':member_name'')'
+				  using p_parentID, v_activeMember;
+				
 				v_remainingMembers := substr(v_remainingMembers, v_delim_comma+1);
 				v_delim_comma := INSTR(v_remainingMembers, p_delim);
 			end loop;
 		end if;
-		if length(v_remainingMembers) > 0 then 
+		if length(v_remainingMembers) > 0 then
+		
+			execute immediate 'insert into '||p_tableName||' (parent_id, member_name) values('':parent_id'', '':member_name'')'
+			  using p_parentID, v_remainingMembers;
+/*
 			DBMS_SQL.BIND_VARIABLE(v_cursor, 'member_name', v_remainingMembers);
 			v_NumRows := DBMS_SQL.EXECUTE(v_cursor);
+*/
 		end if;
 
 		DBMS_SQL.CLOSE_CURSOR(v_cursor);
