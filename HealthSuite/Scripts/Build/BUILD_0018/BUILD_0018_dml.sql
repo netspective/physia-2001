29fdcf94dcc3ffeab835329b93f3aaf787fff686
alex_hillman <alex_hillman>
2000-12-19 21:56:51 +0000

    Init. implm.

---
+++ HealthSuite/Scripts/Build/BUILD_0018/BUILD_0018_dml.sql
@@ -0,0 +1,45 @@
+whenever sqlerror exit sql.sqlcode rollback
+
+--bug 1792
+
+insert into Adjust_Method (id, caption, abbrev) values (4, 'Transfer to Next Payer', '');
+insert into Adjust_Method (id, caption, abbrev) values (5, 'Reverse Transfer to Next Payer', '');
+insert into Adjust_Method (id, caption, abbrev) values (6, 'Reverse Payment', '');
+insert into writeoff_type (id, caption, abbrev) values (11, 'Transfer from Legacy System', '');
+
+--bug 1813
+
+insert into Ethnicity (id, caption) values (5, 'Other');
+
+--bug 1795
+
+insert into Session_Action_Type (id, caption) values (10, 'Prescribe');
+insert into Session_Action_Type (id, caption) values (11, 'Refill');
+
+--bug unknown - Thai?
+
+insert into Transaction_Type (id, caption, group_name, icon_img_summ, remarks) 
+	values (8026, 'Patient Appointment Alert', 'Alert', 'alert_btn.gif', 
+	'Alert requiring action on a patient appointments');
+
+
+--bug 1336
+
+update INSURANCE_ADDRESS set state=upper(state);
+update INSURANCE_ADDRESS_AUD set state=upper(state);
+update INVOICE_ADDRESS set state=upper(state);
+update INVOICE_ADDRESS_AUD set state=upper(state);
+update ORG_ADDRESS set state=upper(state);
+update ORG_ADDRESS_AUD set state=upper(state);
+update PERSON_ADDRESS set state=upper(state);
+update PERSON_ADDRESS_AUD set state=upper(state);
+update TRANS_ADDRESS set state=upper(state);
+update TRANS_ADDRESS_AUD set state=upper(state);
+
+
+--bug 1833
+
+update Adjust_Method set caption = 'Auto Capitated Service Adjustment' where id=3;
+
+
+commit;
