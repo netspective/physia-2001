499e89e1e22459522d40d9b7907bd534d2452d28
yousuf_sayed <yousuf_sayed>
2000-12-19 10:51:48 +0000

    [B1791] Added a column Speciality as Category in the report

--- HealthSuite/Lib/perl/App/Dialog/Report/Org/General/Accounting/ReferringDoctor.pm
+++ HealthSuite/Lib/perl/App/Dialog/Report/Org/General/Accounting/ReferringDoctor.pm
@@ -10,7 +10,13 @@ use App::Universal;
 use CGI::Dialog;
 use CGI::Validator::Field;
 use DBI::StatementManager;
+
 use Data::Publish;
+use Data::TextPublish;
+use App::Configuration;
+use App::Device;
+use App::Statements::Device;
+
 use App::Statements::Report::ReferringDoctor;
 
 use vars qw(@ISA $INSTANCE);
@@ -64,34 +70,46 @@ sub execute
 
 	my $pub =
 	{
+		reportTitle => "Referring Doctor",
 		columnDefn =>
 		[
 			{
 				colIdx => 0,
+				head => 'Category',
+				hAlign => 'center',
+				dAlign => 'left',
+				dataFmt => '#4#',
+#				groupBy => '#4#',
+			},
+			{
+				colIdx => 1,
 				head => 'Doctor',
 				hAlign => 'center',
 				dAlign => 'left',
 				dataFmt => '#0# <A HREF = "/person/#1#/profile">#1#</A>',
 			},
 			{
-				colIdx => 1,
+				colIdx => 2,
 				head => '# of Patients',
 				hAlign => 'center',
 				dAlign => 'right',
 				dataFmt => '#2#',
+#				summarize => 'sum',
 			},
 			{
-				colIdx => 2,
+				colIdx => 3,
 				head => '% of Patients',
 				hAlign => 'center',
 				dAlign => 'right',
 				dataFmt => '#3#',
+#				summarize => 'sum',
 			},
 		],
 	};
 
 	my $pubOrg =
 	{
+		reportTitle => "Referring Doctor",
 		columnDefn =>
 		[
 			{
@@ -127,7 +145,8 @@ sub execute
 
 
 	my $totalPatients = $STMTMGR_REPORT_REFERRING_DOCTOR->getSingleValue($page, STMTMGRFLAG_NONE, 'totalPatientCount', $page->field('report_begin_date'), $page->field('report_end_date'), $page->field('service_begin_date'), $page->field('service_end_date'));
-	my @data = undef;
+	my @data = ();
+	my @dataText = undef;
 
 	if($page->field('insurance_select') ne '')
 	{
@@ -146,6 +165,8 @@ sub execute
 					sprintf  "%3.2f%", ($_->{patientcount} / $totalPatients) * 100
 				);
 				push(@data, \@rowData);
+				push(@dataText, \@rowData);
+
 				$patients += $_->{patientcount};
 				$percent += ($_->{patientcount} / $totalPatients) * 100;
 				$prvDoctor = $_->{person_id}
@@ -158,8 +179,8 @@ sub execute
 					push(@data, \@rowData1);
 					my @rowData2 = (undef, undef, undef, undef, undef);
 					push(@data, \@rowData2);
-					$patients=0;
-					$percent=0;
+					$patients = 0;
+					$percent = 0;
 				}
 				my @rowData =
 				(
@@ -170,6 +191,7 @@ sub execute
 					sprintf  "%3.2f%", ($_->{patientcount} / $totalPatients) * 100
 				);
 				push(@data, \@rowData);
+				push(@dataText, \@rowData);
 				$patients += $_->{patientcount};
 				$percent += ($_->{patientcount} / $totalPatients) * 100;
 				$prvDoctor = $_->{person_id};
@@ -186,36 +208,159 @@ sub execute
 	else
 	{
 		my $referringPhysician = $STMTMGR_REPORT_REFERRING_DOCTOR->getRowsAsHashList($page, STMTMGRFLAG_NONE, 'patientCount', $page->field('report_begin_date'), $page->field('report_end_date'), $page->field('service_begin_date'), $page->field('service_end_date'));
+
+		my $groupCount = 0;
+		my $groupPercent = 0;
+		my $prevCategory;
+		my $category;
+
 		foreach(@$referringPhysician)
 		{
-			my $patientCountPercent = sprintf  "%3.2f%", ($_->{patientcount} / $totalPatients) * 100;
+
+#			my $patientCountPercent = sprintf  "%3.2f%", ($_->{patientcount} / $totalPatients) * 100;
+			if(($_->{category} ne $prevCategory) && ($groupCount > 0))
+			{
+				$category = $_->{category};
+
+				my @rowData =
+				(
+					undef,
+					undef,
+					"<B>$groupCount</B>",
+					"<B>$groupPercent%</B>",
+					"<B>$prevCategory</B>",
+				);
+				push(@data, \@rowData);
+
+				my @rowDataBlank =
+				(
+					undef,
+					undef,
+					undef,
+					undef,
+					undef,
+				);
+				push(@data, \@rowDataBlank);
+				$groupCount = 0;
+				$groupPercent = 0;
+
+			}
+			elsif($groupCount == 0)
+			{
+				$category = $_->{category};
+			}
+			else
+			{
+				$category = '';
+			}
+			$prevCategory = $_->{category};
+
+
 			my @rowData =
 			(
 				$_->{name},
 				$_->{person_id},
 				$_->{patientcount},
-				$patientCountPercent,
+				$_->{patientpercent},
+				$category,
 			);
 			push(@data, \@rowData);
+			push(@dataText, \@rowData);
+
+			$groupCount += $_->{patientcount};
+			$groupPercent += substr($_->{patientpercent}, 0, length($_->{patientpercent})-1);
+
 		}
+		if($groupCount > 0)
+		{
+			if($prevCategory eq '')
+			{
+				$prevCategory = 'Unknown';
+			}
+			my @rowData =
+			(
+				undef,
+				undef,
+				"<B>$groupCount</B>",
+				"<B>$groupPercent%</B>",
+				"<B>$prevCategory</B>",
+			);
+			push(@data, \@rowData);
+			my @rowDataBlank =
+			(
+				undef,
+				undef,
+				undef,
+				undef,
+				undef,
+			);
+			push(@data, \@rowDataBlank);
+		}
+
 	}
 
 	my $patientTotalPercent = '100.00%' if ($totalPatients !=0);
 	my $html;
 
+	my $hardCopy = $page->field('printReport');
+	my $textOutputFilename;
+
+	# Get a printer device handle...
+	my $printerAvailable = 1;
+	my $printerDevice;
+	$printerDevice = ($page->field('printerQueue') ne '') ? $page->field('printerQueue') : App::Device::getPrinter ($page, 0);
+	my $printHandle = App::Device::openPrintHandle ($printerDevice, "-o cpi=17 -o lpi=6");
+
+	$printerAvailable = 0 if (ref $printHandle eq 'SCALAR');
+
 	if($page->field('insurance_select') ne '')
 	{
 		my @rowData = (	"<B>Grand Total</B>", undef, undef, "<B>$totalPatients</B>", "<B>$patientTotalPercent</B>");
+		my @rowDataText = (	"Grand Total", undef, undef, "$totalPatients", "$patientTotalPercent");
+
 		push(@data, \@rowData);
+		push(@dataText, \@rowDataText);
 		$html = createHtmlFromData($page, 0, \@data, $pubOrg);
+		my $textOutputFilename = createTextRowsFromData($page, 0, \@dataText, $pubOrg);
+
+		if ($hardCopy == 1 and $printerAvailable) {
+			my $reportOpened = 1;
+			my $tempDir = $CONFDATA_SERVER->path_temp();
+			open (ASCIIREPORT, $tempDir.$textOutputFilename) or $reportOpened = 0;
+			if ($reportOpened) {
+				while (my $reportLine = <ASCIIREPORT>) {
+					print $printHandle $reportLine;
+				}
+			}
+			close ASCIIREPORT;
+		}
+
+
 	}
 	else
 	{
-		my @rowData = (	"<B>Total</B>", undef, "<B>$totalPatients</B>",	"<B>$patientTotalPercent</B>");
+		my @rowData = (	"<B>Total</B>", undef, "<B>$totalPatients</B>",	"<B>$patientTotalPercent</B>", undef);
+		my @rowDataText = (	"Total", undef, "$totalPatients", "$patientTotalPercent", undef);
 		push(@data, \@rowData);
+		push(@dataText, \@rowDataText);
 		$html = createHtmlFromData($page, 0, \@data, $pub);
+#		$html = $STMTMGR_REPORT_REFERRING_DOCTOR->createHtml($page, 0, 'patientCount', [$page->field('report_begin_date'), $page->field('report_end_date'), $page->field('service_begin_date'), $page->field('service_end_date')]);
+		$textOutputFilename = createTextRowsFromData($page, 0, \@dataText, $pub);
+
+		if ($hardCopy == 1 and $printerAvailable) {
+			my $reportOpened = 1;
+			my $tempDir = $CONFDATA_SERVER->path_temp();
+			open (ASCIIREPORT, $tempDir.$textOutputFilename) or $reportOpened = 0;
+			if ($reportOpened) {
+				while (my $reportLine = <ASCIIREPORT>) {
+					print $printHandle $reportLine;
+				}
+			}
+			close ASCIIREPORT;
+		}
 	}
-	return $html;
+	return ($textOutputFilename ? qq{<a href="/temp$textOutputFilename">Printable version</a> <br>} : "" ) . $html;
+	#return $html;
 }
 
 # create a new instance which will automatically add it to the directory of
--- HealthSuite/Lib/perl/App/Statements/Report/ReferringDoctor.pm
+++ HealthSuite/Lib/perl/App/Statements/Report/ReferringDoctor.pm
@@ -10,7 +10,7 @@ use App::Universal;
 use vars qw(@ISA @EXPORT $STMTMGR_REPORT_REFERRING_DOCTOR);
 
 @ISA    = qw(Exporter DBI::StatementManager);
-@EXPORT = qw($STMTMGR_REPORT_REFERRING_DOCTOR );
+@EXPORT = qw($STMTMGR_REPORT_REFERRING_DOCTOR);
 
 $STMTMGR_REPORT_REFERRING_DOCTOR = new App::Statements::Report::ReferringDoctor
 (
@@ -19,15 +19,18 @@ $STMTMGR_REPORT_REFERRING_DOCTOR = new App::Statements::Report::ReferringDoctor
 		sqlStmt => qq
 		{
 			select count(i.client_id) patientCount
-			from person p, transaction t, invoice i
+			from person p, transaction t, invoice i,
+				(
+					select parent_id from invoice_item ii
+					where (ii.service_begin_date >= to_date(:3, '$SQLSTMT_DEFAULTDATEFORMAT') OR :3 is NULL)
+					and (ii.service_end_date <= to_date(:4, '$SQLSTMT_DEFAULTDATEFORMAT') OR :4 is NULL)
+					and ii.item_type in (0, 1, 2)
+					and ii.data_text_b is null
+				) inv_items
 			where i.invoice_date between to_date(:1, '$SQLSTMT_DEFAULTDATEFORMAT') and to_date(:2, '$SQLSTMT_DEFAULTDATEFORMAT')
 			and t.trans_id = i.main_transaction
 			and p.person_id = t.data_text_a
-			and i.invoice_id in 
-				(select parent_id from invoice_item ii
-				where (ii.service_begin_date >= to_date(:3, 'MM/DD/YYYY') OR :3 is NULL)
-				and (ii.service_end_date <= to_date(:4, 'MM/DD/YYYY') OR :4 is NULL)
-				)			
+			and i.invoice_id = inv_items.parent_id
 		}
 	},
 
@@ -35,17 +38,52 @@ $STMTMGR_REPORT_REFERRING_DOCTOR = new App::Statements::Report::ReferringDoctor
 	{
 		sqlStmt => qq
 		{
-			select p.simple_name name, p.person_id, count(i.client_id) patientCount
-			from person p, transaction t, invoice i
+			select
+				p.simple_name name,
+				p.person_id,
+				count(i.client_id) patientCount,
+				pa.item_name category,
+				p.name_last,
+				round((count(i.client_id) / cnt.ptCount * 100), 2) || '%' patientPercent
+			from
+				person p,
+				transaction t,
+				invoice i,
+				(
+					select parent_id, item_name
+					from person_attribute
+					where value_type = @{[ App::Universal::ATTRTYPE_SPECIALTY ]}
+					and value_int = @{[ App::Universal::SPECIALTY_PRIMARY ]}
+				) pa,
+				(
+					select count(i.client_id) ptCount
+					from person p, transaction t, invoice i,
+						(
+							select parent_id from invoice_item ii
+							where (ii.service_begin_date >= to_date(:3, '$SQLSTMT_DEFAULTDATEFORMAT') OR :3 is NULL)
+							and (ii.service_end_date <= to_date(:4, '$SQLSTMT_DEFAULTDATEFORMAT') OR :4 is NULL)
+							and ii.item_type in (0, 1, 2)
+							and ii.data_text_b is null
+						) inv_items
+					where i.invoice_date between to_date(:1, '$SQLSTMT_DEFAULTDATEFORMAT') and to_date(:2, '$SQLSTMT_DEFAULTDATEFORMAT')
+					and t.trans_id = i.main_transaction
+					and p.person_id = t.data_text_a
+					and i.invoice_id = inv_items.parent_id
+				) cnt,
+				(
+					select parent_id from invoice_item ii
+					where (ii.service_begin_date >= to_date(:3, '$SQLSTMT_DEFAULTDATEFORMAT') OR :3 is NULL)
+					and (ii.service_end_date <= to_date(:4, '$SQLSTMT_DEFAULTDATEFORMAT') OR :4 is NULL)
+					and ii.item_type in (0, 1, 2)
+					and ii.data_text_b is null
+				) inv_items
 			where i.invoice_date between to_date(:1, '$SQLSTMT_DEFAULTDATEFORMAT') and to_date(:2, '$SQLSTMT_DEFAULTDATEFORMAT')
 			and t.trans_id = i.main_transaction
 			and p.person_id = t.data_text_a
-			and i.invoice_id in 
-				(select parent_id from invoice_item ii
-				where (ii.service_begin_date >= to_date(:3, 'MM/DD/YYYY') OR :3 is NULL)
-				and (ii.service_end_date <= to_date(:4, 'MM/DD/YYYY') OR :4 is NULL)
-				)			
-			group by p.simple_name, p.person_id
+			and i.invoice_id = inv_items.parent_id
+			and p.person_id = pa.parent_id (+)
+			group by p.simple_name, p.person_id, pa.item_name, p.name_last, cnt.ptCount
+			order by pa.item_name, p.name_last
 		},
 
 		sqlStmtBindParamDescr => ['Date Range when the Doctor has referred the patient in which Plan'],
@@ -56,17 +94,18 @@ $STMTMGR_REPORT_REFERRING_DOCTOR = new App::Statements::Report::ReferringDoctor
 			[
 				{
 					colIdx => 0,
-					head => 'Speciality',
+					head => 'Category',
 					hAlign => 'center',
 					dAlign => 'left',
 					dataFmt => '#3#',
+					groupBy => '#3#',
 				},
 				{
 					colIdx => 1,
 					head => 'Doctor',
 					hAlign => 'center',
 					dAlign => 'left',
-					dataFmt => '#0#',
+					dataFmt => '#0# <A HREF = "/person/#1#/profile">#1#</A>',
 				},
 				{
 					colIdx => 2,
@@ -74,6 +113,15 @@ $STMTMGR_REPORT_REFERRING_DOCTOR = new App::Statements::Report::ReferringDoctor
 					hAlign => 'center',
 					dAlign => 'right',
 					dataFmt => '#2#',
+					summarize => 'sum',
+				},
+				{
+					colIdx => 3,
+					head => '% of Patients',
+					hAlign => 'center',
+					dAlign => 'right',
+					dataFmt => '#5#',
+					summarize => 'sum',
 				},
 			],
 		},
@@ -84,7 +132,14 @@ $STMTMGR_REPORT_REFERRING_DOCTOR = new App::Statements::Report::ReferringDoctor
 		sqlStmt => qq
 		{
 			select p.simple_name name, p.person_id, o.name_primary, count(i.client_id) patientCount
-			from person p, transaction t, invoice i, org o, invoice_billing ib, insurance ins
+			from person p, transaction t, invoice i, org o, invoice_billing ib, insurance ins,
+				(
+					select parent_id from invoice_item ii
+					where (ii.service_begin_date >= to_date(:3, 'MM/DD/YYYY') OR :3 is NULL)
+					and (ii.service_end_date <= to_date(:4, 'MM/DD/YYYY') OR :4 is NULL)
+					and ii.item_type in (0, 1, 2)
+					and ii.data_text_b is null
+				) inv_items
 			where i.invoice_date between to_date(:1, '$SQLSTMT_DEFAULTDATEFORMAT') and to_date(:2, '$SQLSTMT_DEFAULTDATEFORMAT')
 			and t.trans_id = i.main_transaction
 			and p.person_id = t.data_text_a
@@ -92,27 +147,26 @@ $STMTMGR_REPORT_REFERRING_DOCTOR = new App::Statements::Report::ReferringDoctor
 			and ib.bill_ins_id = ins.ins_internal_id
 			and ins.ins_org_id = o.org_internal_id
 			and ib.bill_party_type = 3
-			and i.invoice_id in 
-				(select parent_id from invoice_item ii
-				where (ii.service_begin_date >= to_date(:3, 'MM/DD/YYYY') OR :3 is NULL)
-				and (ii.service_end_date <= to_date(:4, 'MM/DD/YYYY') OR :4 is NULL)
-				)			
+			and i.invoice_id = inv_items.parent_id
 			group by p.simple_name, p.person_id, o.name_primary
 
 			union
 
 			select p.simple_name name, p.person_id, null, count(i.client_id) patientCount
-			from person p, transaction t, invoice i, invoice_billing ib
+			from person p, transaction t, invoice i, invoice_billing ib,
+				(
+					select parent_id from invoice_item ii
+					where (ii.service_begin_date >= to_date(:3, 'MM/DD/YYYY') OR :3 is NULL)
+					and (ii.service_end_date <= to_date(:4, 'MM/DD/YYYY') OR :4 is NULL)
+					and ii.item_type in (0, 1, 2)
+					and ii.data_text_b is null
+				) inv_items
 			where i.invoice_date between to_date(:1, '$SQLSTMT_DEFAULTDATEFORMAT') and to_date(:2, '$SQLSTMT_DEFAULTDATEFORMAT')
 			and t.trans_id = i.main_transaction
 			and p.person_id = t.data_text_a
 			and i.billing_id = ib.bill_id
 			and ib.bill_party_type <> 3
-			and i.invoice_id in 
-				(select parent_id from invoice_item ii
-				where (ii.service_begin_date >= to_date(:3, 'MM/DD/YYYY') OR :3 is NULL)
-				and (ii.service_end_date <= to_date(:4, 'MM/DD/YYYY') OR :4 is NULL)
-				)			
+			and i.invoice_id = inv_items.parent_id
 			group by p.simple_name, p.person_id
 		},
 
