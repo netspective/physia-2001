32b78468f34f64122cd42336af796b669b73cfe9
alex_hillman <alex_hillman>
2000-12-20 00:11:06 +0000

    Fixed some errors in execute immediate statements

--- HealthSuite/Database/schema-physia-src/oracle-imports/pre/011-pkg-set.sql
+++ HealthSuite/Database/schema-physia-src/oracle-imports/pre/011-pkg-set.sql
@@ -21,56 +21,42 @@ create or replace package body Pkg_Set as
 		v_numRows number;
 		v_delim_comma NUMBER := INSTR(p_membersList, p_delim);
 		v_activeMember varchar2(32);
-		v_remainingMembers varchar2(512) := p_membersList;	
+		v_remainingMembers varchar2(512) := p_membersList;
+		v_parent_id varchar2(32);
+		
 	begin
+	
+	        v_parent_id := p_parentID;
+	
 		begin
-		        execute immediate 'delete from '||p_tableName||' where parent_id = '''||:parent_id||'''' using p_parentID; 
-/*	        
-			v_cursor := DBMS_SQL.OPEN_CURSOR;
-			DBMS_SQL.PARSE(v_cursor, 'delete from ' || p_tableName || ' where parent_id = ''' || p_parentId || '''', DBMS_SQL.NATIVE);
-			v_NumRows := DBMS_SQL.EXECUTE(v_cursor);
-			DBMS_SQL.CLOSE_CURSOR(v_cursor);
+		        execute immediate 'delete from '||p_tableName||' where parent_id = :parent_id' using v_parent_id; 
 
-*/
 		exception
 			when others then null;
 		end;
-
-/*
-
-		v_cursor := DBMS_SQL.OPEN_CURSOR;
-		
-		
-		DBMS_SQL.PARSE(v_cursor, 'insert into ' || p_tableName || ' (parent_id, member_name) values (''' || p_parentId || ''', :member_name)', DBMS_SQL.NATIVE);
-*/
 
 
 		if v_delim_comma > 0 then
 			while v_delim_comma > 0 
 			loop
 				v_activeMember := substr(v_remainingMembers, 1, v_delim_comma-1);
-/*				
-				DBMS_SQL.BIND_VARIABLE(v_cursor, 'member_name', v_activeMember);   */
-				v_NumRows := DBMS_SQL.EXECUTE(v_cursor);
-*/
-				execute immediate 'insert into '||p_tableName||' (parent_id, member_name) values('':parent_id'', '':member_name'')'
-				  using p_parentID, v_activeMember;
+
+				execute immediate 'insert into '||p_tableName||' (parent_id, member_name) values(:parent_id, :member_name)'
+				using v_parent_id, v_activeMember;
 				
 				v_remainingMembers := substr(v_remainingMembers, v_delim_comma+1);
 				v_delim_comma := INSTR(v_remainingMembers, p_delim);
 			end loop;
 		end if;
+		
 		if length(v_remainingMembers) > 0 then
 		
-			execute immediate 'insert into '||p_tableName||' (parent_id, member_name) values('':parent_id'', '':member_name'')'
-			  using p_parentID, v_remainingMembers;
-/*
-			DBMS_SQL.BIND_VARIABLE(v_cursor, 'member_name', v_remainingMembers);
-			v_NumRows := DBMS_SQL.EXECUTE(v_cursor);
-*/
+		
+			execute immediate 'insert into '||p_tableName||' (parent_id, member_name) values(:parent_id, :member_name)'
+			  using v_parent_id, v_remainingMembers;
+
 		end if;
 
-		DBMS_SQL.CLOSE_CURSOR(v_cursor);
 	end;
 	
 end Pkg_Set;
