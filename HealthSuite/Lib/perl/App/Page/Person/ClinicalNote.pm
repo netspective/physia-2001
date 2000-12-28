d01f441eae7ee1cd35d531b834c5535aa9e2d334
shahbaz_javeed <shahbaz_javeed>
2000-12-28 23:28:31 +0000

    Clinical Notes: Updated perl cnote generator...

--- HealthSuite/Lib/perl/App/Page/Person/ClinicalNote.pm
+++ HealthSuite/Lib/perl/App/Page/Person/ClinicalNote.pm
@@ -22,6 +22,7 @@ sub prepare_view
 
 	my $personId = $self->param ('person_id');
 	my $templateFile = $self->param ('template');
+	my $dictionaryFile = $self->param ('dictionary');
 	my $html;
 	
 	if ($templateFile) {
@@ -30,10 +31,16 @@ sub prepare_view
 			<link rel="stylesheet" type="text/css" href="/lib/dynamic-page.css">
 	
 			<script>
-				if(loadSource('/resources/data/$templateFile'))
+				if(loadSource('/resources/data/$dictionaryFile', '/resources/data/$templateFile'))
 				{
 					var html = createTemplateHtml(activeTemplate);
 					document.write(html);
+					var srcWin = window.open("", "source", "location,status,scrollbars,width=800,height=600");
+					var srcHtml = html.replace (/</g, '&lt;');
+					srcHtml = srcHtml.replace (/>/g, '&gt;');
+					srcHtml = srcHtml.replace (/&lt;/g, '<font color="blue">&lt;</font>');
+					srcHtml = srcHtml.replace (/&gt;/g, '<font color="blue">&gt;</font>');
+					srcWin.document.write('<pre>' + srcHtml + '</pre>');				
 				}
 				else
 				{
@@ -54,18 +61,61 @@ sub prepare_view
 sub findResources {
 	my ($self) = shift;
 	my ($url) = shift;
+	my $personId = $self->param ('person_id');
 	my $html = qq{Please choose from the follow xml templates:<br>};
 	
 	my $dirOpened = opendir DIRHANDLE, $CONFDATA_SERVER->path_XMLData();
 	
 	if ($dirOpened) {
-		my @xmlDatafiles = grep /\.xml$/i, readdir DIRHANDLE;
+		my @xmlDatafiles = grep /\.data.xml$/i, readdir DIRHANDLE;
+		rewinddir DIRHANDLE;
+		my @xmlTemplatefiles = grep /\.template.xml$/i, readdir DIRHANDLE;
 		
-		$html .= '<ul>';
+		my $dataSelectHtml = q{<select name="dictionary" size="1">\n};
+		my $templateSelectHtml = q{<select name="template" size="!">\n};
+
 		foreach my $xmlFile (sort @xmlDatafiles) {
-			$html .= qq{<li><a href="$url$xmlFile">$xmlFile</a></li>};
+			my $xmlFilename = $xmlFile;
+			
+			if ($xmlFile =~ /(.+).data.xml$/) {
+				$xmlFilename = $1;
+			}
+			
+			$dataSelectHtml .= qq{\t<option value="$xmlFile">$xmlFilename</option>\n};
 		}
-		$html .= '</ul><br>';
+		
+		foreach my $xmlFile (sort @xmlTemplatefiles) {
+			my $xmlFilename = $xmlFile;
+			
+			if ($xmlFile =~ /(.+).template.xml$/) {
+				$xmlFilename = $1;
+			}
+			
+			$templateSelectHtml .= qq{\t<option value="$xmlFile">$xmlFilename</option>\n};
+		}
+		
+		$dataSelectHtml .= q{</select>};
+		$templateSelectHtml .= q{</select>};
+		
+		$html .= qq{
+			<form action="/person/$personId/cnote" method="post">
+				<table>
+					<tr>
+						<td align="right" valign="top">Data Dictionary:</td>
+						<td align="left" valign="top">$dataSelectHtml</td>
+					</tr>
+	
+					<tr>
+						<td align="right" valign="top">Template:</td>
+						<td align="left" valign="top">$templateSelectHtml</td>
+					</tr>
+					
+					<tr colspan="0" align="center">
+						<td align="center" valign="top"><input type="submit" value="submit"><input type="reset" value="reset"></td>
+					</tr>
+				</table>
+			</form>
+		};
 	} else {
 		$html .= '<b>Couldnt open directory /export/home/sjaveed/projects/HealthSuite/WebSite/resources/data<br>';
 	}
