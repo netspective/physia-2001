5fb6caeda0441caeef6921811a33f4b084be3654
munir_faridi <munir_faridi>
2000-12-28 16:38:35 +0000

    Changed my configuration.

--- HealthSuite/Lib/perl/App/Configuration.pm
+++ HealthSuite/Lib/perl/App/Configuration.pm
@@ -275,7 +275,7 @@ sub getDefaultConfig
 	'db-sde01' => getDefaultConfig('New SWDev Configuration', CONFIGGROUP_SWDEV, 'sde01/sde@dbi:Oracle:SDEDBS04'),
 	'db-pro_thai' => getDefaultConfig('Production Test Configuration', CONFIGGROUP_TEST, 'sde04/sde@dbi:Oracle:SDEDBS04'),
 	'db-sde02' => getDefaultConfig('New SWDev Configuration', CONFIGGROUP_SWDEV, 'sde02/sde@dbi:Oracle:SDEDBS04'),
-	'db-pro_munir' => getDefaultConfig("Munir's Database", CONFIGGROUP_SWDEV, 'sde02/sde@dbi:Oracle:SDEDBS05'),
+	'db-pro_munir' => getDefaultConfig("Munir's Database", CONFIGGROUP_SWDEV, 'sde02/sde@dbi:Oracle:SDEDBS04'),
 	'db-acs_test' => getDefaultConfig("ACS TEST Database", CONFIGGROUP_SWDEV, 'ACS_TEST/ACS@dbi:Oracle:SDEDBS04'),
 	'db-pro_fkm' => getDefaultConfig("ACS TEST Database", CONFIGGROUP_SWDEV, 'sde03/sde@dbi:Oracle:SDEDBS05'),
 
