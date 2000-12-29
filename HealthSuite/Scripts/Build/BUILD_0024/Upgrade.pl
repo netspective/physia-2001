9adebf9c8464b00518bb0c08a68d6904f4512206
thai_nguyen <thai_nguyen>
2000-12-29 15:27:07 +0000

    Initial creation for BUILD_0024

---
+++ HealthSuite/Scripts/Build/BUILD_0024/Upgrade.pl
@@ -0,0 +1,39 @@
+#!/usr/bin/perl -I.
+
+##############################################################################
+package Main;
+##############################################################################
+
+use strict;
+use App::Universal;
+use App::Configuration;
+use App::External;
+
+use File::Spec;
+
+use vars qw($page $sqlPlusKey $BUILDIR);
+
+$BUILDIR = `pwd`;
+chomp $BUILDIR;
+
+($page, $sqlPlusKey) = App::External::connectDB();
+# You are now connected to the database and have access to a minimal $page object.
+
+
+######## BEGIN UPGRADE SCRIPT #########
+
+system(qq{
+	cd @{[ $CONFDATA_SERVER->path_Database ]}
+	./GenerateSchema.pl
+});
+
+my $schemaName = uc($sqlPlusKey);
+$schemaName =~ s/\/.*//;
+
+App::External::runSQL('BUILD_0024_analyze_schema.sql', $schemaName);
+App::External::runSQL('BUILD_0024_compile_invalid_and_disabled.sql');
+
+
+######## END UPGRADE SCRIPT #########
+
+exit;
