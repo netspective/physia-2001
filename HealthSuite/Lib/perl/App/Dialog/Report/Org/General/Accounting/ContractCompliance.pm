a54e2c91e7ac4fea34eb970ec158e6b4cf709f63
yousuf_sayed <yousuf_sayed>
2001-01-16 07:03:27 +0000

    [B1384] Sorting Order in the compliance report

--- HealthSuite/Lib/perl/App/Dialog/Report/Org/General/Accounting/ContractCompliance.pm
+++ HealthSuite/Lib/perl/App/Dialog/Report/Org/General/Accounting/ContractCompliance.pm
@@ -25,33 +25,40 @@ sub new
 	my $self = App::Dialog::Report::new(@_, id => 'rpt-contract-compliance', heading => 'Contract Compliance Report');
 
 	$self->addContent(
-		new CGI::Dialog::Field::Duration(
+		new CGI::Dialog::Field::Duration (
 			name => 'service',
 			caption => 'Service Date',
 			begin_caption => 'Service Begin Date',
 			end_caption => 'Service End Date',
 		),
-		new CGI::Dialog::MultiField(
+		new CGI::Dialog::MultiField (
 			caption => 'Batch ID Range',
 			name => 'batch_fields',
 			fields => [
-				new CGI::Dialog::Field(
-				caption => 'Batch ID From',
-				name => 'batch_id_from',
-				size => 12
+				new CGI::Dialog::Field (
+					caption => 'Batch ID From',
+					name => 'batch_id_from',
+					size => 12
 				),
-				new CGI::Dialog::Field(
-				caption => 'Batch ID To',
-				name => 'batch_id_to',
-				size => 12
+				new CGI::Dialog::Field (
+					caption => 'Batch ID To',
+					name => 'batch_id_to',
+					size => 12
 				),
 			]
 		),
-		new App::Dialog::Field::Insurance::Product(
+		new App::Dialog::Field::Insurance::Product (
 			caption => 'Insurance Product',
 			name => 'product_id',
 			findPopup => '/lookup/insproduct/insorgid',
-			),
+		),
+		new CGI::Dialog::Field (
+			caption => 'Sort Order',
+			name => 'sort_order',
+			type => 'select',
+			selOptions => 'Date of Service:1;Insurance Product:2;CPT Code:3',
+			defaultValue => '1'
+		),
 	);
 
 	$self->addFooter(new CGI::Dialog::Buttons);
@@ -77,18 +84,24 @@ sub execute
 
 	my $serviceBeginDate = $page->field('service_begin_date');
 	my $serviceEndDate = $page->field('service_end_date');
-
 	my $batchIDFrom = $page->field('batch_id_from');
 	my $batchIDTo = $page->field('batch_id_to');
-
 	my $productID = $page->field('product_id');
-
+	my $sortOrder = $page->field('sort_order');
 	my $orgInternalID = $page->session('org_internal_id');
+
+	my @stmtList = (
+		'selCompliantInvoicesByServiceDate',
+		'selCompliantInvoicesByProductName',
+		'selCompliantInvoicesByCode'
+	);
+
+	my $stmt = $stmtList[$sortOrder-1];
 
 	my $html = $STMTMGR_REPORT_CONTRACT_COMPLIANCE->createHtml(
 		$page,
 		STMTMGRFLAG_NONE,
-		'selCompliantInvoices',
+		$stmt,
 		[$orgInternalID, $serviceBeginDate, $serviceEndDate, $batchIDFrom, $batchIDTo, $productID]);
 
 	return $html;
--- HealthSuite/Lib/perl/App/Statements/Report/ContractCompliance.pm
+++ HealthSuite/Lib/perl/App/Statements/Report/ContractCompliance.pm
@@ -7,100 +7,127 @@ use Exporter;
 use DBI::StatementManager;
 use App::Universal;
 
-use vars qw(@ISA @EXPORT $STMTMGR_REPORT_CONTRACT_COMPLIANCE);
+use vars qw(@ISA @EXPORT $STMTMGR_REPORT_CONTRACT_COMPLIANCE $STMTMGR_SEL_COMPLIANT_INVOICES $PUB_DEFN);
 
 @ISA    = qw(Exporter DBI::StatementManager);
 @EXPORT = qw($STMTMGR_REPORT_CONTRACT_COMPLIANCE );
 
+$STMTMGR_SEL_COMPLIANT_INVOICES = qq
+{
+	select distinct
+		to_char(ii.service_begin_date, '$SQLSTMT_DEFAULTDATEFORMAT') service_date,
+		to_char(iia.pay_date, '$SQLSTMT_DEFAULTDATEFORMAT') payment_date,
+		ins.product_name,
+		i.invoice_id,
+		ii.code,
+		iia.plan_allow,
+		iia.plan_paid
+	from
+		invoice i,
+		invoice_item ii,
+		invoice_item_adjust iia,
+		invoice_billing ib,
+		insurance ins,
+		invoice_attribute ia
+	where i.invoice_id = ii.parent_id
+	and ii.item_id = iia.parent_id
+	and iia.plan_allow <> iia.plan_paid
+	and i.billing_id = ib.bill_id
+	and ib.bill_ins_id = ins.ins_internal_id
+	and i.invoice_subtype not in ( @{[ App::Universal::CLAIMTYPE_SELFPAY ]} , @{[ App::Universal::CLAIMTYPE_CLIENT ]} )
+	and i.invoice_id = ia.parent_id
+	and ia.item_name = 'Invoice/Creation/Batch ID'
+	and i.owner_id = :1
+	and (ii.service_begin_date >= to_date(:2, '$SQLSTMT_DEFAULTDATEFORMAT') or :2 is null)
+	and (ii.service_end_date <= to_date(:3, '$SQLSTMT_DEFAULTDATEFORMAT') or :3 is null)
+	and (ia.value_text >= :4 or :4 is null)
+	and (ia.value_text <= :5 or :5 is null)
+	and (ins.product_name = :6 or :6 is null)
+	%orderbyClause%
+};
+
+$PUB_DEFN =
+{
+	columnDefn =>
+	[
+		{
+			colIdx => 0,
+			head => 'Service Date',
+			hAlign => 'center',
+			dAlign => 'center',
+			dataFmt => '#0#',
+		},
+		{
+			colIdx => 1,
+			head => 'Payment Date',
+			hAlign => 'center',
+			dAlign => 'center',
+			dataFmt => '#1#',
+		},
+		{
+			colIdx => 2,
+			head => 'Product ID',
+			hAlign => 'center',
+			dAlign => 'left',
+			dataFmt => '#2#',
+		},
+		{
+			colIdx => 3,
+			head => 'Invoice ID',
+			hAlign => 'center',
+			dAlign => 'right',
+			dataFmt => '<A HREF = "/invoice/#3#/summary">#3#</A>'
+		},
+		{
+			colIdx => 4,
+			head => 'CPT Code',
+			hAlign => 'center',
+			dAlign => 'left',
+			dataFmt => '#4#',
+		},
+		{
+			colIdx => 5,
+			head => 'Amount<br>Expected',
+			hAlign => 'center',
+			dAlign => 'right',
+			dataFmt => '#5#',
+			dformat => 'currency',
+		},
+		{
+			colIdx => 6,
+			head => 'Amount<br>Paid',
+			hAlign => 'center',
+			dAlign => 'right',
+			dataFmt => '#6#',
+			dformat => 'currency',
+		},
+	],
+};
+
 $STMTMGR_REPORT_CONTRACT_COMPLIANCE = new App::Statements::Report::ContractCompliance
 (
-	'selCompliantInvoices' =>
+	'selCompliantInvoicesByServiceDate' =>
 	{
-		sqlStmt => qq
-		{
-			select distinct
-				to_char(iia.pay_date, '$SQLSTMT_DEFAULTDATEFORMAT'),
-				ins.product_name,
-				i.invoice_id,
-				ii.code,
-				iia.plan_allow,
-				iia.plan_paid
-			from
-				invoice i,
-				invoice_item ii,
-				invoice_item_adjust iia,
-				invoice_billing ib,
-				insurance ins,
-				invoice_attribute ia
-			where i.invoice_id = ii.parent_id
-			and ii.item_id = iia.parent_id
-			and iia.plan_allow <> iia.plan_paid
-			and i.billing_id = ib.bill_id
-			and ib.bill_ins_id = ins.ins_internal_id
-			and i.invoice_subtype not in ( @{[ App::Universal::CLAIMTYPE_SELFPAY ]} , @{[ App::Universal::CLAIMTYPE_CLIENT ]} )
-			and i.invoice_id = ia.parent_id
-			and ia.item_name = 'Invoice/Creation/Batch ID'
-			and i.owner_id = :1
-			and (ii.service_begin_date >= to_date(:2, '$SQLSTMT_DEFAULTDATEFORMAT') or :2 is null)
-			and (ii.service_end_date <= to_date(:3, '$SQLSTMT_DEFAULTDATEFORMAT') or :3 is null)
-			and (ia.value_text >= :4 or :4 is null)
-			and (ia.value_text <= :5 or :5 is null)
-			and (ins.product_name = :6 or :6 is null)
-			order by 1, 2, 3, 4
-		},
+		sqlStmt => $STMTMGR_SEL_COMPLIANT_INVOICES,
+		orderbyClause => 'order by service_date',
+		sqlStmtBindParamDescr => ['org internal id, service begin and end date, batch from and to id, product name, sort order'],
+		publishDefn => $PUB_DEFN,
+	},
 
-		sqlStmtBindParamDescr => ['org internal id, service begin and end date, batch from and to id, product name'],
+	'selCompliantInvoicesByProductName' =>
+	{
+		sqlStmt => $STMTMGR_SEL_COMPLIANT_INVOICES,
+		orderbyClause => 'order by product_name',
+		sqlStmtBindParamDescr => ['org internal id, service begin and end date, batch from and to id, product name, sort order'],
+		publishDefn => $PUB_DEFN,
+	},
 
-		publishDefn =>
-		{
-			columnDefn =>
-			[
-				{
-					colIdx => 0,
-					head => 'Payment Date',
-					hAlign => 'center',
-					dAlign => 'center',
-					dataFmt => '#0#',
-				},
-				{
-					colIdx => 1,
-					head => 'Product ID',
-					hAlign => 'center',
-					dAlign => 'left',
-					dataFmt => '#1#',
-				},
-				{
-					colIdx => 2,
-					head => 'Invoice ID',
-					hAlign => 'center',
-					dAlign => 'right',
-					dataFmt => '<A HREF = "/invoice/#2#/summary">#2#</A>'
-				},
-				{
-					colIdx => 3,
-					head => 'CPT Code',
-					hAlign => 'center',
-					dAlign => 'left',
-					dataFmt => '#3#',
-				},
-				{
-					colIdx => 4,
-					head => 'Amount<br>Expected',
-					hAlign => 'center',
-					dAlign => 'right',
-					dataFmt => '#4#',
-					dformat => 'currency',
-				},
-				{
-					colIdx => 5,
-					head => 'Amount<br>Paid',
-					hAlign => 'center',
-					dAlign => 'right',
-					dataFmt => '#5#',
-					dformat => 'currency',
-				},
-			],
-		},
+	'selCompliantInvoicesByCode' =>
+	{
+		sqlStmt => $STMTMGR_SEL_COMPLIANT_INVOICES,
+		orderbyClause => 'order by code',
+		sqlStmtBindParamDescr => ['org internal id, service begin and end date, batch from and to id, product name, sort order'],
+		publishDefn => $PUB_DEFN,
 	},
 
 );
