efcb7addb4e88c7e962fb94a51bc6051289361ff
munir_faridi <munir_faridi>
2001-01-23 23:27:53 +0000

    Update statement for new parent_inv_id field in invoice_item_adjust table.

--- HealthSuite/Scripts/Build/BUILD_0020/BUILD_0020_dml.sql
+++ HealthSuite/Scripts/Build/BUILD_0020/BUILD_0020_dml.sql
@@ -1,9 +1,9 @@
 whenever sqlerror exit sql.sqlcode rollback
 
 
---bug 1930 (Munir Faridi)
-
+--BUG 1930 (MAF)
 insert into transaction_type (id, caption, group_name, remarks) values (9040, 'On Call', 'Action', 'This type is created when a provider is on call');
+
 
 --BUG 2008 (FKM) Move data to new columns in offering_catalog_entry
 update offering_catalog_entry
@@ -13,5 +13,12 @@ name = description
 where catalog_id IN
 (SELECT internal_catalog_id from offering_catalog where
 catalog_type = 5);
+
+
+--NO BUG (MAF)
+update invoice_item_adjust iia set parent_inv_id = (select ii.parent_id from 
+invoice_item ii where ii.item_id = iia.parent_id);
+
+
 
 commit;
