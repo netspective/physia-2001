eb64a9a6542c1de5dc671c2de76609ea50e9e988
frank_major <frank_major>
2001-01-23 21:34:38 +0000

    [B2008] Move data to new location for ancillary service catalogs

--- HealthSuite/Scripts/Build/BUILD_0020/BUILD_0020_dml.sql
+++ HealthSuite/Scripts/Build/BUILD_0020/BUILD_0020_dml.sql
@@ -5,4 +5,13 @@ whenever sqlerror exit sql.sqlcode rollback
 
 insert into transaction_type (id, caption, group_name, remarks) values (9040, 'On Call', 'Action', 'This type is created when a provider is on call');
 
+--BUG 2008 (FKM) Move data to new columns in offering_catalog_entry
+update offering_catalog_entry
+set data_text = modifier,
+modifier = name,
+name = description
+where catalog_id IN
+(SELECT internal_catalog_id from offering_catalog where
+catalog_type = 5);
+
 commit;
